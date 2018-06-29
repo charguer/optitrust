590f1bcf09bc7ad1fac56efab174240f2709c625
Ramon <rf1515@ic.ac.uk>
2018-06-29 12:03:49 +0200

    Started writing the transformations as relations.

--- Transformation.v
+++ Transformation.v
@@ -22,6 +22,7 @@ Definition struct_x : typdef_struct := (\{})["x" := typ_int].
 Definition pos' : typdef_struct := (\{})["s" := (typ_struct "struct_x")]["y" := typ_int]["z" := typ_int].
 Definition C' : typdefctx := (\{})["pos" := pos'].
 
+(*
 Axiom map_tr_vals_on_map_vals : forall A B, map A B -> map A B.
 
 (*
@@ -64,20 +65,86 @@ Fixpoint tr (t:trm) : trm :=
           trm_app outer_access ((trm_app inner_access (p::nil))::nil)
       | _, _ => trm_app f ts'
       end
-  end.
+  end.*)
 
-(* Thoughts:
-The critical terms in which we need to change the pointer are those applied on
-a get or a set. At first I thought that the whole program should be changed 
-using what now is called tr_critical. However, then we would not be able to 
-prove that the semantics are preserved as a program could evaluate to a 
-val_abstract_ptr that we change. So we could invoke tr_critical in terms 
-that follow a get or a set, but that would not work either because of variables.
-If we apply tr_critical on let bindings as well though, we have the same problem
-with the semantics not being preserved. I assume that that is not so bad and we
-can reformulate the statement of semantics preservation to take into account this
-case.
-*)
+Inductive tr_accesses : accesses -> accesses -> Prop :=
+  | tr_accesses_nil :
+      tr_accesses nil nil
+  | tr_accesses_access_field_x : forall π π',
+      tr_accesses π π' ->
+      tr_accesses ((access_field "x")::π) ((access_field "s")::(access_field "x")::π')
+  | tr_accesses_other : forall π π' x,
+      tr_accesses π π' ->
+      tr_accesses (x::π) (x::π').
+
+Inductive tr_val : val -> val -> Prop :=
+  | tr_val_unit :
+      tr_val val_unit val_unit
+  | tr_val_bool : forall b,
+      tr_val (val_bool b) (val_bool b)
+  | tr_val_int : forall i,
+      tr_val (val_int i) (val_int i)
+  | tr_val_double : forall d,
+      tr_val (val_double d) (val_double d)
+  | tr_val_abstract_ptr : forall l π π',
+      tr_accesses π π' ->
+      tr_val (val_abstract_ptr l π) (val_abstract_ptr l π')
+  | tr_val_array : forall a a',
+      length a = length a' ->
+      (forall i, 
+        index a i -> 
+        index a' i /\ tr_val a[i] a'[i]) ->
+      tr_val (val_array a) (val_array a')
+  | tr_val_struct : forall s s',
+      dom s = dom s' ->
+      (forall f,
+        index s f ->
+        index s' f /\ tr_val s[f] s'[f]) ->
+      tr_val (val_struct s) (val_struct s').
+
+Inductive tr_state : state -> state -> Prop :=
+  | tr_state_intro : forall m m',
+      dom m = dom m' ->
+      (forall l,
+        index m l ->
+        index m' l /\ tr_val m[l] m'[l]) ->
+      tr_state m m'.
+
+Inductive tr_trm : trm -> trm -> Prop :=
+  | tr_trm_val : forall v v',
+      tr_val v v' ->
+      tr_trm (trm_val v) (trm_val v')
+  | tr_trm_var : forall x,
+      tr_trm (trm_var x) (trm_var x)
+  | tr_trm_if : forall t1 t2 t3 t1' t2' t3',
+      tr_trm t1 t1' ->
+      tr_trm t2 t2' ->
+      tr_trm t3 t3' ->
+      tr_trm (trm_if t1 t2 t3) (trm_if t1' t2' t3')
+  | tr_trm_let : forall x t1 t2 t1' t2',
+      tr_trm t1 t1' ->
+      tr_trm t2 t2' ->
+      tr_trm (trm_let x t1 t2) (trm_let x t1' t2')
+  | tr_trm_binop : forall t1 t2 t1' t2',
+      tr_trm t1 t1' ->
+      tr_trm t2 t2' ->
+      tr_trm (trm_app binop_add (t1::t2::nil)) (trm_app binop_add (t1'::t2'::nil))
+  (* Abstract heap operations *)
+  | tr_trm_get : forall T p p',
+      tr_trm p p' ->
+      tr_trm (trm_app (prim_get T) (p::nil)) (trm_app (prim_get T) (p'::nil))
+  | tr_trm_set : forall T p p' t t',
+      tr_trm p p' ->
+      tr_trm t t' ->
+      tr_trm (trm_app (prim_set T) (p::t::nil)) (trm_app (prim_set T) (p'::t'::nil))
+  | tr_trm_new : forall T t t',
+      tr_trm t t' ->
+      tr_trm (trm_app (prim_new T) (t::nil)) (trm_app (prim_new T) (t'::nil))
+  | tr_trm_array_access : forall A t i t' i',
+      tr_trm (trm_app (prim_array_access A) (t::i::nil)) (trm_app (prim_array_access A) (t'::i'::nil)).
+  (* Special case: struct access *)
+  (*| tr_trm_struct_access :
+      (trm_app (prim_struct_access T f) (t::nil))*)
 
 (* Semantics preserved by tr. *)
 Theorem red_tr: forall E t s1 s2 v,
