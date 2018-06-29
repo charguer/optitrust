f719e4785bdb5cac946cec8d3e7d2d0f8c1b8e44
Ramon <rf1515@ic.ac.uk>
2018-06-29 15:10:33 +0200

    Created group_tr.

--- Transformation.v
+++ Transformation.v
@@ -22,63 +22,73 @@ Definition struct_x : typdef_struct := (\{})["x" := typ_int].
 Definition pos' : typdef_struct := (\{})["s" := (typ_struct "struct_x")]["y" := typ_int]["z" := typ_int].
 Definition C' : typdefctx := (\{})["pos" := pos'].
 
+(* Grouping transformation *)
+Record group_tr := make_group_tr {
+  group_tr_struct_name : typvar;
+  group_tr_new_struct_name : typvar;
+  group_tr_fields : list field
+}.
+
 (* π ~ |π| *)
-Inductive tr_accesses : accesses -> accesses -> Prop :=
-  | tr_accesses_nil :
-      tr_accesses nil nil
-  | tr_accesses_access_x : forall π π',
-      tr_accesses π π' ->
-      tr_accesses ((access_field "x")::π) ((access_field "s")::(access_field "x")::π')
-  | tr_accesses_other : forall π π' x,
-      x <> access_field "x" ->
-      tr_accesses π π' ->
-      tr_accesses (x::π) (x::π').
+Inductive tr_accesses : group_tr -> accesses -> accesses -> Prop :=
+  | tr_accesses_nil : forall gt,
+      tr_accesses gt nil nil
+  | tr_accesses_array : forall gt π π' i,
+      tr_accesses gt π π' ->
+      tr_accesses gt ((access_array i)::π) ((access_array i)::π')
+  | tr_accesses_field_group : forall gt π π' f f',
+      tr_accesses gt π π' ->
+      List.In f (group_tr_fields gt) ->
+      f' = group_tr_new_struct_name gt ->
+      tr_accesses gt ((access_field f)::π) ((access_field f')::(access_field f)::π')
+  | tr_accesses_field_other : forall gt π π' f,
+      tr_accesses gt π π' ->
+      ~ List.In f (group_tr_fields gt) ->
+      tr_accesses gt ((access_field f)::π) ((access_field f)::π').
 
 (* v ~ |v| *)
-Inductive tr_val : val -> val -> Prop :=
-  | tr_val_unit :
-      tr_val val_unit val_unit
-  | tr_val_bool : forall b,
-      tr_val (val_bool b) (val_bool b)
-  | tr_val_int : forall i,
-      tr_val (val_int i) (val_int i)
-  | tr_val_double : forall d,
-      tr_val (val_double d) (val_double d)
-  | tr_val_abstract_ptr : forall l π π',
-      tr_accesses π π' ->
-      tr_val (val_abstract_ptr l π) (val_abstract_ptr l π')
-  | tr_val_array : forall a a',
+Inductive tr_val : group_tr -> val -> val -> Prop :=
+  | tr_val_unit : forall gt,
+      tr_val gt val_unit val_unit
+  | tr_val_bool : forall gt b,
+      tr_val gt (val_bool b) (val_bool b)
+  | tr_val_int : forall gt i,
+      tr_val gt (val_int i) (val_int i)
+  | tr_val_double : forall gt d,
+      tr_val gt (val_double d) (val_double d)
+  | tr_val_abstract_ptr : forall gt l π π',
+      tr_accesses gt π π' ->
+      tr_val gt (val_abstract_ptr l π) (val_abstract_ptr l π')
+  | tr_val_array : forall gt a a',
       length a = length a' ->
       (forall i, 
         index a i -> 
-        index a' i /\ tr_val a[i] a'[i]) ->
-      tr_val (val_array a) (val_array a')
-  | tr_val_struct : forall s s',
+        index a' i /\ tr_val gt a[i] a'[i]) ->
+      tr_val gt (val_array a) (val_array a')
+  | tr_val_struct : forall gt s s',
       dom s = dom s' ->
       (forall f,
         index s f ->
-        index s' f /\ tr_val s[f] s'[f]) ->
-      tr_val (val_struct s) (val_struct s').
-
-Search Ctx.ctx.
+        index s' f /\ tr_val gt s[f] s'[f]) ->
+      tr_val gt (val_struct s) (val_struct s').
 
 (* S ~ |S| *)
-Inductive tr_stack : stack -> stack -> Prop :=
-  | tr_stack_intro : forall S S',
+Inductive tr_stack : group_tr -> stack -> stack -> Prop :=
+  | tr_stack_intro : forall gt S S',
       List.map fst S = List.map fst S' ->
       (forall x v v',
         Ctx.lookup x S = Some v ->
-        Ctx.lookup x S' = Some v' /\ tr_val v v') ->
-      tr_stack S S'.
+        Ctx.lookup x S' = Some v' /\ tr_val gt v v') ->
+      tr_stack gt S S'.
 
 (* m ~ |m| *)
-Inductive tr_state : state -> state -> Prop :=
-  | tr_state_intro : forall m m',
+Inductive tr_state : group_tr -> state -> state -> Prop :=
+  | tr_state_intro : forall gt m m',
       dom m = dom m' ->
       (forall l,
         index m l ->
-        index m' l /\ tr_val m[l] m'[l]) ->
-      tr_state m m'.
+        index m' l /\ tr_val gt m[l] m'[l]) ->
+      tr_state gt m m'.
 
 (* t ~ |t| *)
 Inductive tr_trm : trm -> trm -> Prop :=
@@ -111,8 +121,11 @@ Inductive tr_trm : trm -> trm -> Prop :=
   | tr_trm_new : forall T t t',
       tr_trm t t' ->
       tr_trm (trm_app (prim_new T) (t::nil)) (trm_app (prim_new T) (t'::nil))
-  | tr_trm_array_access : forall A t i t' i',
-      tr_trm (trm_app (prim_array_access A) (t::i::nil)) (trm_app (prim_array_access A) (t'::i'::nil))
+  | tr_trm_array_access : forall A t i t' i' r,
+      tr_trm t t' ->
+      tr_trm i i' ->
+      r = trm_app (prim_array_access A) (t'::i'::nil) ->
+      tr_trm (trm_app (prim_array_access A) (t::i::nil)) r
   (* Special case: struct access *)
   | tr_trm_struct_access_x : forall p p' a1 a2 r,
       tr_trm p p' ->
@@ -127,15 +140,16 @@ Inductive tr_trm : trm -> trm -> Prop :=
       tr_trm (trm_app (prim_struct_access (typ_struct T) f) (p::nil)) r.
 
 (* Semantics preserved by tr. *)
-Theorem red_tr: forall t t' v S m1 m2,
-  red S m1 t m2 v -> 
+Theorem red_tr: forall t t' v v' S S' m1 m1' m2 m2',
   tr_trm t t' ->
-  (exists v' S' m1' m2',
-        tr_val v v'
-    /\  tr_stack S S'
-    /\  tr_state m1 m1'
-    /\  tr_state m2 m2'
-    /\  red S' m1' t' m2' v').
+  tr_val v v' ->
+  tr_stack S S' ->
+  tr_state m1 m1' ->
+  tr_state m2 m2' ->
+  red S m1 t m2 v -> 
+  red S' m1' t' m2' v'.
 Proof.
-  introv H TRt. induction H. 
+  introv Ht Hv HS Hm1 Hm2 H. gen t' v' S' m1' m2'.
+  induction H. 
+  { intros. inverts Ht. inverts HS. inverts Hm1. inverts Hm2. admit. }
 Admitted.
