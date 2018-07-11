476cdb4be655b1991db7124e24ce9ad6892cb906
Ramon <rf1515@ic.ac.uk>
2018-07-11 15:40:29 +0200

    Added explicit fixpoints.

--- Transformation.v
+++ Transformation.v
@@ -558,6 +558,69 @@ Proof.
         rewrite~ <- H11. } } }
 Qed.
 
+Fixpoint fun_tr_accesses (gt:group_tr) (π:accesses) : accesses :=
+  let aux := fun_tr_accesses gt in
+  match π with
+  | nil => nil
+  | ((access_array i)::π') => ((access_array i)::(aux π'))
+  | ((access_field T f)::π') => 
+      if isTrue(T = group_tr_struct_name gt) then
+        if isTrue(f \in group_tr_fields gt) then
+          let Tg := group_tr_new_struct_name gt in
+          let fg := group_tr_new_struct_field gt in
+          ((access_field T fg)::(access_field Tg f)::(aux π'))
+        else
+          ((access_field T f)::(aux π'))
+      else
+        ((access_field T f)::(aux π'))
+  end.
+
+Fixpoint fun_tr_val_depth (depth:nat) (gt:group_tr) (v:val) : val :=
+  match depth with
+    | O => v
+    | S n =>
+      let aux := fun_tr_val_depth n gt in
+      match v with
+      | val_error => val_error
+      | val_uninitialized => val_uninitialized
+      | val_unit => val_unit
+      | val_bool b => val_bool b
+      | val_int i => val_int i
+      | val_double d => val_double d
+      | val_abstract_ptr l π => val_abstract_ptr l (fun_tr_accesses gt π)
+      | val_array nil => val_array nil
+      | val_array vs => (val_array (LibList.map aux vs)) 
+      | val_struct T s =>
+          let m : monoid_op (map field val) := monoid_make (fun a b => a \u b) \{} in
+          let g : field -> val -> map field val := fun f v => (\{})[f:=(aux v)] in
+          if isTrue(T=group_tr_struct_name gt) then
+            let Tg := group_tr_new_struct_name gt in
+            let fg := group_tr_new_struct_field gt in
+            let fs := group_tr_fields gt in
+            let s' := fold m g s in
+            let g1' : field -> val -> map field val := 
+              fun f v => if (isTrue(f \in fs)) then (\{}) else (\{})[f:=v] in
+            let g2' : field -> val -> map field val := 
+              fun f v => if (isTrue(f \in fs)) then (\{})[f:=v] else (\{}) in
+            let s1' := fold m g1' s in
+            let s2' := fold m g2' s in
+            val_struct T (s1'[fg:=(val_struct Tg s2')])
+          else
+            val_struct T (fold m g s)
+      end
+   end.
+
+Lemma total_tr_val' : forall gt C T v,
+  exists v', tr_val gt v v'.
+Proof.
+  introv Hu. gen gt. induction Hu; intros;
+  try solve [ exists __ ; constructors~ ].
+  { induction a.
+    { admit. }
+    { admit. } }
+  { exists  }
+Qed.
+
 
 (* ---------------------------------------------------------------------- *)
 (* TLC BUFFER *)
