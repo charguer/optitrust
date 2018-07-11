b42e2b019d2312bfed8ce5f807b098cbc41b1d20
Ramon <rf1515@ic.ac.uk>
2018-07-11 18:03:06 +0200

    Proof of tr_uninitialized_val' cleaned up.

--- TrGroup.v
+++ TrGroup.v
@@ -467,7 +467,7 @@ Lemma tr_uninitialized_val' : forall gt v v' T C C',
   tr_val gt v v' ->
   uninitialized_val C T v ->
   uninitialized_val C' T v'.
-Proof.
+Proof using.
   introv HC Hv Hu. gen C' v'. induction Hu; intros;
   try solve [ inverts Hv ; constructors~ ].
   { (* val array *)
@@ -475,36 +475,49 @@ Proof.
     introv Hi. forwards*: Hai i. }
   { (* val struct *)
     tests: (T = group_tr_struct_name gt); 
-    inverts Hv; inverts HC; simpls; tryfalse.
+    inverts Hv as; inverts HC as; 
+    try solve [ intros ; simpls ; tryfalse ].
     { (* fields grouped *)
-      constructors*. unfolds typdefctx. unfolds typdef_struct.
-      rewrite H13. rew_set~. unfolds typdefctx. unfolds typdef_struct.
-      inverts H6. subst. congruence. inverts H6. introv Hi. subst.
-      tests: (f = fg).
-      { unfolds typdefctx; unfolds typdef_struct. rewrite H14. rewrite H20.
-        constructors*. 
-        { rewrite H13 at 1. rew_set~. }
-        { introv Hi'. rewrite~ H22. forwards*: H3 f C' sg[f].
-          { rew_set in *. applys* H16. }
-          { constructors*. }
-          { applys* H11. rewrite~ <- H21. } } }
-      { forwards*: H3 f C' (s'[f]); unfolds typdefctx; unfolds typdef_struct. 
-        rewrite H18 in Hi; rew_set in *. inverts Hi; tryfalse. destruct~ H0.
-        constructors*. rewrite H18 in Hi. rew_set in Hi. inverts Hi; tryfalse.
-        applys~ H12. destruct H0. rewrite~ <- H21. destruct H0. rewrite~ <- H1.
-        rewrite* H19; rewrite H18 in Hi; rew_set in Hi; inverts Hi; tryfalse;
-        destruct~ H4. } }
+      introv HTtD HTgD HDC' HDCT HDCTt HfgD HCTtf HDC'Tt HC'Ttfg HC'Tgf.
+      introv HDvfs Hmg Hfg0 HDs' Htrsg Htrs' Hs'fg0. 
+      constructors~; unfolds typdefctx; unfolds typdef_struct.
+      { rewrite HDC'. rew_set~. }
+      { inverts Hmg. simpls. congruence. }
+      { inverts Hmg. simpls. introv Hi. subst.
+        tests: (f = fg0).
+        { rewrite HC'Ttfg. rewrite Hs'fg0. constructors~. 
+          { rewrite HDC' at 1. rew_set~. }
+          { introv Hi'. rewrite~ HC'Tgf. forwards~: H3 f C' sg[f].
+            { rew_set in *. applys~ HDCTt. }
+            { constructors~. }
+            { applys~ Htrsg. rewrite~ <- H5. } } }
+        { forwards~: H3 f C' (s'[f]).
+          { rewrite HDC'Tt in Hi; rew_set in *. 
+            inverts Hi as (Hin&Hnin); eauto. }
+          { constructors~. }
+          { applys~ Htrs'; rewrite HDC'Tt in Hi; rew_set in Hi.
+            { inverts Hi as (Hin&Hnin); tryfalse.
+              rewrite~ <- H5. }
+            { inverts Hi as (Hin&Hnin); tryfalse. 
+              rewrite~ <- H1. } }
+          { rewrite~ HCTtf; rewrite HDC'Tt in Hi; rew_set in Hi;
+            inverts Hi as (Hin&Hnin); tryfalse; auto. } } } }
     { (* other struct *)
-      constructors*. unfolds typdefctx. unfolds typdef_struct.
-      rewrite* H10. rew_set~.  unfolds typdefctx. unfolds typdef_struct.
-      rewrite~ H11. congruence. introv Hi. rewrite* H11. subst.
-      forwards*: H3 f C' (s'[f]).
-      { rewrite~ <- H11. }
-      { constructors*. } 
-      { applys* H9. unfolds typdefctx. unfolds typdef_struct. rewrite <- H1.
-        rewrite~ <- H11. } } }
+      introv HTtD HTgD HDC' HC'T HDC'Tg HfgD HC'Tt HDC'Tt HC'Ttfg HC'Tgf. 
+      introv Hneq HDvfs Htrs'f.
+      constructors~; unfolds typdefctx; unfolds typdef_struct.
+      { rewrite HDC'. rew_set~. }
+      { simpls. rewrite~ HC'T. congruence. }
+      { introv Hi. rewrite~ HC'T. subst. forwards~: H3 f C' (s'[f]).
+        { rewrite~ <- HC'T. }
+        { constructors~. }
+        { applys~ Htrs'f. rewrite <- H1. rewrite~ <- HC'T. } } } }
+(* TODO: I had to add this because there were 'remaining goals on the shelf'. *)
+Unshelve. typeclass.
 Qed.
 
+(* This will be proved when the relation is translated to a function. 
+   See TrGroupFun.v. *)
 Lemma total_tr_val' : forall gt v,
   exists v', tr_val gt v v'.
 Proof.
@@ -639,10 +652,6 @@ Qed.
 
 (* ---------------------------------------------------------------------- *)
 (** Correctness of the transformation *)
-
-Axiom isTrue_var_eq : forall A (v1 v2:A), v1 = v2 -> isTrue (v1 = v2) = true.
-
-Axiom isTrue_var_neq : forall A (v1 v2:A), v1 <> v2 -> isTrue (v1 = v2) = false.
 
 (* TODO: Rewrites used quite often throughout: 
    - dom_update_at_indom
