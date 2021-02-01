f9cfdbb389e2f5ca590a315730d47356fac08aea
charguer <arthur@chargueraud.org>
2021-02-01 14:14:40 +0100

    add comments

---
+++ test_suite/test_split.ml
@@ -0,0 +1,15 @@
+(* option: use option to add open at compile time *)
+open ScriptTools
+
+let _ =
+  run
+    (fun () ->
+
+      set_init_source "test_suite/test_split.cpp";
+      add_label "return_instr" [cInstrSubstr "return"];
+
+      split_loop ~keep_labels:false [cInstrSubstr ~regexp:true "^x ="];
+      
+      dump ()
+
+    )
