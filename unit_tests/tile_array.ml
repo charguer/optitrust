18c61b5ba77ca1811843a150eb5879309cca30ed
bbytyqi <begatim.bytyqi@inria.fr>
2021-02-16 14:55:20 +0100

    Working on tile array problem

--- unit_tests/tile_array.ml
+++ unit_tests/tile_array.ml
@@ -1,6 +1,8 @@
 open ScriptTools
 
-let _ = run_unit_test (fun () ->
+let _ = 
+  run
+  (fun _ ->
   tile_array ~block_name:"X" ~block_size:"B" "T";
   tile_array ~block_name:"Y" ~block_size:"B" "U";
   )
