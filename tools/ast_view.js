8f8cb8a29e90d636d45703af810cbcb0c035afd2
charguer <arthur@chargueraud.org>
2021-04-29 15:30:16 +0200

    set editor

--- tools/ast_view.js
+++ tools/ast_view.js
@@ -1,63 +1,105 @@
-/*
-//---------------------------------------------------
-// For debugging
 
-// Demo code if no code provided
-if (codeString == undefined) {
-   codeString = 'var t = [];\nfor (var i = 0; i < 3; i++) {\n  t[i] = (function(j) {\n      return function() { return j; }; \n    })(i); \n};\nt[0](); ';
-}
 
 //---------------------------------------------------
-// CodeMirror tooling
+// Code Mirror editor
+// Documentation: https://codemirror.net/doc/manual.html
 
-// Core Mirror objects
-var source = CodeMirror.fromTextArea(document.querySelector('#source_code'), {
+var editor = CodeMirror.fromTextArea(document.getElementById('source_code'), {
   mode: 'text/x-c++src',
   lineNumbers: true,
   lineWrapping: true,
   readOnly: true,
   tabSize: 2,
-  value: codeString
   extraKeys: {
-    'N': function(cm) { next(); },
+    'N': function(cm) { console.log("pressed N in editor"); },
   },
 });
 
-// Registers a new source doc
-function newSourceDoc(name, text, readOnly) {
-  let doc = CodeMirror.Doc(text, 'text/javascript');
-  doc.setName(name);
-  doc.cantEdit = !!readOnly;
-  return doc;
+
+//---------------------------------------------------
+// Code Mirror highlight expressions
+
+// Scroll to the given location in the given CodeMirror or Doc object.
+function scrollToLoc(loc) {
+  editor.scrollIntoView(loc, 100);
 }
 
-function updateSelectionInCodeMirror(loc, opts) {
+// Update the highlighted contents
+// The "loc" argument should be of the form:
+//  { start: { line: 1, col: 4 }, end: { line: 3, col: 8 } }
+
+function updateSelection(loc) {
+  // for other options, see https://codemirror.net/doc/manual.html#markText
+  const opts = { className: "highlight" };
   if (loc === undefined) {
     return;
   }
 
   // Clear old marks
-  source.getAllMarks().forEach(m => m.clear());
+  editor.getAllMarks().forEach(m => m.clear());
 
-  // Substracting 1 because Clang counts from 1, and Codemirror from 0
-  var from = {line: loc.start.line-1, ch: loc.start.column };
-  var to = {line: loc.end.line-1, ch: loc.end.column };
+  // Substracting 1 because compilers counts from 1, and Codemirror from 0
+  var from = { line: loc.start.line-1, ch: loc.start.col };
+  var to = { line: loc.end.line-1, ch: loc.end.col };
 
-  source.markText(from, to, opts);
-  scrollToLoc(source, {from: from, to: to});
+  // Highlight and scroll to the highlighted place
+  editor.markText(from, to, opts);
+  scrollToLoc({from: from, to: to});
+  // editor.focus();
 }
 
-// Scroll to the given location in the given CodeMirror or Doc object.
-// No-op if the given doc is not currently active.
-function scrollToLoc(loc) {
-  if (source instanceof CodeMirror.Doc) {
-    source = source.getEditor();
-  }
-  if (source) {
-    source.scrollIntoView(loc, 100);
-  }
-}
 
+//---------------------------------------------------
+// Handling parameters
+
+var parameter_depth = 0; // 0 means infinity
+
+// Event listner for the depth parameter
+$("#parameter_depth").change(function(e) {
+  // get the value as an integer
+  var n = + $("#parameter_depth").val();
+  parameter_depth = n;
+  console.log("Changed parameter depth to: " + n);
+});
+
+// Event listner for the run button
+$("#button_run").click(function (e) {
+   console.log("Click on run button");
+});
+
+
+//---------------------------------------------------
+// DEMO
+
+var exampleSource = `
+   /* C demo code */
+   #include <stdio.h>
+   int f() {
+      printf("hello f");
+      return 0;
+   }
+   int g() {
+      printf("hello g");
+      return 0;
+   }
+   int main() {
+      printf("hello world\n");
+      return 0;
+   }
+`;
+
+editor.setValue(exampleSource);
+
+var selection = { start: { line: 5, col: 6 }, end: { line: 6, col: 15 } };
+
+updateSelection(selection);
+
+
+
+//---------------------------------------------------
+// FOR LATER: function to scroll between marks
+
+/*
 // Scroll to the first mark in the given CodeMirror or Doc object.
 // No-op if given object not active, or if no marks in the doc.
 function scrollToFirstMark() {
@@ -66,37 +108,5 @@ function scrollToFirstMark() {
   let loc = ms[0].find();
   scrollToLoc(loc);
 }
-
-
-//---------------------------------------------------
-// Page behavior
-
-function updateSelection(selection) {
-  const selectionOpts = { className: "highlight" };
-  var from = {line: loc.start.line-1, ch: loc.start.column };
-  var to = {line: loc.end.line-1, ch: loc.end.column };
-  updateSelectionInCodeMirror(selection, selectionOpts);
-  interpreter.focus();
-}
-
-var selection = { start: { line: 2, col: 3 }, end: { line: 4, col: 6 } };
-updateSelection(selection);
-
-function next() {
-   selection.end.line = selection.end.line+1;
-   updateSelection(selection);
-   $("#viewast").html("foo " + selection.end.line);
-}
-
-
-var current_id = null;
-var parameter_depth = 0; // 0 means infinity
-
-// Event listner for the depth parameter
-$("#parameter_depth").change(function(e) {
-  var n = + $("#parameter_depth").val();
-  parameter_depth = n;
-});
-
-
 */
+
