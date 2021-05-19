d131ab46914f3f7be306a6cf6fc5208ef42e05cf
charguer <arthur@chargueraud.org>
2021-05-19 12:25:32 +0200

    targetbetween

--- tests/infra/target_between.ml
+++ tests/infra/target_between.ml
@@ -3,7 +3,8 @@ open Optitrust
 let _ = run_unit_test (fun () ->
   (** There should be exactly one result to each of the commands;
       if it is not the case, we'll get an error. *)
-  let show = Tr.target_show in
+  let show = Tr.target_between_show in (* TODO: probably you'll need target_between_show *)
+
   (* TODO: the implementation of target_show, when the target_struct features
      a target_relative that is not "TargetAt", should invoke the transformation
      "seq_insert" to insert an empty instruction (trm_unit, displayed just a semicolumn).
