c193c536251a94d6e0c8576dbed170a69d62c400
charguer <arthur@chargueraud.org>
2021-06-22 12:24:21 +0200

    sequence_sub

--- tests/basic/sequence_sub.ml
+++ tests/basic/sequence_sub.ml
@@ -1,7 +1,19 @@
 open Optitrust
 open Target
 
-(* Works *)
 let _ = Run.script_cpp (fun _ ->
-  Sequence.sub 1 2 [cFunDef "main"; cStrict; cBody];
+ (* TODO:
+ internal:     Sequence_core.sub index nb p
+ external:
+  Sequence.sub  (path_to_first_item) nb  (using isolate_seq_and_last_dir)
+  Sequence.sub_between (target_between_start) (target_between_stop)
+
+examples:
+  Sequence.sub  [ cInstr "int y" ] 2
+  Sequence.sub_between [ cBefore; cInstr "int y" ] [ cAfter; cInstr "int z" ]
+  Sequence.sub_between [cFunDef "main"; cStrict; cBody] [ cAfter; cInstr "int z" ]
+ *)
+  !! Sequence.sub 1 2 [cFunDef "main"; cStrict; cBody];
+  !! Sequence.sub 3 1 [cFunDef "main"; cStrict; cBody];
+  !! Sequence.sub 0 4 [cFunDef "main"; cStrict; cBody];
 )
