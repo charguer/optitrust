7f65943fc1cda9f5313783dede6bd945b755c55e
bbytyqi <begatim.bytyqi@inria.fr>
2021-06-30 15:51:29 +0200

    Typedef_insert_and_fold

---
+++ tests/basic/typedef_insert_and_fold.cpp
@@ -0,0 +1,23 @@
+// typedef unsigned int uint;
+
+// typedef double const cdouble;
+
+typedef struct { unsigned int x; unsigned int y; } vect;
+
+// typedef int** mat2d;
+
+// typedef int*** mat3d; // to be folded before mat2d, else M would be of type mat2d*
+
+int main() {
+  unsigned int x;
+  unsigned int t[3];
+  int a = 1, b;
+  const double y1 = 1.0, y2 = 2.0;
+  // const double y2 = 2.0;
+  vect v;
+  int** m;
+  int*** M;
+  int**** T;
+}
+
+
