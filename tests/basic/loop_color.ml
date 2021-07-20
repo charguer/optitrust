b94a53daf9beb0411e686a10b100726925af4487
charguer <arthur@chargueraud.org>
2021-07-20 09:41:06 +0200

    more transfo

--- tests/basic/loop_color.ml
+++ tests/basic/loop_color.ml
@@ -2,6 +2,12 @@ open Optitrust
 open Target
 
 let _ = Run.script_cpp (fun _ ->
-  !! Loop.color "C" "ci" [cFor "i"] ;
-  !! Loop.color "C" "D" [cFor "j"] ;
+  !! Loop.color "C" "ci" [cFor "i"];
+  !! Loop.color "C" "D" [cFor "j"];
+  (* TODO:
+        Loop.color "C" ~index:"ci" [cFor "i"] ;
+        Loop.color "C" [cFor "j"] ;
+     default value for index is "ci" when the loop has index "i"  *)
+  (* LATER: in the future, we will check that "ci" is not conflicting
+     with an existing variable *)
 )
