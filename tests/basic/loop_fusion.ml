a72b4af503eb4076335f7182c476ba6e6b39526c
charguer <arthur@chargueraud.org>
2021-07-21 10:21:09 +0200

    trace alternative

--- tests/basic/loop_fusion.ml
+++ tests/basic/loop_fusion.ml
@@ -4,17 +4,14 @@ open Target
 let _ = Run.script_cpp ( fun _ ->
 
   !! Loop.fusion_on_block [cLabel "tofusion"];
-  !! Trace.alternative (fun () ->
-     !! Loop.fusion_on_block [cLabel "tofusion"];
-     !!());
-
 
   (* TODO: move to combi:
     !! Loop.fusion [tIndex ~nb:2 0; cFor "i"]);
 
     !! Trace.alternative (fun () ->
       !! Sequence.sub 3 ~label:"tofusion" [tIndex ~nb:3 0; cFor "i"];
-      !! Loop.fusion_on_block [cLabel "tofusion"];)
+      !! Loop.fusion_on_block [cLabel "tofusion"];
+      !!());
   *)
 
 )
