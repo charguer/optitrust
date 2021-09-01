4edb4559d53df400f0a2743f0309b04f2338c05c
bbytyqi <begatim.bytyqi@inria.fr>
2021-09-01 13:59:33 +0200

    Fix for variable_rename

--- tests/basic/variable_rename.ml
+++ tests/basic/variable_rename.ml
@@ -2,6 +2,6 @@ open Optitrust
 open Target
 
 let _ = Run.script_cpp (fun _ ->
-  !! Variable_basic.rename ~list:[("y","y1");("z","z1")] [cFunDef "f"; dBody];
-  !! Variable_basic.rename ~func: (fun s -> s ^ "2") [cTopFun "main"; dBody]; 
+  !! Variable_basic.rename (Ast.Rename_list [("y","y1");("z","z1")]) [cFunDef "f"; dBody];
+  !! Variable_basic.rename (Ast.Postfix "2") [cTopFun "main"; dBody]; 
 )
