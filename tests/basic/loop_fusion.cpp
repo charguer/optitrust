2b9d458847422b4ae5782d62292bbae3e4c7eb61
charguer <arthur@chargueraud.org>
2021-09-03 11:24:39 +0200

    review fusion

--- tests/basic/loop_fusion.cpp
+++ tests/basic/loop_fusion.cpp
@@ -5,16 +5,28 @@ int n;
 
 int main() {
   float p = 5.0;
-  tofusion: {
-    for (int i = 0; i < n; i++) {
-      t[i] = i;
-    }
-    for (int i = 0; i < n; i++) {
-      u[i] += i;
-    }
-    for (int i = 0; i < n; i++) {
-      v[i] += i;
-    }
+  for (int i = 0; i < n; i++) {
+    t[i] = i;
+  }
+  for (int i = 0; i < n; i++) {
+    u[i] += i;
+  }
+  for (int i = 0; i < n; i++) {
+    v[i] += i;
+  }
+  return 0;
+}
+
+int fusion_on_block() {
+  float p = 5.0;
+  for (int i = 0; i < n; i++) {
+    t[i] = i;
+  }
+  for (int i = 0; i < n; i++) {
+    u[i] += i;
+  }
+  for (int i = 0; i < n; i++) {
+    v[i] += i;
   }
   return 0;
 }
--- tests/basic/loop_fusion.ml
+++ tests/basic/loop_fusion.ml
@@ -2,5 +2,18 @@ open Optitrust
 open Target
 
 let _ = Run.script_cpp ( fun _ ->
+  (* fuse three loops *)
+  !! Loop.fusion ~nb:3 [cFunDef "main"; cFor "i" ~body:[sInstr "t[i]"]];
+
+  !! Trace.alternative (fun () ->
+    (* default is two loops *)
+    !! Loop.fusion [cFunDef "main"; cFor "i" ~body:[sInstr "t[i]"]];
+    !!());
+
+  (* LATER: try to improve error: "cannot fuse 3 loops as requested, only 2 loops are found"
+    !! Loop.fusion ~nb:3 [cFunDef "main"; tIndex ~nb:3 1; cFor "i"]; *)
+
+  (* fuse loops inside a labelled block *)
+  !! Sequence_basic.intro ~label:"tofusion" 3 [cFunDef "fusion_on_block"; cFor "i" ~body:[sInstr "t[i]"]];
   !! Loop_basic.fusion_on_block [cLabel "tofusion"];
-)
+  )
