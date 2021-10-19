4ee7246993894f6f574788b792cac321b7e1e971
charguer <arthur@chargueraud.org>
2021-10-19 11:32:03 +0200

    subst

--- tests/basic/variable_replace_occurrences.cpp
+++ tests/basic/variable_replace_occurrences.cpp
@@ -1,9 +1,11 @@
 int main() {
 
-  int x = 6;
+  int x = 3;
 
-  int y = x;
-  int z = y + x;
+  int y = x + 2;
+  int z = y + y;
+  int a = y + 3;
   z = y + 2;
+  z = y + y;
   return 0;
 }
--- tests/basic/variable_replace_occurrences.ml
+++ tests/basic/variable_replace_occurrences.ml
@@ -1,8 +1,8 @@
 open Optitrust
 open Target
 
-let _ = Run.script_cpp (fun _ -> 
+let _ = Run.script_cpp (fun _ ->
 
-  !! Variable_basic.replace_occurrences "y" ~space:"x" [cVarDef "z"];
+  !! Variable_basic.replace_occurrences "y" ~space:"(2 + x)" [cVarDef "z"]; (* TODO: rename to    subst "a" ~put:"x" tg *)
   !! Variable_basic.replace_occurrences "y" ~space:"5" [cFunDef "main"];
 )
