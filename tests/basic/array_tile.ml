d0c81c8db8ff929ef3ffb14ce13b1ed89b64b499
charguer <arthur@chargueraud.org>
2021-11-30 11:08:28 +0100

    doc

--- tests/basic/array_tile.ml
+++ tests/basic/array_tile.ml
@@ -1,8 +1,19 @@
 open Optitrust
 open Target
 
+let _ = Run.doc_script_cpp (fun _ ->
+    !! Arrays_basic.tile "B" ~block_type:"BLOCK" [cTypDef "T"];
+  )
+"
+typedef int *T;
+int main() {
+  T t;
+  int a = t[3];
+}
+"
+
 let _ = Run.script_cpp (fun _ ->
-  
+
   !! Arrays_basic.tile "B" ~block_type:"U_BLOCK" [cTypDef "U"];
   !! Arrays_basic.tile "B" [cTypDef "T"];
   !! Arrays_basic.tile "B" [cTypDef "V"];
