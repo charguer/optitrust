a962da4c09c9fd6bc018861b4afac20a251b1d1b
charguer <arthur@chargueraud.org>
2021-11-30 14:07:32 +0100

    doc progress

--- tests/basic/struct_inline.ml
+++ tests/basic/struct_inline.ml
@@ -1,8 +1,26 @@
 open Optitrust
 open Target
 
+
+let _ = Run.doc_script_cpp (fun _ ->
+    !! Struct_basic.inline "pos" [cTypDef "obj"];
+  )
+"
+typedef struct {
+  int x;
+  int y;
+} vect;
+
+typedef struct {
+  vect pos;
+  int mass;
+} obj;
+"
+
 let _ = Run.script_cpp (fun _ ->
 
    !! Struct_basic.inline "pos" [cTypDef "obj"];
    !! Struct_basic.inline "speed" [cTypDef "obj"];
 )
+
+(* LATER: at the combi level, combine struct_inline with struct-renaming-field *)
--- tests/basic/struct_rename_fields.ml
+++ tests/basic/struct_rename_fields.ml
@@ -1,6 +1,24 @@
 open Optitrust
 open Target
 
+(* TODO: another unit test for rename_field *)
+
+let _ = Run.doc_script_cpp (fun _ ->
+    !! Struct_basic.rename_fields (fun f -> String.uppercase_ascii f) [cTypDef "obj"];
+  )
+"
+typedef struct {
+  int x;
+  int y;
+} vect;
+
+typedef struct {
+  vect pos;
+  int mass;
+} obj;
+"
+
+
 let _ = Run.script_cpp (fun _ ->
 
     !! Struct_basic.rename_fields (fun x -> "rel_" ^ x) [cTypDef "vect"];
