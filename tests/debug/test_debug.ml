079c2c07de7e774e6e1ccb6c0eae3681540e2a35
charguer <arthur@chargueraud.org>
2021-12-06 16:37:28 +0100

    intermediate states

--- tests/debug/test_debug.ml
+++ tests/debug/test_debug.ml
@@ -2,7 +2,7 @@ open Optitrust
 open Target
 open Ast
 
-let _ = Run.script_cpp (fun _ -> 
+let _ = Run.script_cpp (fun _ ->
 
-    !! Function_basic.bind_intro ~fresh_name:"a" [sExpr "f(1)"]; 
+    !! Function_basic.bind_intro ~fresh_name:"a" [sExpr "f(1)"];
 )
