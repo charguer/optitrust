3996a82f0e09acec4e2af278be26a927d2a7156f
bbytyqi <begatim.bytyqi@inria.fr>
2021-12-10 08:12:27 +0100

    Fix for printing const pointers

--- src/ast_to_rawC.ml
+++ src/ast_to_rawC.ml
@@ -4,6 +4,8 @@ open Ast
 (* translate an ast to a C/C++ document *)
 let rec typ_desc_to_doc (t : typ_desc) : document =
   match t with
+  | Typ_const t when is_typ_ptr t ->
+    typ_to_doc t ^^ string " const" 
   | Typ_const t -> string " const "  ^^ typ_to_doc t
   | Typ_constr (tv, _, _) -> string tv
   | Typ_auto  -> string "auto"
