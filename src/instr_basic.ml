c15fde64dd5a5c4ade4bf867d6b339506f248d49
bbytyqi <begatim.bytyqi@inria.fr>
2022-01-13 09:20:24 +0100

    pic_demo_update

--- src/instr_basic.ml
+++ src/instr_basic.ml
@@ -7,15 +7,13 @@ open Target
 let update ?(reparse: bool = false)  (f : trm -> trm) : Target.Transfo.t =
   Target.reparse_after ~reparse (Target.apply_on_targets (Instr_core.update f))
 
-(* TODO: replace can be implemented as a particular case of update *)
-
-(* [replace code tg] expects the target to point at an instruction,
+(* [replace node tg] expects the target to point at an instruction,
     then it will replace this instruction with [node]. Note that [node] can be
     also some code entered as string which is transformed into a trm through function code
     then this node is merged into the ast by doing a reparse of the full ast.
 *)
-let replace ?(reparse: bool = false) (node : trm) : Target.Transfo.t =
-  Target.reparse_after ~reparse (Target.apply_on_targets (Instr_core.replace node))
+let replace ?(reparse : bool = false) (node : trm) : Target.Transfo.t = 
+  update ~reparse (fun _t -> node)
 
 (* [replace_fun code tg] expects the target to point to a function call,
     it then replaces the name of the function call with the one entered
