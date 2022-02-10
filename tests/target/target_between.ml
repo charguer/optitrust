c7193d502b8baf35550244f5d2740acc35b3994c
charguer <arthur@chargueraud.org>
2022-02-10 11:47:26 +0100

    show in batch mode

--- tests/target/target_between.ml
+++ tests/target/target_between.ml
@@ -1,41 +1,40 @@
 open Optitrust
 open Target
 
-let aim tg = 
-  if Flags.get_exit_line() <> None then Marks.clean [dRoot];
-  Marks.add_between (Mark.next()) tg
+let _ = Flags.execute_show_even_in_batch_mode := true
 
 let _ = Run.script_cpp (fun () ->
   (** There should be exactly one result to each of the commands;
       if it is not the case, we'll get an error. *)
 
   (* Before *)
-  !! aim [ tBefore; cVarDef "r1" ];
-  !! aim [ tBefore; cVarDef "r2" ];
-  !! aim [ tBefore; cVarDef "m1" ];
-  !! aim [ tBefore; cVarDef "m2" ];
+  show [ tBefore; cVarDef "r1" ];
+  show [ tBefore; cVarDef "r2" ];
+  show [ tBefore; cVarDef "m1" ];
+  show [ tBefore; cVarDef "m2" ];
 
   (* After *)
-  !! aim [ tAfter; cVarDef "r1" ];
-  !! aim [ tAfter; cVarDef "r2" ];
-  !! aim [ tAfter; cVarDef "m1" ];
-  !! aim [ tAfter; cVarDef "m2" ];
+  show [ tAfter; cVarDef "r1" ];
+  show [ tAfter; cVarDef "r2" ];
+  show [ tAfter; cVarDef "m1" ];
+  show [ tAfter; cVarDef "m2" ];
 
   (* First *)
-  !! aim [ tFirst; cFor "i"; dBody ];
-  !! aim [ tFirst; cIf(); dElse ];
+  show [ tFirst; cFor "i"; dBody ];
+  show [ tFirst; cIf(); dElse ];
 
   (* Last *)
-  !! aim [ tLast; cIf(); dElse ];
-  !! aim [ tLast; cFor "i"; dBody];
-  !! aim [ tLast; cIf(); dThen ];
-  !! aim [ tLast; cIf(); dElse ];
+  show [ tLast; cIf(); dElse ];
+  show [ tLast; cFor "i"; dBody];
+  show [ tLast; cIf(); dThen ];
+  show [ tLast; cIf(); dElse ];
 
   (* Nested paths *)
-  !! aim [ tLast; cFor"i"; cIf() ;dThen];
+  show [ tLast; cFor"i"; cIf() ;dThen];
   (* Top level paths *)
-  !! aim [tBefore; cTopFunDef "main"];
-  !! aim [tAfter; cTopFunDef "main"];
+  show [tBefore; cTopFunDef "main"];
+  show [tAfter; cTopFunDef "main"];
+
 )
 
 
