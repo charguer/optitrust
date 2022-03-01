f213ce42c94ff5ec1fef32bdebd7c2817f950881
charguer <arthur@chargueraud.org>
2022-03-01 15:38:13 +0100

    add tests for function inline

--- tests/combi/function_inline_simple.cpp
+++ tests/combi/function_inline_simple.cpp
@@ -39,6 +39,8 @@ int main() {
   int x = 3;
   int y = f(x);
   int z = g(x);
+  int s = y + z;
+
   int u = h(x);
   int *q;
   m(q);
@@ -48,3 +50,18 @@ int main() {
   vect c = vect_add (b, vect_mul(x, a));
   return 0;
 }
+
+void test_const_ret() {
+  int x = 3;
+  const int y = f(x);
+  const int z = g(x);
+  int s = y + z;
+}
+
+void test_const_arg() {
+  const int x = 3;
+  int y = f(x);
+  int z = g(x);
+  int s = y + z;
+}
+
--- tests/combi/function_inline_simple.ml
+++ tests/combi/function_inline_simple.ml
@@ -5,20 +5,27 @@ open Target
 
 
 let _ = Run.script_cpp (fun _ ->
-  
+
+  (* inlining a function with single return *)
+  !! Function.inline [nbMulti; cFun "f"];
+  (* inlining a function with if else branches *)
+  !! Function.inline [nbMulti; cFun "g"];
+  (* inlining a function with one if branch *)
+  !! Function.inline [nbMulti; cFun "h"];
+  (* inlining a function of type void *)
+  !! Function.inline [nbMulti; cFun "m"];
+
+
   (* !! Function.inline ~vars:(AddSuffix "${occ}") [occFirst;cFun "f?"]; *)
   !! Function.inline [cFun "vect_mul"];
   !! Function.inline [cFun "vect_add"];
-  
+
   (* with naming of the arguments *)
   (* !! Trace.alternative (fun () ->
     (* TODO: Fix this error *)
     !! Function.inline  ~args:["v"] [nbMulti;cFunDef "main";cFun "f"];
     !!()); *)
-  (* inlining a function with if else branches *)
-  !! Function.inline [cFun "g"];
-  (* inlining a function with one if branch *)
-  !! Function.inline [cFun "h"]; 
-  (* inlining a function of type void *)
-  !! Function.inline [cFun "m"];
+
+  (* TODO: add tests with ~args  and with ~vars *)
+
 )
