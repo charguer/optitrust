cd4c18d112a725a6938c4a932a50095a64c73d10
bbytyqi <begatim.bytyqi@inria.fr>
2022-03-03 11:31:32 +0100

    Fixed variable_to_const

--- tests/basic/variable_to_const.ml
+++ tests/basic/variable_to_const.ml
@@ -10,8 +10,6 @@ int main() {
 }
 "
 
-(* let _ = Flags.dump_ast_details := true *)
-
 let _ = Run.script_cpp (fun _ ->
 
   !! Variable_basic.to_const [cVarDef "x"];
@@ -24,34 +22,10 @@ let _ = Run.script_cpp (fun _ ->
   !! Variable_basic.to_const [cVarDef "p"];
   !! Variable_basic.to_const [cVarDef "q"];
   !! Variable_basic.to_const [cVarDef "r"];
+
+  
+  
 )
 
 (* Note: recall that currently const references are not supported,
    see Ast_fromto_AstC *)
-
-
-
-   (*
-   TODO try
-
-   toconst x
-
-exception Variable_to_const_abort
-
-let aux u =
-  match u with
-  | trm_var y when y = x -> u
-  | struct_access(u1, f) -> struct_get(aux u1, f)
-  | _ -> raise Aux_abort
-
-  (* LATER: check we don't need any support for arrays
-   | array_access(u1, u2) -> struct_get(aux u1, u2) *)
-
-let tr t =
-  match t with
-  | get(u) ->
-      try (aux u)
-      with Variable_to_const_abort -> t
-  | _ -> trm_map (fun t -> (aux t u))
-
-*)
--- tests/basic/variable_to_const_exp.cpp
+++ tests/basic/variable_to_const_exp.cpp
@@ -4,3 +4,27 @@ int main() {
   const int z = x + y + 4;
   return z;
 }
+
+typedef struct {
+  int x;
+  int y;
+} vect;
+
+typedef struct {
+  vect pos;
+  vect speed;
+} part;
+
+int test_vect() {
+  const vect v = {0, 1};
+  const vect w = v;
+  int a = v.x;
+  int b = v.y;
+  const part p = {v, w};
+  vect ppos = p.pos;
+  int c = p.pos.x;
+  const part[2] q = {{v, w}, {v, w}};
+  int d = q[0].pos.x;
+  part *const r = &(*q)[0];
+  int e = r[0].pos.x;
+}
