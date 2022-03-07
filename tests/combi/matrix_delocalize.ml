9816897a9bc67ad6447b69e164aa225bc85cd829
bbytyqi <begatim.bytyqi@inria.fr>
2022-03-07 20:24:00 +0100

    generalized specialize

--- tests/combi/matrix_delocalize.ml
+++ tests/combi/matrix_delocalize.ml
@@ -10,7 +10,7 @@ let _ = Run.script_cpp (fun _ ->
         !!());
 
   !! Trace.alternative (fun () ->
-        !! Matrix.delocalize "a" ~use:"k" ~into:"x" ~init_zero:true ~acc_in_place:false  ~acc:"sum" ~dim:(var "N0") ~index:"i0" ~ops:(Delocalize_arith (Lit_int 0, Binop_add)) [cFor "i"];
+        !! Matrix.delocalize "a" ~use:(Some (expr"k")) ~into:"x" ~init_zero:true ~acc_in_place:false  ~acc:"sum" ~dim:(var "N0") ~index:"i0" ~ops:(Delocalize_arith (Lit_int 0, Binop_add)) [cFor "i"];
         !!());
 
 )
