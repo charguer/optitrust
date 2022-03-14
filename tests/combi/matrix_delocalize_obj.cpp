df4043c1ba46fd6bceedacec8b84ca1b3627c3e8
charguer <arthur@chargueraud.org>
2022-03-14 17:04:01 +0100

    review

--- tests/combi/matrix_delocalize_obj.cpp
+++ tests/combi/matrix_delocalize_obj.cpp
@@ -45,27 +45,27 @@ int main() {
   int const N3 = 12;
 
 // Allocate bagsNext and bagsCur with empty bags in every cell
-  
+
   bag* bagCur = (bag*) MMALLOC1(nbCells, sizeof(bag));
   bag_iter bag_it;
-  
+
   bag* bagNext = (bag*) MMALLOC1(nbCells, sizeof(bag));
   for (int idCell = 0; idCell < nbCells; idCell++){
-    bag_init(&bagNext[MINDEX1(nbCells,idCell)], 0, idCell);
+    bag_init(&bagNext[MINDEX1(nbCells,idCell)]);
   }
-  
+
   for (int idCell = 0; idCell < nbCells; idCell++){
     for (particle* p = bag_iter_begin(&bag_it, NULL); p != NULL; p = bag_iter_next_common(&bag_it, true)) {
       bag_push(&bagNext[MINDEX1(nbCells, idCell)],*p);
     }
   }
-    
+
   for (int idCell = 0; idCell < nbCells; idCell++){
     bag_swap(&bagNext[MINDEX1(nbCells,idCell)], &bagCur[MINDEX1(nbCells,idCell)]);
   }
   MFREE(bagCur);
   MFREE(bagNext);
-  
+
   return 0;
 
 }
--- tests/combi/matrix_delocalize_obj.ml
+++ tests/combi/matrix_delocalize_obj.ml
@@ -4,7 +4,7 @@ open Ast
 
 let _ = Run.script_cpp (fun _ ->
 
-  !! Matrix.delocalize ~last:true "bagNext" ~into:"bagNexts" ~init_zero:true ~acc_in_place:false ~dim:(var "N0") ~index:"i0" ~acc:"sum" ~ops:(Local_obj ("bag_init", "bag_merge", "bag_free")) [cFor "idCell" ~body:[cFun "bag_push"]];
+  !! Matrix.delocalize ~last:true "bagNext" ~into:"bagNexts" ~init_zero:true ~acc_in_place:false ~dim:(var "N0") ~index:"bagKind" ~indices:["idCell"] ~acc:"sum" ~ops:(Local_obj ("bag_init", "bag_merge", "bag_free")) [cFor "idCell" ~body:[cFun "bag_push"]];
   !! Trace.alternative (fun () ->
         !! Matrix.delocalize "bagNext" ~into:"bagNexts" ~init_zero:true ~acc_in_place:false ~dim:(var "N0") ~index:"i0" ~acc:"sum" ~ops:(Local_obj ("bag_init", "bag_merge", "bag_free")) [cFor "idCell" ~body:[cFun "bag_push"]];
         !!());
