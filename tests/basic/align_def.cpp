961e5431d78c91f512e39ff805d80d0fa9d6c156
bbytyqi <begatim.bytyqi@inria.fr>
2022-03-17 09:34:01 +0100

    Fix for align_def

--- tests/basic/align_def.cpp
+++ tests/basic/align_def.cpp
@@ -1,7 +1,7 @@
 
  const double coeffs_x[8]  = {  1.,  1.,  1.,  1.,  0.,  0.,  0.,  0.};
  const double coeffs_y[8]  = {  1.,  1.,  0.,  0.,  1.,  1.,  0.,  0.};
-const double a = 1.;
+ const double a = 1.;
 
 int main (){
   const double b = 1.;
