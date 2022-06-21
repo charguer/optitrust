562a0861e26454b75cb01113763864872f16806a
bbytyqi <begatim.bytyqi@inria.fr>
2022-06-21 15:08:03 +0200

    wip

--- src/apac_basic.ml
+++ src/apac_basic.ml
@@ -6,7 +6,7 @@ open Target
 let use_goto_for_return ?(mark : mark = "") : Transfo.t =
   apply_on_targets (Apac_core.use_goto_for_return mark)
 
-let insert_tasks (sa : sorted_arg_deps) (tg : target) : unit =
+let insert_task (sa : sorted_arg_deps) (tg : target) : unit =
   let deps = [In sa.dep_in; Out sa.dep_out; Inout sa.dep_inout; Outin sa.dep_outin] in 
   transfo_on_targets (trm_add_pragma (Task [(Ast.Depend deps)])) tg
 
@@ -15,7 +15,7 @@ let insert_tasks (sa : sorted_arg_deps) (tg : target) : unit =
 type taskable = (var * arg_deps) list  (* var is a mangled name. *)
 
 
-
+(* Note: Naive implementation. *)
 let identify_taskable_functions (tg : target) : taskable = 
   let tg_trm = get_trm_at tg in 
   match tg_trm with 
@@ -24,7 +24,7 @@ let identify_taskable_functions (tg : target) : taskable =
     | Some tl -> 
       Mlist.fold_left (fun acc t -> 
         match t.desc with 
-        | Trm_let_fun (qn, ty, args, body) -> 
+        | Trm_let_fun (qn, ty, args, body) when qn.qvar_var <> "main" -> 
           (qn.qvar_var, get_arg_dependencies t) :: acc
         | _ -> acc
        ) [] tl
@@ -33,4 +33,14 @@ let identify_taskable_functions (tg : target) : taskable =
   | _ -> fail None "Apac_basic.identify_taskable_functions: expected a target to the main file sequence."
 
   
-  
+(* let bind_taskable_calls (tsk : taskable) (tg : target) : unit =
+  iter_on_transformed_targets (Internal.get_instruction_in_surrounding_sequence)
+    (fun i t (path_to_seq, local_path, i1)  -> 
+      let path_to_instruction = path_to_seq @ [Dir_seq_nth i1] in
+      let path_to_call = path_to_instruction @ local_path in
+      let call = Path.get_trm_at_path path_to_call in 
+      match call.desc with 
+      | Trm_apps ()
+      
+      
+    ) tg *)
--- src/apac_core.ml
+++ src/apac_core.ml
@@ -12,17 +12,17 @@ let use_goto_for_return_aux (mark : mark) (t : trm) : trm =
     let new_body = 
       begin match ret_ty.typ_desc with 
       | Typ_unit -> 
-        trm_seq_nomarks [
+        trm_add_mark mark (trm_seq_nomarks [
           seq_to_insert;
-          trm_add_label "__exit" (trm_unit())]
+          trm_add_label "__exit" (trm_unit())])
       | _ -> 
         let new_decl = trm_let_mut ("__res", ret_ty) (trm_uninitialized ()) in 
-        trm_seq_nomarks [
+        trm_add_mark mark (trm_seq_nomarks [
           new_decl;
           seq_to_insert;
           trm_add_label "__exit" (trm_unit());
           trm_ret (Some (trm_var_get "__res"))
-        ]
+        ])
       end in 
       trm_alter ~desc:(Some (Trm_let_fun (qn, ret_ty, args, new_body))) t
   | _ -> fail t.loc "Apac_core.use_goto_for_return_aux: expected  a target to a function definition."
