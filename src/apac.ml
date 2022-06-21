6f1bdcd8ac58757cb12e7303d51d3507a2a202f5
bbytyqi <begatim.bytyqi@inria.fr>
2022-06-21 10:33:39 +0200

    apac_parallel_task_group

--- src/apac.ml
+++ src/apac.ml
@@ -1,1 +1,11 @@
 include Apac_basic
+
+open Ast
+open Target
+
+
+let parallel_task_group ?(mark : mark = "") : Transfo.t =
+  iter_on_targets ( fun t p -> 
+    Apac_basic.use_goto_for_return ~mark (target_of_path p);
+    List.iter (fun prg -> transfo_on_targets (trm_add_pragma prg) (target_of_path p)) [Taskgroup; Master; Parallel [] ]
+)
---
+++ tests/basic/apac_parallel_task_group.cpp
@@ -0,0 +1,33 @@
+int g(int a, int b) {
+  int __res;
+  {
+    {
+      __res = a + b + a;
+      goto __exit;
+    }
+  }
+__exit:;
+  return __res;
+}
+
+int f() {
+  int __res;
+  {
+    const int x = 3;
+    int y = 5;
+    int z = 3;
+    {
+      __res = g(x, g(y, z));
+      goto __exit;
+    }
+  }
+__exit:;
+  return __res;
+}
+
+void h() {
+  { int a = g(f(), f()); }
+__exit:;
+}
+
+int main() { f(); }
