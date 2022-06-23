dcd3438ce425a72a2c9e5721aa0d017955206ea9
bbytyqi <begatim.bytyqi@inria.fr>
2022-06-23 09:11:00 +0200

    wip bind_taskable

--- src/apac_basic.ml
+++ src/apac_basic.ml
@@ -2,45 +2,65 @@ open Ast
 open Target
 
 
-(* [use_goto_for_return mark]: *)
+(* [use_goto_for_return mark]: expects the target [tg] to point at a function definition,
+    then it will transform the body of that function definition as follows.
+
+    First of all wraps the body of the function into a sequence and marks it with [mark] if 
+    [mark] <> "". Then it considers two cases.
+
+    Case1:
+      Function is of type void:
+        1) Replaces each return statement inside the new sequence with goto __exit
+        2) After the wrapped sequence inserts an empty label "__exit".
+    Case2:
+      Function is returns a value of type [T] then:
+        1) Inserts a declaration "T __res" just befor the introduced sequence.
+        2) Replaces each return statement inside the wrapped sequence with "__res = x; goto __exit"
+        3) Add after the new sequence, adds the labelled statement "__exit; return __res;" *)
 let use_goto_for_return ?(mark : mark = "") : Transfo.t =
   apply_on_targets (Apac_core.use_goto_for_return mark)
 
-let insert_task (sa : sorted_arg_deps) (tg : target) : unit =
-  let deps = [In sa.dep_in; Out sa.dep_out; Inout sa.dep_inout; Outin sa.dep_outin] in 
+
+(* [insert_task sad tg]: expects the target [tg] to be pointing at an insturction or a sequence.
+    Then it will insert an OpenMP pragma at that insturction. *)
+let insert_task (sad : sorted_arg_deps) (tg : target) : unit =
+  let deps = [In sad.dep_in; Out sad.dep_out; Inout sad.dep_inout; Outin sad.dep_outin] in 
   transfo_on_targets (trm_add_pragma (Task [(Ast.Depend deps)])) tg
 
-
-
-type taskable = (var * arg_deps) list  (* var is a mangled name. *)
-
+(* [taskable]: a Hashtable used for storing all the taskable functions. *)
+type taskable = (var , arg_deps) Hashtbl.t
 
 (* Note: Naive implementation. *)
-let identify_taskable_functions (tg : target) : taskable = 
-  let tg_trm = get_trm_at tg in 
+let identify_taskable_functions (tg : target) : taskable =
+  let tg_trm  = get_trm_at tg in 
   match tg_trm with 
   | Some t when trm_is_mainfile t -> 
     begin match trm_seq_inv t with 
     | Some tl -> 
-      Mlist.fold_left (fun acc t -> 
+      let tm = Fun_map.empty in 
+      let ht = Hashtbl.create 100 in 
+      Mlist.folf_left (fun acc t -> 
         match t.desc with 
         | Trm_let_fun (qn, ty, args, body) when qn.qvar_var <> "main" -> 
-          (qn.qvar_var, get_arg_dependencies t) :: acc
+          Hashtbl.add acc f.qvar_var (get_arg_dependencies t)
         | _ -> acc
-       ) [] tl
-    | None -> fail None "Apac_basic.identify_taskable_functions: expected a target to the main file sequence."
-    end
+      ) ht tl
+    | None -> fail None "Apac_basic.identify_taskable_functions:expected a target to the main file sequence."
+    end 
   | _ -> fail None "Apac_basic.identify_taskable_functions: expected a target to the main file sequence."
 
-  
-(* let bind_taskable_calls (tsk : taskable) (tg : target) : unit =
+
+
+let bind_taskable_calls (tsk : taskable) (tg : target) : unit =
   iter_on_transformed_targets (Internal.get_instruction_in_surrounding_sequence)
     (fun i t (path_to_seq, local_path, i1)  -> 
       let path_to_instruction = path_to_seq @ [Dir_seq_nth i1] in
       let path_to_call = path_to_instruction @ local_path in
       let call = Path.get_trm_at_path path_to_call in 
-      match call.desc with 
-      | Trm_apps ()
+      let tg_out_trm = Path.resolvea_path path_to_instruction t in 
+      match tg_out_trm.desc with 
+      | Trm_let _ -> 
+      | Trm_apps (_,|ls; rhs) when is_set_operation tg_out_trm -> 
+      | _ -> Functin.bind (target_of_path path_to_call)
       
-      
-    ) tg *)
+) tg
