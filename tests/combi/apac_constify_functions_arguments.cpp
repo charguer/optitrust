5b82727ce4fcd8466fdf8e4e73d654f7937e6497
Tching Michel <michel.tching@hotmail.fr>
2022-07-12 16:21:20 +0200

    update constify test

--- tests/combi/apac_constify_functions_arguments.cpp
+++ tests/combi/apac_constify_functions_arguments.cpp
@@ -9,32 +9,58 @@ void g(int a[2]) {
     a[f(1)]--;
 }
 
-void h(int &a) {
+void h(int & a) {
     a = 1;
 }
 
-void j(int &a) {
+void j(int & a) {
     h(a);
 }
 
-void k(int *a) {
-    int * b = a;
-    *b++;
+void k(int * a, int * b) {
+    int * c = a;
+    c = b;
+    *c++;
 }
 
-void l1(int &a) {
+void l1(int & a) {
     a += 1;
 }
 
-void l2(int &a) {
+void l2(int & a) {
     l1(a);
 }
 
-void l3(int &a) {
-    l2(a);
+void l3(int & a) {
+    int &b = a;
+    l2(b);
 }
 
 void m(int &a, int * b, int *& c) {
     int * d = a + b;
     d[0] = 1;
+}
+
+int * n1(int * a, int & b) {
+    int * c = a;
+    return c;
+}
+
+int * n2(int * a, int & b) {
+    int * c = a;
+    return n1(c, b);
+}
+
+int & n3(int & a, int& b) {
+    return a;
+}
+
+int & n4(int & a, int & b) {
+    int &c = a;
+    return n3(c, b);
+}
+
+void n5(int * a, int & b, int c) {
+    int * d = n2(a, c);
+    int & e = n4(b, c);
 }
