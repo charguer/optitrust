86af59fc2520c96518fcbb6fff8a7de53b81617c
bbytyqi <begatim.bytyqi@inria.fr>
2022-07-19 11:32:39 +0200

    wip specialize_funcalls

--- src/specialize_core.ml
+++ src/specialize_core.ml
@@ -50,20 +50,68 @@ let choose (select_arg : string list -> int) : Target.Transfo.local =
   Target.apply_on_path (choose_aux select_arg)
 
 
-(* [fundefs_aux spec_name spec_args t]: inserts a copy of the function definition [t], specializing
+(* [fun_defs_aux spec_name spec_args t]: inserts a copy of the function definition [t], specializing
       one of its arguments based on the list [spec_args].
       [spec_name] - the name of the copy
       [spec_args] - an optional list of trms, telling the transformation which argss it shoudl specialize,
       [t] - ast of the function definition. *)
-let fundefs_aux (spec_name : string) (spec_args : (trm option) list) (t : trm) : trm =
+let fun_defs_aux (spec_name : string) (spec_args : (trm option) list) (t : trm) : trm =
   match t.desc with 
   | Trm_let_fun (qf, ret_ty, args, body) -> 
-    let new_def = trm_let_fun spec_name ret_ty args body in 
+    (* Check if spec_args is of the correct shape. *)
+    if List.length spec_args <> List.length args then fail t.loc "Specialize_core.fun_defs_aux: the list of arguments to specialize
+        should match the list of the arguments of the targeted function.";
+    
+    let args_map = List.combine args spec_args in
+
+    let new_args = List.fold_left (fun acc arg -> 
+      match List.assq_opt arg args_map with 
+      | Some v -> 
+        begin match v with 
+        | Some _ -> acc
+        | _ -> arg :: acc
+        end
+      | None -> assert false) [] (List.rev args) in
+   
+    let call_args = List.fold_left (fun acc (arg, ty) ->
+      match List.assq_opt (arg, ty) args_map with 
+      | Some v -> 
+        begin match v with 
+        | Some t1 -> acc
+        | None -> 
+          if is_typ_const ty then trm_var arg :: acc else trm_var_get arg :: acc
+        end 
+      | None -> assert false
+    
+    ) [] (List.rev args) in
+
+    let new_body = trm_seq_nomarks [trm_apps (trm_var ~qvar:qf "") call_args] in
+
+    let new_def = trm_let_fun spec_name ret_ty new_args new_body in 
     
     trm_seq_no_brace [t; new_def]
-  | _ -> fail t.loc "Specialize_core.fundefs_aux: expected a target to a function definition."
+  | _ -> fail t.loc "Specialize_core.fun_defs_aux: expected a target to a function definition."
 
 
-(* [fundefs spec_name spec_args t p]: applies [fundefs_aux] at trm [ŧ] with path [p]. *)
-let fundefs (spec_name : string) (spec_args : (trm option) list) : Target.Transfo.local =
-  apply_on_path (fundefs_aux spec_name spec_args)
+(* [fun_defs spec_name spec_args t p]: applies [fun_defs_aux] at trm [ŧ] with path [p]. *)
+let fun_defs (spec_name : string) (spec_args : (trm option) list) : Target.Transfo.local =
+  apply_on_path (fun_defs_aux spec_name spec_args)
+
+
+(* [fun_calls_aux spec_name args_to_choose t]: replaces the function call [t] with another function call.
+    [spec_name] - the name of the function that appears on the new call,
+    [args_to_choose] - a list of booleans telling the transformation which of the current arguments 
+        from the current call should be kept.*)
+let fun_calls_aux (spec_name : string) (args_to_choose : bool list) (t : trm) : trm =
+  match t.desc with 
+  | Trm_apps (_f, args) -> 
+    let new_args = List.fold_left2 (fun acc b t1 ->
+      if b 
+        then t1 :: acc 
+        else acc) [] args_to_choose args in
+    trm_apps (trm_var spec_name) new_args
+  | _ -> fail t.loc "Specialize_core.fun_calls_aux: expected a target to a function call"
+
+(* [fun_calls spec_name args_to_choose t p]: applies [fun_calls_aux] to the trm [t] with path [p]. *)
+let fun_calls (spec_name : string) (args_to_choose : bool list) : Transfo.local = 
+  apply_on_path (fun_calls_aux spec_name args_to_choose) 
