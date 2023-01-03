aae9b5ffb574f860cb52c65fa4a8f1a67cc2e69d
Thomas Koehler <basta.t.k+git@gmail.com>
2023-01-03 16:44:05 +0000

    fiddle with MM

--- tests/debug/test_debug.cpp
+++ tests/debug/test_debug.cpp
@@ -1,12 +1,58 @@
+// output: m.n.f
+//      a: m.o.f
+//      b: o.n.f
+int mm(float* output, float* a, float* b, int m, int n, int o) {
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) {
+      float sum = 0.0f;
+      for (int k = 0; k < o; k++) {
+        sum += a[k + (o * i)] * b[j + (n * k)];
+      }
 
-int f(int x, int y, int z){
-  return x + y + z;
+      output[(j + (n * i))] = sum;
+    }
+  }
 }
 
-int main(){
+int reduction(float* output, float* input, int n, int m) {
+  for (int i = 0; i < n; i++) {
+    float sum = 0.0f;
+    for (int j = 0; j < m; j++) {
+      sum += input[j + (i * m)];
+    }
+    *output = sum;
+  }
 
- int a;
- a = f(1, 2 ,3);
+  /* target:
 
- return 0;
+  loop hoist;
+  loop fission;
+  loop swap;
+
+  float sum[n];
+  for (int i = 0; i < n; i++) {
+    sum[i] = 0.0f;
+  }
+  for (int j = 0; j < m; j++) {
+    for (int i = 0; i < n; i++) {
+      sum[i] += input[j + (i * m)];
+    }
+  }
+  for (int i = 0; i < n; i++) {
+    *output = sum[i];
+  }
+  */
+}
+
+int main() {
+  /*
+  for (int i = 0; i < 5; i++) {
+    for (int j = 0; j < 10; j++) {
+      for (int k = 0; k < 20; k++) {
+
+      }
+    }
+  }
+  */
+  return 0;
 }
--- tests/debug/test_debug.ml
+++ tests/debug/test_debug.ml
@@ -2,10 +2,87 @@ open Optitrust
 open Target
 open Path
 
-
 let _ = Flags.dump_ast_details := true
 
-let _ = Run.script_cpp (fun _ ->
+let _ = Run.script_cpp (fun () ->
+   (* -- Test Targets -- *)
+   show [ cFunDef "mm" ];
 
-   show_type  [cVar "a"];
+   show [ cFor "i" ];
+   (* nbAny; nbExact 1; *)
+   show [ cFunDef "mm"; cFor "i" ];
+
+   show [ cFor "j" ];
+   show [ cFor "i"; cFor "j" ];
+   (* unexpected outcome: *)
+   (* show [ cForNestedAtDepth 0 ]; *)
+
+   show [ cFor "k" ];
+   show [ cFor "i"; cFor "j"; cFor "k" ];
+
+   (* unexpected outcome: DepthAt 0 ? *)
+   (* show [ cInDepth; cReadVar "a" ]; *)
+   show [ cCellAccess () ];
+   (* not working?: *)
+   show [ cCellRead () ]; show [ cRead () ];
+   show [ cCellWrite () ];
+
+   (* -- reduction rewrites -- *)
+   bigstep "liftReduce1";
+
+   show [cFunDef "reduction"; cVarDef "sum"];
+   !! Loop.hoist [cFunDef "reduction"; cVarDef "sum"];
+   !! Variable.inline [cFunDef "reduction"; cVarDef "sum"];
+   !! Variable.rename ~into:"sum" [cFunDef "reduction"; cVarDef "sum_step"];
+   !! Loop.fission ~split_between:true [cFunDef "reduction"; cFor "i"];
+   !! Loop.swap [cFunDef "reduction"; cFor ~body:[cFor "j"] "i"];
+   (* !! Variable.inline_and_rename *)
+
+   (* -- MM rewrite -- *)
+   bigstep "matrix";
+
+   (* -- Split Loops -- *)
+   !! Loop_basic.tile (lit "32") ~index:"bi" ~bound:TileBoundDivides [cFunDef "mm"; cFor "i"];
+   !! Loop_basic.tile (lit "32") ~index:"bj" ~bound:TileBoundDivides [cFunDef "mm"; cFor "j"];
+   !! Loop_basic.tile (lit "4") ~index:"bk" ~bound:TileBoundDivides [cFunDef "mm"; cFor "k"];
+
+   (* -- Reorder Loops -- *)
+   (* I assume the loop nest is not 'perfect' enough for:
+   - 'blocking'
+   !! Loop.reorder ~order:["bi"; "bj"; "bk"; "i"; "j"; "k"] [cFor "bi"];
+   - 'loop-perm'
+   !! Loop.reorder ~order:["bi"; "bj"; "bk"; "i"; "k"; "j"] [cFor "bi"];
+   *)
+   show [ cFor "bi" ];
+   !! Loop.reorder ~order:["bi"; "bj"; "i"; "j"] [cFunDef "mm"; cFor "bi"];
+
+   (* TODO: hoist_inline helper? *)
+   (* FIXME: hoist bugged? array size + init *)    
+   !! Loop.hoist ~array_size:(Some (expr "32")) [cFunDef "mm"; cVarDef "sum"];
+   !! Loop.hoist ~array_size:(Some (expr "32")) [cFunDef "mm"; cVarDef "sum_step"];
+   !! Instr.delete [cWriteVar "sum_step"];
+   !! Variable.inline [cFunDef "mm"; cVarDef "sum"];
+   !! Variable.inline [cFunDef "mm"; cVarDef "sum_step"];
+   !! Variable.rename ~into:"sum" [cFunDef "mm"; cVarDef "sum_step_step"];
+   !! Loop.fission ~split_between:true [cFunDef "mm"; cFor "j"];
+   !! Loop.fission ~split_between:true [cFunDef "mm"; cFor "i"];
+   (* FIXME: target is wrong? *)
+   show [cFunDef "mm"; cFor ~body:[cFor ~body:[cFor ~body:[cFor "k"] "bk"] "j"] "i"];
+   show [cFunDef "mm"; cFor ~body:[sExpr"+="] "i"];
+   (* !! Loop.reorder ~order:["bk"; "i"; "k"; "j"]
+    [cFunDef "mm"; cFor ~body:[cFor ~body:[cFor ~body:[cFor "k"] "bk"] "j"] "i"]; *)
+   !! Loop.swap [cFunDef "mm"; cFor "i"; cFor ~body:[sExpr"+="] "j"];
+   !! Loop.swap [cFunDef "mm"; cFor ~body:[sExpr"+="] "i"];
+   !! Loop.swap [cFunDef "mm"; cFor ~body:[sExpr"+="] "j"];
+
+   (* -- Vectorize -- *)
+   (* OMP pragma *)
+
+   (* -- Array Packing -- *)
+   (* Store Temporary Array (alloc + copy loops) *)
+
+   (* -- Loop Unroll -- *)
+
+   (* -- Parallelize -- *)
+   (* OMP pragma *)
 )
