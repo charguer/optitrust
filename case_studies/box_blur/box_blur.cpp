79ef128e3cb83bd3ff473a3486eeee11b0b3bb0c
thomas <Thomas Koehler>
2023-01-25 15:44:07 +0100

    more box blur

--- case_studies/box_blur/box_blur.cpp
+++ case_studies/box_blur/box_blur.cpp
@@ -3,30 +3,59 @@
 
 // output: (height-2).(width-2).float
 // input :     height.    width.float
-void blur(float* output, float* input, int width, int height) {
-  float* tmp; //[(width - 2) * height];
-
-  for (int x = 0; x < (width - 1); x++) {
-    for (int y = 0; y < height; y++) {
-      tmp[x + (width - 2)*y] = (
-       input[x - 1 + width*y] +
-       input[x - 0 + width*y] +
-       input[x + 1 + width*y]
-      ) / 3.0f;
-    }
-  }
-
+void blur_fused(float* output, float* input, int width, int height) {
   for (int x = 0; x < (width - 2); x++) {
-    for (int y = 0; y < height; y++) {
+    for (int y = 0; y < (height - 2); y++) {
+      float tmp[3];
+
+      for (int k = 0; k < 3; k++) {
+        tmp[k] = (
+          input[x + 0 + width*(y + k)] +
+          input[x + 1 + width*(y + k)] +
+          input[x + 2 + width*(y + k)]
+        ) / 3.0f;
+      }
+
       output[x + (width - 2)*y] = (
-       input[x + (width - 2)*(y - 1)] +
-       input[x + (width - 2)*(y + 0)] +
-       input[x + (width - 2)*(y + 1)]
+       tmp[0] + tmp[1] + tmp[2]
       ) / 3.0f;
     }
   }
 }
 
+// output: (height-2).(width-2).float
+// input :     height.    width.float
+void blur(float* output, float* input, int width, int height) {
+  float* tmp; // MALLOC2(height, width - 2, sizeof(float));
+
+  for (int x = 0; x < (width - 2); x++) {
+    for (int y = 0; y < height; y++) {
+      tmp[x + (width - 2)*y] = (
+       input[(x + 0) + width*y] +
+       input[(x + 1) + width*y] +
+       input[(x + 2) + width*y]
+      ) / 3.0f;
+    }
+  }
+
+  // fuse:
+  // tmp[x + (width - 2)*y] ~= MINDEX2(height, width - 2, y, x)
+  //  -->
+  // tmp2[y] ~= MINDEX1(width - 2, y)
+  //  with tmp2 : [width - 2] ~= MALLOC1(width - 2, sizeof(float))
+
+  for (int x = 0; x < (width - 2); x++) {
+    for (int y = 0; y < height; y++) {
+      output[x + (width - 2)*y] = (
+       tmp[x + (width - 2)*(y + 0)] +
+       tmp[x + (width - 2)*(y + 1)] +
+       tmp[x + (width - 2)*(y + 2)]
+      ) / 3.0f;
+    }
+  }
+}
+
+/*
 int main() {
   const int W = 1024;
   const int H = 1024;
@@ -45,3 +74,4 @@ int main() {
 
   return 0;
 }
+*/
--- case_studies/box_blur/box_blur.ml
+++ case_studies/box_blur/box_blur.ml
@@ -10,9 +10,18 @@ let foreach (_doc : string) (l : 'a list) (f: 'a -> unit) : unit =
   List.iter f l
 
 let _ = Run.script_cpp (fun () ->
+  let lines_per_thread = 32 in
+  let vector_size = 8 in
+
   bigstep "---- reorder loops ----";
 
   !! Loop.reorder ~order:["y"; "x"] [nbMulti; cFunDef "blur"; cFor "x"];
+
+  !! Loop.fusion [occFirst; cFunDef "blur"; cFor "y"];
+
+  !! Loop.tile (lit (string_of_int vector_size)) ~index:"bx" [nbMulti; cFunDef "blur"; cFor "x"];
+
+  !! Loop.tile (lit (string_of_int lines_per_thread)) ~index:"by" [nbMulti; cFunDef "blur"; cFor "y"];
 
   bigstep "---- introduce vector and thread parallelism ----";
 
@@ -22,4 +31,6 @@ let _ = Run.script_cpp (fun () ->
 
   (* -- Multi-thread -- *)
   !! Omp.parallel_for [cFunDef "blur"; cFor "by"];
+
+  !!! ();
 )
