99d2daef5a3ac9df5ac151ad1d2fda35db14542c
FELŠÖCI Marek <marek.felsoci@inria.fr>
2023-05-10 18:44:51 +0200

    Add Guix software environment specifications

---
+++ .guix/channels.scm
@@ -0,0 +1,16 @@
+(list
+ (channel
+  (name 'guix)
+  (url "https://git.savannah.gnu.org/git/guix.git")
+  (branch "master")
+  (commit "fe17b667d98e7a0a17231947ec505be0f41f9300")
+  (introduction
+   (make-channel-introduction
+    "9edb3f66fd807b096b48283debdcddccfea34bad"
+    (openpgp-fingerprint
+     "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+ (channel
+  (name 'guix-extra)
+  (url "https://gitlab.inria.fr/mfelsoci/guix-extra.git")
+  (branch "master")
+  (commit "0db4a2fb39f7140ebdd607f8c4225358e47bf691")))
