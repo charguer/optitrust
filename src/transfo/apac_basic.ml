5c821349ef920918b9c13f439e385b712ec21fef
FELŠÖCI Marek <marek.felsoci@inria.fr>
2023-05-11 15:37:30 +0200

    Use new API for use_goto_for_return

--- src/transfo/apac_basic.ml
+++ src/transfo/apac_basic.ml
@@ -1,25 +1,58 @@
 open Ast
 open Target
+open Path
 include Apac_core
 
-(* [use_goto_for_return mark]: expects the target [tg] to point at a function definition,
-    then it will transform the body of that function definition as follows.
+(* [use_goto_for_return_aux mark t]: transforms the body of the funciton
+      declaration in such a way that all return statements are replaced with
+      gotos,
+    [mark] - mark used to mark the introduced sequence.
+    [t] - ast of the function definition. *)
+let use_goto_for_return_aux (mark : mark) (t : trm) : trm =
+  match t.desc with
+  | Trm_let_fun (qn, ret_ty, args, body) ->
+    let seq_to_insert, _ = Internal.replace_return_with_assign ~check_terminal:false ~exit_label:"__exit" "__res" body in
+    let seq_to_insert = trm_seq_add_last (trm_add_label "__exit" (trm_unit())) seq_to_insert in
+    let new_body =
+      begin match ret_ty.typ_desc with
+      | Typ_unit ->
+        trm_add_mark mark (trm_seq_nomarks [seq_to_insert;])
+      | _ ->
+        let new_decl = trm_let_mut ("__res", ret_ty) (trm_uninitialized ()) in
+        trm_add_mark mark (trm_seq_nomarks [
+          new_decl;
+          seq_to_insert;
+          trm_ret (Some (trm_var_get "__res"))
+        ])
+      end in
+      trm_alter ~desc:(Trm_let_fun (qn, ret_ty, args, new_body)) t
+  | _ -> fail t.loc "Apac_core.use_goto_for_return_aux: expected a target to a function definition."
 
-    First of all wraps the body of the function into a sequence and marks it with [mark] if 
-    [mark] <> "". Then it considers two cases.
+(* [use_goto_for_return mark]: expects the target [tg] to point at a function
+    definition, then it will transform the body of that function definition as
+    follows.
+
+    First of all wraps the body of the function into a sequence and marks it
+    with [mark] if [mark] <> "". Then it considers two cases.
 
     Case1:
       Function is of type void:
-        1) Replaces each return statement inside the new sequence with goto __exit
+        1) Replaces each return statement inside the new sequence with
+           goto __exit.
         2) After the wrapped sequence inserts an empty label "__exit".
     Case2:
       Function is returns a value of type [T] then:
         1) Inserts a declaration "T __res" just befor the introduced sequence.
-        2) Replaces each return statement inside the wrapped sequence with "__res = x; goto __exit"
-        3) Add after the new sequence, adds the labelled statement "__exit; return __res;" *)
+        2) Replaces each return statement inside the wrapped sequence with
+           "__res = x; goto __exit".
+        3) Add after the new sequence, adds the labelled statement
+           "__exit; return __res;". *)
 let use_goto_for_return ?(mark : mark = "") (tg : target) : unit =
-  Internal.nobrace_remove_after (fun _ -> 
-    apply_on_targets (Apac_core.use_goto_for_return mark) tg)
+  Internal.nobrace_remove_after (fun _ ->
+    Target.apply(fun t p ->
+      Path.apply_on_path(use_goto_for_return_aux mark) t p
+    ) tg
+  )
   
 (* [constify_args ~is_const tg]: expect the target [tg] to point at a function definition.
    Then it will add the "const" keyword where is it possible in the type of the argument. 
--- src/transfo/apac_core.ml
+++ src/transfo/apac_core.ml
@@ -1,37 +1,6 @@
 open Ast
 open Target
 
-
-(* [use_goto_for_return_aux mark t]: transforms the body of the funciton declaration in such a way that
-      all return statements are replaced with gotos,
-    [mark] - mark used to mark the introduced sequence.
-    [t] - ast of the function definition. *)
-let use_goto_for_return_aux (mark : mark) (t : trm) : trm =
-  match t.desc with
-  | Trm_let_fun (qn, ret_ty, args, body, contract) ->
-    let seq_to_insert, _ = Internal.replace_return_with_assign ~check_terminal:false ~exit_label:"__exit" "__res" body in
-    let seq_to_insert = trm_seq_add_last (trm_add_label "__exit" (trm_unit())) seq_to_insert in
-    let new_body =
-      begin match ret_ty.typ_desc with
-      | Typ_unit ->
-        trm_add_mark mark (trm_seq_nomarks [seq_to_insert;])
-      | _ ->
-        let new_decl = trm_let_mut ("__res", ret_ty) (trm_uninitialized ()) in
-        trm_add_mark mark (trm_seq_nomarks [
-          new_decl;
-          seq_to_insert;
-          trm_ret (Some (trm_var_get "__res"))
-        ])
-      end in
-      trm_alter ~desc:(Trm_let_fun (qn, ret_ty, args, new_body, contract)) t
-  | _ -> fail t.loc "Apac_core.use_goto_for_return_aux: expected  a target to a function definition."
-
-
-(* [use_goto_for_return mark t p]: applies [use_goto_for_return_aux] at the trm [t] with path [p]. *)
-let use_goto_for_return (mark : mark) : Transfo.local =
-  apply_on_path(use_goto_for_return_aux mark)
-
-
 (* [is_typdef_alias ty]: checks if [ty] is a defined type alias. *)
 let is_typdef_alias (ty : typ) : bool =
   match ty.typ_desc with
