9fa4f0b5822c06456d1b2d9db460a40d0ee6693d
Guillaume Bertholon <guillaume.bertholon@ens.fr>
2023-05-15 15:39:03 +0200

    Slightly improve build performance for IDE <F6>

--- tools/view_result.sh
+++ tools/view_result.sh
@@ -36,16 +36,25 @@ ${TOOLS_FOLDER}/add_lines.sh ${FILEBASE}.ml ${FILEBASE}_with_lines.ml
 
 # LATER: add_exit should also introduce special commands for figuring out the line of the command that executes
 
+# Using only one dune command that builds the project is faster than using two.
+# Therefore we also force the runner to be up to date at this step.
+# FIXME: This will not work if the script is used outside an optitrust buildtree.
+# We should find a way to detect if we are.
+
 PROG="${FILEBASE}_with_lines.cmxs"
-mv dune dune.bak && trap 'mv dune.bak dune' EXIT || true
+mv dune dune.bak 2>/dev/null || true
 echo "(executable
   (name ${FILEBASE}_with_lines)
   (modules ${FILEBASE}_with_lines)
   (modes (native plugin))
   (promote)
-  (libraries optitrust))" > dune
-dune build ${PROG}
-rm dune
+  (libraries optitrust))
+
+(alias
+  (name default)
+  (deps ${PROG} %{project_root}/runner/optitrust_runner.exe))" > dune
+trap 'mv dune.bak dune 2>/dev/null || rm dune' EXIT
+dune build
 
 # Third, we execute the transformation program, obtain "${FILEBASE}_before.cpp" and "${FILEBASE}_after.cpp, unless mode is view_trace
 
@@ -58,10 +67,8 @@ else
   fi
 fi
 
-# For that run, we activate the backtrace
-RUNNER="dune exec optitrust_runner --"
-echo "OCAMLRUNPARAM=b ${RUNNER} ${PROG} ${OPTIONS} ${FLAGS}"
-OCAMLRUNPARAM=b ${RUNNER} ${PROG} ${OPTIONS} ${FLAGS}
+# FIXME: Will not work if we are outside the optitrust buildtree either
+OCAMLRUNPARAM=b dune exec --no-build optitrust_runner -- ${PROG} ${OPTIONS} ${FLAGS}
 
 # Fourth, we vizualize a result or a diff or a trace
 
