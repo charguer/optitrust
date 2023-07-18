2ccf19ad5dbe8d6921b5681896f07fdfc7b51e42
Guillaume Bertholon <guillaume.bertholon@ens.fr>
2023-07-18 16:30:23 +0200

    Check resources in Omp.parallel on the loop_contract example

--- tests/resources/loop_contracts.ml
+++ tests/resources/loop_contracts.ml
@@ -6,4 +6,9 @@ let _ = Flags.resource_errors_as_warnings := true
 
 let _ = Run.script_cpp (fun () ->
   show_res ();
+
+  !! Trace.failure_expected (fun () ->
+    Omp.parallel [cFunDef "array_copy_explicit"; cFor "i"]);
+
+  !! Omp.parallel [cFunDef "array_copy_par"; cFor "i"];
 )
