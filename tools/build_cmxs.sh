cb0c7db806ec0794d7d6c8674d53071c65478e75
thomas <Thomas Koehler>
2023-07-20 14:48:13 +0200

    build_cmxs.sh in tmp dir to keep local dune files

--- tools/build_cmxs.sh
+++ tools/build_cmxs.sh
@@ -2,24 +2,30 @@
 set -euo pipefail
 
 # builds the .cmxs file from file $1 (.ml)
+FILE=$1
+FILEBASE=${1%.ml}
 
 # Using only one dune command that builds the project is faster than using two.
 # Therefore we also force the runner to be up to date at this step.
 # FIXME: This will not work if the script is used outside an optitrust buildtree.
 # We should find a way to detect if we are.
 
-FILEBASE=${1%.ml}
-mv dune dune.bak 2>/dev/null || true
+# Path to the tools and optitrust folder
+TOOLS_FOLDER=$(dirname -- "$(readlink -f -- "$0";)")
+OPTITRUST_FOLDER="${TOOLS_FOLDER}/.."
+
+TMPDIR=${OPTITRUST_FOLDER}/tmp
+mkdir -p ${TMPDIR}
+ln -sf "$(readlink -f -- "${FILE}";)" ${TMPDIR}/main.ml
 echo "(executable
-  (name ${FILEBASE})
-  (modules ${FILEBASE})
+  (name main)
+  (modules main)
   (modes (native plugin))
   (promote)
   (preprocess (pps ppx_transfo))
-  (libraries optitrust))
-
-(alias
-  (name default)
-  (deps ${FILEBASE}.cmxs %{project_root}/runner/optitrust_runner.exe))" > dune
-trap 'mv dune.bak dune 2>/dev/null || rm dune' EXIT
-dune build
+  (libraries optitrust))" > ${TMPDIR}/dune
+pushd ${OPTITRUST_FOLDER} > /dev/null
+dune build tmp/main.cmxs runner/optitrust_runner.exe
+popd > /dev/null
+mv ${TMPDIR}/main.cmxs ${FILEBASE}.cmxs
+rm -r ${TMPDIR}
