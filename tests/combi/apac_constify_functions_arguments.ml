18d3ccbfa40fab82d3020efa88a59591540cee71
FELŠÖCI Marek <marek.felsoci@inria.fr>
2023-08-03 15:50:29 +0200

    Update constification test script

--- tests/combi/apac_constify_functions_arguments.ml
+++ tests/combi/apac_constify_functions_arguments.ml
@@ -6,6 +6,8 @@ let _ = Run.script_cpp (fun () ->
   !! Apac_basic.const_lookup_candidates [cFunDef ""];
 
   !! Apac_basic.const_compute_all [cFunDef ""];
+
+  Apac_basic.unconst_and_const ();
   
   !! Apac.constify_functions_arguments Apac_basic.cstfbl [cFunDefAndDecl ""];
 
