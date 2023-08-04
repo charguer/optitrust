3f6d76e766eac539293e22e9ad1f3e329f2c874b
FELŠÖCI Marek <marek.felsoci@inria.fr>
2023-08-04 15:12:53 +0200

    Fix constification test

--- tests/combi/apac_constify_functions_arguments.ml
+++ tests/combi/apac_constify_functions_arguments.ml
@@ -3,12 +3,12 @@ open Target
 
 let _ = Run.script_cpp (fun () -> 
 
-  !! Apac_basic.const_lookup_candidates [cFunDef ""];
+  !! Apac_basic.const_lookup_candidates [nbAny; cFunDefAndDecl ""];
 
-  !! Apac_basic.const_compute_all [cFunDef ""];
+  !! Apac_basic.const_compute_all [nbAny; cFunDefAndDecl ""];
 
   Apac_basic.unconst_and_const ();
   
-  !! Apac.constify_functions_arguments Apac_basic.cstfbl [cFunDefAndDecl ""];
+           !! Apac.constify_functions_arguments Apac_basic.cstfbl [nbAny; cFunDefAndDecl ""];
 
 )
