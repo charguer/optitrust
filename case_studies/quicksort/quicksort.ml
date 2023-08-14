90a930af14e8065fdd4c0224a479d35a429a26a9
FELŠÖCI Marek <marek.felsoci@inria.fr>
2023-08-14 19:16:56 +0200

    Perform only constification in quicksort test for now

--- case_studies/quicksort/quicksort.ml
+++ case_studies/quicksort/quicksort.ml
@@ -1,23 +1,22 @@
 open Optitrust
 open Target
-
+open Ast
 
 let _ = Run.script_cpp (fun () ->
-  let t = get_ast() in
-  Ast_data.fill_fun_defs_tbl t;
-
 (******************************************************************************)
 (*                                Pre-processing                              *)
 (******************************************************************************)
-
-  (* constify arguments *)
-  let cstfbl = Apac.identify_constifiable_functions [] in
-  !! Apac.constify_functions_arguments cstfbl [nbAny; cFunDef ""];
-
+  bigstep "constification";
+  let target = [nbAny; cFunDefAndDecl ""] in
+              !! Apac_basic.const_lookup_candidates target;
+              !! Apac_basic.const_compute_all target;
+              !! Apac_basic.unconst ();
+              !! Apac_basic.constify_args target;
+              !! Apac_basic.constify_aliases target;
 (******************************************************************************)
 (*                                Processing                                  *)
 (******************************************************************************)
-  !! Apac.mark_taskable_function "taskable" [nbAny; cFunDef ""];
+  (*!! Apac.mark_taskable_function "taskable" [nbAny; cFunDef ""];
 
   (* unfold call taskable *)
   Apac.unfold_funcalls [
@@ -39,5 +38,5 @@ let _ = Run.script_cpp (fun () ->
   (* adds taskgroup + transforms return to goto *)
   !! Apac.parallel_task_group [nbAny; cMark "taskable"];
 
-  !! Marks.remove "taskable" [nbAny; cMark "taskable"];
+  !! Marks.remove "taskable" [nbAny; cMark "taskable"];*)
 )
