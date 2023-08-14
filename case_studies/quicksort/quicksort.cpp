2972055101f5ede70bdae754903dc8caf8386db3
FELŠÖCI Marek <marek.felsoci@inria.fr>
2023-08-14 19:16:36 +0200

    Use C implementation of quicksort

--- case_studies/quicksort/quicksort.cpp
+++ case_studies/quicksort/quicksort.cpp
@@ -1,87 +1,91 @@
-#include <random>
-#include <iostream>
-#include <stdexcept>
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define SIZE 100000000
 
-class Quicksort{
-    static void Partition (int* outPivot, int arr[], const int rightLimit)
-    {
-        const int pivot = arr[rightLimit];
-        int idxLeft = (0 - 1);
-        for (int idxIter = 0; idxIter <= rightLimit - 1; idxIter++){
-            if (arr[idxIter] < pivot)
-            {
-                idxLeft += 1;
-                std::swap(arr[idxLeft], arr[idxIter]);
-            }
-        }
-        std::swap(arr[idxLeft + 1], arr[rightLimit]);
-        *outPivot = (idxLeft + 1);
+void swap(int * a, int * b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+void init(int * arr) {
+  for(int idx = 0; idx < SIZE; idx++) {
+    arr[idx] = rand();
+  }
+}
+
+void Partition(int * outPivot, int * arr, int rightLimit) {
+  int pivot = arr[rightLimit - 1];
+  int idxLeft = -1;
+  int idxIter;
+
+  for(idxIter = 0; idxIter < rightLimit - 1; idxIter++) {
+    if(arr[idxIter] < pivot) {
+      idxLeft++;
+      swap(&arr[idxLeft], &arr[idxIter]);
     }
+  }
+  
+  swap(&arr[idxLeft + 1], &arr[rightLimit - 1]);
+  *outPivot = (idxLeft + 1);
+}
 
-
-
-    static void InsertionSort(int arr[], const int rightLimit)
-    {
-        for(int idx = 0 ; idx < rightLimit; ++idx){
-            int idxMin = idx;
-            for(int idxIter = idxMin + 1 ; idxIter <= rightLimit ; ++idxIter){
-                if(arr[idxMin] > arr[idxIter]){
-                    idxMin = idxIter;
-                }
-            }
-            int tmp = arr[idx];
-            arr[idx] = arr[idxMin];
-            arr[idxMin] = tmp;
-        }
+void InsertionSort(int * arr, int rightLimit) {
+  for(int idx = 0; idx < rightLimit - 1; ++idx) {
+    int idxMin = idx;
+    int idxIter;
+    for(idxIter = idxMin + 1; idxIter < rightLimit; ++idxIter) {
+      if(arr[idxMin] > arr[idxIter]) {
+        idxMin = idxIter;
+      }
     }
-
-    static void SortCore(int inOutData[], const int rightLimit){
-        if (0 >= rightLimit)
-        {
-            return;
-        }
-        if(rightLimit<=256){
-            InsertionSort(inOutData, rightLimit);
-        }
-        else{
-            int pivot;
-
-            Partition(&pivot, inOutData, rightLimit);
-
-            SortCore(&inOutData[0], pivot - 1);
-            SortCore(&inOutData[pivot + 1], rightLimit -(pivot + 1));
-        }
-    }
-
-public:
-    static void Sort(int inOutData[], const int inSize){
-        SortCore(inOutData, inSize);
-    }
-};
-
-
+    swap(&arr[idx], &arr[idxMin]);
+  }
+}
+  
+void SortCore(int * inOutData, int rightLimit) {
+  if (0 >= rightLimit) {
+    return;
+  }
+  
+  if(rightLimit <= 256) {
+    InsertionSort(inOutData, rightLimit);
+  } else {
+    int pivot;
+    Partition(&pivot, inOutData, rightLimit);
+    SortCore(&inOutData[0], pivot);
+    SortCore(&inOutData[pivot + 1], rightLimit - (pivot + 1));
+  }
+}
+  
+void Sort(int * inOutData, int inSize) {
+  SortCore(inOutData, inSize);
+}
 
 int main(){
-    const int Size = 100000000;
-    std::vector<int> data(Size);
+  int * data = (int *) malloc(SIZE * sizeof(int));
 
-    std::mt19937 gen(0);
-    std::uniform_int_distribution<> dis(1, Size);
+  if(!data) {
+    printf("Error while allocating data array!\n");
+    return 1;
+  }
+  
+  srand(time(NULL));
+  
+  init(data);
 
-    for(int idx = 0 ; idx < Size ; ++idx){
-        data[idx] = dis(gen);
+  Sort(data, SIZE);
+  
+  int idx;
+  for(idx = 1; idx < SIZE; idx++) {
+    if(data[idx - 1] > data[idx]){
+      printf("Array is not sorted!\n");
+      free(data);
+      return 1;
     }
+  }
 
-    Quicksort::Sort(data.data(), int(data.size()));
-
-    for(int idx = 1 ; idx < Size ; ++idx){
-        if(data[idx-1] > data[idx]){
-            throw std::runtime_error("Array is not sorted!");
-        }
-    }
-
-    // std::cout << "Done" << std::endl;
-
-    return 0;
+  free(data);
+  return 0;
 }
