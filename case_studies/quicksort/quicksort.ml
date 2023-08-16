a3e739c7aa6436e90c589399a4f39cde320175ea
FELŠÖCI Marek <marek.felsoci@inria.fr>
2023-08-16 20:22:24 +0200

    Update constification tests

--- case_studies/quicksort/quicksort.ml
+++ case_studies/quicksort/quicksort.ml
@@ -6,13 +6,7 @@ let _ = Run.script_cpp (fun () ->
 (******************************************************************************)
 (*                                Pre-processing                              *)
 (******************************************************************************)
-  bigstep "constification";
-  let target = [nbAny; cFunDefAndDecl ""] in
-              !! Apac_basic.const_lookup_candidates target;
-              !! Apac_basic.const_compute_all target;
-              !! Apac_basic.unconst ();
-              !! Apac_basic.constify_args target;
-              !! Apac_basic.constify_aliases target;
+  !! Apac.constify [nbAny; cFunDefAndDecl ""];
 (******************************************************************************)
 (*                                Processing                                  *)
 (******************************************************************************)
