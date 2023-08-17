0957843d113cde758fdca0049bf46e945033a832
Michel Weber <michel.weber@inf.ethz.ch>
2023-08-17 16:16:35 +0200

    try to show

--- case_studies/floyd_warshall/floyd_warshall.ml
+++ case_studies/floyd_warshall/floyd_warshall.ml
@@ -10,29 +10,31 @@ let _ = Run.script_cpp (fun () ->
 
   (* Store the values in the kth-row needed to compute distances in a contiguous manner *)
   !! Loop.hoist_expr ~dest:[tBefore; cFor "i" ] "kj" ~indep:["i"] [occIndex 1; cArrayRead "A"];
+  !! show [cVarDef "kj"];
+  (*!! Loop.hoist_alloc_loop_list [0] [cVarDef "kj"];*)
   (* Update them properly when i == k *)
-  !! Variable.insert ~name:"dummy" ~value:(lit "42") [tAfter; cArrayWrite "A"];
+  (*!! Variable.insert ~name:"dummy" ~value:(lit "42") [tAfter; cArrayWrite "A"];
   !! Flow_basic.insert_if ~cond:(expr "i == k") ~no_else_branch:true [cVarDef "dummy"];
-  !! Expr_basic.replace (stmt "kj[j] = sum;") [cVarDef "dummy"];
+  !! Expr_basic.replace (stmt "kj[j] = sum;") [cVarDef "dummy"];*)
 
   (* Do the same for the k-th column *)
-  !!! Loop.hoist_expr ~dest:[occIndex 0; tAfter; cFor "j"] "ik" ~indep:["j"] [cFor "i"; occIndex 0; cArrayRead "A"];
+  (*!!! Loop.hoist_expr ~dest:[occIndex 0; tAfter; cFor "j"] "ik" ~indep:["j"] [cFor "i"; occIndex 0; cArrayRead "A"];
   !! Variable.insert ~name:"dummy" ~value:(lit "42") [tAfter; cArrayWrite "A"];
   !! Flow_basic.insert_if ~cond:(expr "j == k") ~no_else_branch:true [cVarDef "dummy"];
-  !! Expr_basic.replace (stmt "ik[i] = sum;") [cVarDef "dummy"];
+  !! Expr_basic.replace (stmt "ik[i] = sum;") [cVarDef "dummy"];*)
 
   (*!! Types.replace "auto*" "uint32_t*" [cVarDef "ik"];*)
   (*!! Loop_basic.move_out [cVarDef "kj"];*)
 
   (* Split ranges, such that the ifs can be removed *)
-  !! Loop_basic.split_range ~cut:(expr "k") [occIndex 1; cFor "i"];
+  (*!! Loop_basic.split_range ~cut:(expr "k") [occIndex 1; cFor "i"];
   !! Sequence_basic.delete [occIndex 1; cIf ()];(*~cond:(expr "i == k") ()];*)
 
   !! Loop_basic.split_range ~cut:(expr "k+1") [occIndex 2; cFor "i"];
 
   !! Sequence_basic.intro ~mark:"k=i-loop" 1 [occIndex 2; cFor "i"];
   !! Loop.isolate_first_iteration [cMark "k=i-loop"; cFor "i"];
-  !! Sequence_basic.delete [cMark "k=i-loop"; cFor "i"];
+  !! Sequence_basic.delete [cMark "k=i-loop"; cFor "i"];*)
   (*!! Expr_basic.replace (stmt "ik[k] = sum;") [cMark "j==k-if"];*)
 
   (*!! Sequence_basic.elim [cMark "k=i-loop"];*)
