df50e07b4db0d7eef34a463d35c18c794cb7dcf1
FELŠÖCI Marek <marek.felsoci@inria.fr>
2023-08-29 12:08:18 +0200

    Mark support for unique IDs of anonymous arguments as TODO

--- tests/basic/apac_constify_args.cpp
+++ tests/basic/apac_constify_args.cpp
@@ -8,6 +8,7 @@ void e(const int a) {}
 
 void f(int a) {}
 
+// TODO: Be able to use anonymous arguments, e.g. void g(float *, double &, ...)
 void g(float * a, double & b, int const **& c) {}
 
 void h(pINT a, ppINT b) {}
