d7bf38455713a2c62931e5768876d0a19fcf283a
FELŠÖCI Marek <marek.felsoci@inria.fr>
2023-08-31 19:16:52 +0200

    Simplify targeting in basic heapification test

--- tests/basic/apac_heapify.ml
+++ tests/basic/apac_heapify.ml
@@ -2,31 +2,12 @@ open Optitrust
 open Target 
 
 let _ = Run.script_cpp (fun () ->
-
-  !! Apac_basic.heapify [cVarDef "a" ];
-
-  !! Apac_basic.heapify [cVarDef "b" ];
-
-  !! Apac_basic.heapify [cVarDef "c" ];
-
-  !! Apac_basic.heapify [cVarDef "d" ];
-
-  !! Apac_basic.heapify [cVarDef "e" ];
-
-  !! Apac_basic.heapify [cVarDef "f" ];
-  
-  !! Apac_basic.heapify [cVarDef "g" ];
-  
-  !! Apac_basic.heapify [cVarDef "h" ];
-
-  !! Apac_basic.heapify [cVarDef "j" ];
-
-  !! Apac_basic.heapify [nbAny; cVarsDef "" ];
-
-  !! Apac_basic.heapify [cVarDef "ag" ];
-
-  !! Apac_basic.heapify [cVarDef "ah" ];
-
-  !! Apac_basic.heapify [cVarDef "pg" ];
-
-)
+            (* Target all of the simple variable declarations except for the
+               declarations of [i] and [x] (used only for testing purposes). *)
+            !! Apac_basic.heapify [
+                nbAny;
+                cDiff [[cDiff [[cVarDef ""]] [[cVarDef "i"]]]] [[cVarDef "x"]]
+              ];
+            (* Target all of the multiple variable declarations. *)
+            !! Apac_basic.heapify [nbAny; cVarsDef "" ];
+          )
