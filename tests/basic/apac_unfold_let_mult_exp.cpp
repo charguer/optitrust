4f5f95238178bc8eb493e6b7a2e7ec0b9752ce6e
FELŠÖCI Marek <marek.felsoci@inria.fr>
2023-09-07 17:01:54 +0200

    Use full apac_unfold_let_mult test
    The problems preventing us from processing some of the tested expressions were fixed.

--- tests/basic/apac_unfold_let_mult_exp.cpp
+++ tests/basic/apac_unfold_let_mult_exp.cpp
@@ -5,8 +5,10 @@ int f(int arg, int* argp) {
   int& c = i;
   int d[5] = {1, 2, 3, 4, 5};
   int* e = &i;
+  int* f = &a;
   int* g = e;
   int* const h = &i;
+  int* const j = &a;
   int* const k = e;
   const int l = 1;
   const int m = i;
@@ -14,7 +16,13 @@ int f(int arg, int* argp) {
   const int& o = i;
   const int p[5] = {1, 2, 3, 4, 5};
   const int* q = &i;
+  const int* r = &l;
   const int* s = q;
   const int* const t = &i;
+  const int* const u = &l;
   const int* const v = q;
+  int w = arg;
+  int* x = &arg;
+  int* y = argp;
+  int z = *argp;
 }
