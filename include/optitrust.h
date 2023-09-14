11cf6c5ef891a82b95fb015c605bc3ab982aa3cd
Guillaume Bertholon <guillaume.bertholon@ens.fr>
2023-09-14 13:20:14 +0200

    WIP: optitrust.h with new ghost syntax
    This commit breaks a lot of tests since we do not handle the new ghost syntax in the code.

--- include/optitrust.h
+++ include/optitrust.h
@@ -6,138 +6,150 @@
 
 /* ---- Resource Annotations ---- */
 
-void __pure() {}
-void __requires(const char*) {}
-void __ensures(const char*) {}
-void __invariant(const char*) {}
-void __reads(const char*) {}
-void __modifies(const char*) {}
-void __consumes(const char*) {}
-void __produces(const char*) {}
-void __sequentially_reads(const char*) {}
-void __sequentially_modifies(const char*) {}
+inline void __pure() {}
+inline void __requires(const char*) {}
+inline void __ensures(const char*) {}
+inline void __invariant(const char*) {}
+inline void __reads(const char*) {}
+inline void __modifies(const char*) {}
+inline void __consumes(const char*) {}
+inline void __produces(const char*) {}
+inline void __sequentially_reads(const char*) {}
+inline void __sequentially_modifies(const char*) {}
 
-void __admitted() {}
+inline void __admitted() {}
 
-typedef void __ghost;
+/* ---- Ghost annotations ---- */
 
-template<typename T> T __ghost_args(T ret_val, const char*) { return ret_val; }
+// Return type for ghost functions
+typedef void __ghost_ret;
+
+// Argument type for ghost functions
+typedef const char* __ghost_args;
+
+// Marcro for ghost function prototype
+#define __GHOST(f) inline __ghost_ret f(__ghost_args)
+
+
+/// Postfix call for specifying ghost arguments
+inline void __with(__ghost_args) {}
+template<typename T> T __call_with(T ret_val, __ghost_args) { return ret_val; }
 /*
-template<typename T> T __ghost_bind(T ret_val, const char*) { return ret_val; }
-void __res_rename(const char*) {}
+template<typename T> T __bind(T ret_val, const char*) { return ret_val; }
+inline void __rename(const char*) {}
 */
 
 /* ---- Matrix Functions ---- */
 
-int MINDEX0() {
+inline int MINDEX0() {
   __pure();
   __admitted();
   return 0;
 }
 
-int MINDEX1(int N1, int i1) {
+inline int MINDEX1(int N1, int i1) {
   __pure();
   __admitted();
   return i1;
 }
 
-int MINDEX2(int N1, int N2, int i1, int i2) {
+inline int MINDEX2(int N1, int N2, int i1, int i2) {
   __pure();
   __admitted();
   return i1 * N2 + i2;
 }
 
-int MINDEX3(int N1, int N2, int N3, int i1, int i2, int i3) {
+inline int MINDEX3(int N1, int N2, int N3, int i1, int i2, int i3) {
   __pure();
   __admitted();
   return i1 * N2 * N3 + i2 * N3 + i3;
 }
 
-int MINDEX4(int N1, int N2, int N3, int N4, int i1, int i2, int i3, int i4) {
+inline int MINDEX4(int N1, int N2, int N3, int N4, int i1, int i2, int i3, int i4) {
   __pure();
   __admitted();
   return i1 * N2 * N3 * N4 + i2 * N3 * N4 + i3 * N4 + i4;
 }
 
-void* CALLOC1(int N1, size_t bytes_per_item) {
+inline void* CALLOC1(int N1, size_t bytes_per_item) {
   __produces("_Res ~> Matrix1(N1);");
   __admitted();
   return calloc(N1, bytes_per_item);
 }
 
-void* CALLOC2(int N1, int N2, size_t bytes_per_item) {
+inline void* CALLOC2(int N1, int N2, size_t bytes_per_item) {
   __produces("_Res ~> Matrix2(N1, N2);");
   __admitted();
   return calloc(N1 * N2, bytes_per_item);
 }
 
-void* CALLOC3(int N1, int N2, int N3, size_t bytes_per_item) {
+inline void* CALLOC3(int N1, int N2, int N3, size_t bytes_per_item) {
   __produces("_Res ~> Matrix3(N1, N2, N3);");
   __admitted();
   return calloc(N1 * N2 * N3, bytes_per_item);
 }
 
-void* CALLOC4(int N1, int N2, int N3, int N4, size_t bytes_per_item) {
+inline void* CALLOC4(int N1, int N2, int N3, int N4, size_t bytes_per_item) {
   __produces("_Res ~> Matrix4(N1, N2, N3, N4);");
   __admitted();
   return calloc(N1 * N2 * N3 * N4, bytes_per_item);
 }
 
-void* MALLOC0(size_t bytes_per_item) {
+inline void* MALLOC0(size_t bytes_per_item) {
   __produces("_Res ~> Matrix0();");
   __admitted();
   return malloc(bytes_per_item);
 }
 
-void* MALLOC1(int N1, size_t bytes_per_item) {
+inline void* MALLOC1(int N1, size_t bytes_per_item) {
   __produces("_Res ~> Matrix1(N1);");
   __admitted();
   return malloc(N1 * bytes_per_item);
 }
 
-void* MALLOC2(int N1, int N2, size_t bytes_per_item) {
+inline void* MALLOC2(int N1, int N2, size_t bytes_per_item) {
   __produces("_Res ~> Matrix2(N1, N2);");
   __admitted();
   return malloc(N1 * N2 * bytes_per_item);
 }
 
-void* MALLOC3(int N1, int N2, int N3, size_t bytes_per_item) {
+inline void* MALLOC3(int N1, int N2, int N3, size_t bytes_per_item) {
   __produces("_Res ~> Matrix3(N1, N2, N3);");
   __admitted();
   return malloc(N1 * N2 * N3 * bytes_per_item);
 }
 
-void* MALLOC4(int N1, int N2, int N3, int N4, size_t bytes_per_item) {
+inline void* MALLOC4(int N1, int N2, int N3, int N4, size_t bytes_per_item) {
   __produces("_Res ~> Matrix4(N1, N2, N3, N4);");
   __admitted();
   return malloc(N1 * N2 * N3 * N4 * bytes_per_item);
 }
 
-void MFREE0(void* p) {
+inline void MFREE0(void* p) {
   __consumes("p ~> Matrix0();");
   __admitted();
   free(p);
 }
 
-void MFREE1(int N1, void* p) {
+inline void MFREE1(int N1, void* p) {
   __consumes("p ~> Matrix1(N1);");
   __admitted();
   free(p);
 }
 
-void MFREE2(int N1, int N2, void* p) {
+inline void MFREE2(int N1, int N2, void* p) {
   __consumes("p ~> Matrix2(N1, N2);");
   __admitted();
   free(p);
 }
 
-void MFREE3(int N1, int N2, int N3, void* p) {
+inline void MFREE3(int N1, int N2, int N3, void* p) {
   __consumes("p ~> Matrix3(N1, N2, N3);");
   __admitted();
   free(p);
 }
 
-void MFREE4(int N1, int N2, int N3, int N4, void* p) {
+inline void MFREE4(int N1, int N2, int N3, int N4, void* p) {
   __consumes("p ~> Matrix4(N1, N2, N3, N4);");
   __admitted();
   free(p);
@@ -145,14 +157,14 @@ void MFREE4(int N1, int N2, int N3, int N4, void* p) {
 
 /* ---- Matrix Ghosts ---- */
 
-__ghost ghost_matrix2_focus(float* M, int i, int j) {
-    __requires("m: int; n: int;");
+__GHOST(ghost_matrix2_focus)  {
+    __requires("M: ptr; i: int; j: int; m: int; n: int;");
     __consumes("M ~> Matrix2(m, n);");
     __produces("&M[MINDEX2(m, n, i, j)] ~> Cell; M ~> FocussedMatrix2(m, n, i);");
     __admitted();
 }
 
-__ghost ghost_tile_divides() {
+__GHOST(ghost_tile_divides) {
   __requires(
     "tile_count: int; tile_size: int;"
     "gn: int; to_item: int -> resource;"
@@ -164,7 +176,8 @@ __ghost ghost_tile_divides() {
   __admitted();
 }
 
-__ghost ghost_tile_divides_reverse() {
+// TODO: Could be inferred from ghost_tile_divides?
+__GHOST(ghost_tile_divides_reverse) {
   __requires(
     "tile_count: int; tile_size: int;"
     "gn: int; to_item: int -> resource;"
@@ -176,22 +189,22 @@ __ghost ghost_tile_divides_reverse() {
   __admitted();
 }
 
-__ghost ghost_matrix2_unfocus(float* M) {
-    __requires("m: int; n: int; i: int; j: int;");
+__GHOST(ghost_matrix2_unfocus) {
+    __requires("M: ptr; m: int; n: int; i: int; j: int;");
     __consumes("M ~> FocussedMatrix2(m, n, i); &M[MINDEX2(m, n, i, j)] ~> Cell;");
     __produces("M ~> Matrix2(m, n);");
     __admitted();
 }
 
-__ghost ghost_matrix2_ro_focus(float* M, int i, int j) {
-    __requires("m: int; n: int; f: _Fraction;");
+__GHOST(ghost_matrix2_ro_focus) {
+    __requires("M: ptr; i: int; j: int; m: int; n: int; f: _Fraction;");
     __consumes("_RO(f, M ~> Matrix2(m, n));");
     __produces("_RO(f, &M[MINDEX2(m, n, i, j)] ~> Cell); _RO(f, M ~> FocussedMatrix2(m, n, i, j));");
     __admitted();
 }
 
-__ghost ghost_matrix2_ro_unfocus(float* M) {
-    __requires("m: int; n: int; i: int; j: int; f: _Fraction;");
+__GHOST(ghost_matrix2_ro_unfocus) {
+    __requires("M: ptr; m: int; n: int; i: int; j: int; f: _Fraction;");
     __consumes("_RO(_Full(f), M ~> FocussedMatrix2(m, n, i, j)); _RO(_Full(f), &M[MINDEX2(m, n, i, j)] ~> Cell);");
     __produces("_RO(f, M ~> Matrix2(m, n));");
     __admitted();
@@ -200,7 +213,7 @@ __ghost ghost_matrix2_ro_unfocus(float* M) {
 
 /* ---- Arithmetic Functions ---- */
 
-int exact_div(int n, int b) {
+inline int exact_div(int n, int b) {
   __pure();
   __admitted();
   return n / b;
@@ -208,8 +221,8 @@ int exact_div(int n, int b) {
 
 /* ---- Other Functions ---- */
 
-void MFREE(void* p) {
+inline void MFREE(void* p) {
   free(p);
 }
 
-int ANY(int maxValue) { return 0; }
+inline int ANY(int maxValue) { return 0; }
