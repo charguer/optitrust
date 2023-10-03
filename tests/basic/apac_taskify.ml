39c496c1d2128631987ed9db3bf7d453e5ea1b2b
FELŠÖCI Marek <marek.felsoci@inria.fr>
2023-10-03 21:07:34 +0200

    Use high-level transformation parallel_task_group in taskification test

--- tests/basic/apac_taskify.ml
+++ tests/basic/apac_taskify.ml
@@ -4,8 +4,7 @@ open Target
 let _ = Run.script_cpp (fun () ->
             (* Target all of the function definitions. *)
             !! Apac.constify [nbAny; cFunDefAndDecl ""];
-            !! Apac_basic.task_group [nbAny; cFunDef "c"];
-            !! Apac_basic.use_goto_for_return ~mark:Apac_core.task_group_mark [nbAny; cFunDef "main"];
-            !! Apac_basic.task_group ~master:true [nbAny; cFunDef "main"];
+            !! Apac.parallel_task_group [nbAny; cFunDef "c"];
+            !! Apac.parallel_task_group [nbAny; cFunDef "main"];
             !! Apac_core.taskify [nbAny; cFunDef ""];
           )
