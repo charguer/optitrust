b706ab343a34970da4a46301513a7d0ee1242c9b
FELŠÖCI Marek <marek.felsoci@inria.fr>
2023-10-03 14:01:14 +0200

    Add Ocamlgraph to environment

--- .guix/manifest.scm
+++ .guix/manifest.scm
@@ -1,6 +1,6 @@
 (concatenate-manifests
  (list
   (specifications->manifest
-   (list "bash" "coreutils" "gcc-toolchain"))
+   (list "bash" "coreutils" "gcc-toolchain" "ocaml-graph"))
   (package->development-manifest
    (specification->package "ocaml-optitrust"))))
