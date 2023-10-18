12d4e21f04395daedf1331a385d54d56be9504ee
charguer <arthur@chargueraud.org>
2023-10-18 11:38:19 +0200

    opentest script

--- .vscode/open_test.sh
+++ .vscode/open_test.sh
@@ -1,12 +1,32 @@
-FILE=$1
+#!/bin/bash
 
+# Open in vscode the tests files associated with a transformation
+
+# Usage:
+#    open_test.sh mytest.ml withdoc
+#    open_test.sh mytest.ml
+#    open_test.sh mytest.cpp
+#    open_test.sh mytest_doc.ml
+#    open_test.sh mytest
+
+FILE=$1
+WITHDOC=$2
+
+# remove extension and _doc suffix if any
 BASE=${FILE%.*}
+BASE=${BASE%_doc}
+
 ARGS="${BASE}.ml ${BASE}.cpp"
 
-if [ -f ${BASE}.ml ]
+if [ ! -z "${WITHDOC}" ]; then
+  ARGS+=" ${BASE}_doc.ml ${BASE}_doc.cpp"
+fi
+
+if [ -f "${BASE}.ml" ]
 then
   code ${ARGS}
 else
-  echo "open_test.sh: ${FILE} is not a cpp or a ml file"
+  echo "open_test.sh: ${BASE}.ml is not a cpp or a ml file"
   exit 1
 fi
+
