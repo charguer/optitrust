8a684a37b03f4deb1572e102c65ce6515f3587fe
Guillaume Bertholon <guillaume.bertholon@ens.fr>
2023-10-18 16:09:44 +0200

    Add smart constructors/destructors for ghost pairs

--- src/resources/resource_formula.ml
+++ src/resources/resource_formula.ml
@@ -164,6 +164,37 @@ let formula_checked = trm_var var_checked
 
 let ghost_begin = toplevel_var "__ghost_begin"
 let ghost_end = toplevel_var "__ghost_end"
+let typ_ghost_fn: typ = typ_const (typ_constr ([], "__ghost_fn") ~tid:(-1))
+
+let ghost_pair_last_id = ref 0
+let generate_ghost_pair_var () =
+  let ghost_pair_id = !ghost_pair_last_id in
+  ghost_pair_last_id := ghost_pair_id + 1;
+  new_var (sprintf "__ghost_pair_%d" ghost_pair_id)
+
+let trm_ghost_pair (ghost_fn: var) (ghost_args: (string * trm) list): trm * trm =
+  let ghost_pair_var = generate_ghost_pair_var () in
+  ( trm_add_cstyle GhostCall (trm_let Var_immutable (ghost_pair_var, typ_ghost_fn)
+      (trm_apps (trm_var ghost_begin) [trm_ghost ghost_fn ghost_args])),
+    trm_add_cstyle GhostCall (trm_apps (trm_var ghost_end) [trm_var ghost_pair_var]) )
+
+let trm_ghost_scope (ghost_fn: var) (ghost_args: (string * trm) list) (seq: trm list): trm =
+  let ghost_begin, ghost_end = trm_ghost_pair ghost_fn ghost_args in
+  Nobrace.trm_seq (ghost_begin :: seq @ [ghost_end])
+
+let trm_ghost_begin_inv (t: trm): (var * trm * (hyp * formula) list) option =
+  Pattern.pattern_match t [
+    Pattern.(trm_let __ !__ __ (trm_apps1 (trm_var (var_eq ghost_begin)) (trm_apps !__ nil !__))) (fun pair_var ghost_fn ghost_args ->
+      Some (pair_var, ghost_fn, ghost_args)
+    );
+    Pattern.__ None
+  ]
+
+let trm_ghost_end_inv (t: trm): var option =
+  Pattern.pattern_match t [
+    Pattern.(trm_apps1 (trm_var (var_eq ghost_end)) (trm_var !__)) (fun pair_var -> Some pair_var);
+    Pattern.__ None
+  ]
 
 let ghost_rewrite = toplevel_var "rewrite"
 
