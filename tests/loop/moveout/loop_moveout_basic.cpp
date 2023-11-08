8d3be1e490f1f393065a8dc93caa3b0e66b03128
Thomas Koehler <thomas.koehler@thok.eu>
2023-11-08 13:38:50 +0100

    add negative loop moveout tests

--- tests/loop/moveout/loop_moveout_basic.cpp
+++ tests/loop/moveout/loop_moveout_basic.cpp
@@ -6,6 +6,38 @@ void var(int* t) {
   int x = 0;
   for (int i = 0; i < 3; i++) {
     x = 3;
+    x++;
+    t[MINDEX1(3, i)] = x;
+  }
+}
+
+void var_wrong1(int* t) {
+  __modifies("t ~> Matrix1(3)");
+
+  int x = 0;
+  for (int i = 0; i < 3; i++) {
+    x = i;
+    t[MINDEX1(3, i)] = x;
+  }
+}
+
+void var_wrong2(int* t) {
+  __modifies("t ~> Matrix1(3)");
+
+  int x = 0;
+  for (int i = 0; i < 3; i++) {
+    t[MINDEX1(3, i)] = x;
+    x = 3;
+    t[MINDEX1(3, i)] += x;
+  }
+}
+
+void var_wrong3(int* t) {
+  __modifies("t ~> Matrix1(3)");
+
+  int x = 0;
+  for (int i = 0; i < 3; i++) {
+    x += 3;
     t[MINDEX1(3, i)] = x;
   }
 }
@@ -23,6 +55,20 @@ void arr(int* t, int* x) {
   }
 }
 
+void arr_wrong1(int* t, int* x) {
+  __modifies("t ~> Matrix2(3, 5), x ~> Matrix1(5)");
+
+  int v = 3;
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 5; j++) {
+      x[MINDEX1(5, j)] = v;
+    }
+    for (int j = 0; j < 5; j++) {
+      t[MINDEX2(3, 5, i, j)] = x[MINDEX1(5, j)];
+    }
+    v++;
+  }
+}
 
 void test(int* t){
   __modifies("t ~> Matrix1(10)");
--- tests/loop/moveout/loop_moveout_basic.ml
+++ tests/loop/moveout/loop_moveout_basic.ml
@@ -1,16 +1,25 @@
 open Optitrust
 open Target
 
-(* Note: currently, there is no check that the transformation is legitimate. E.g.:
-      !! Loop_basic.move_out [cVarDef "s"]; *)
+let _ = Flags.check_validity := true
 
 let _ = Run.script_cpp (fun _ ->
 
   !! Loop_basic.move_out [cFunBody "test"; cVarDef "x"];
   !! Loop_basic.move_out [cFunBody "test"; cVarDef "x"];
   !! Loop_basic.move_out [cFunBody "test"; cVarDef "s"];
+  !! Trace.failure_expected (fun () ->
+    Loop_basic.move_out [cFunBody "test"; cVarDef "s"]);
 
   !! Loop_basic.move_out [cFunBody "var"; sInstr "x = 3"];
+  !! Trace.failure_expected (fun () ->
+    Loop_basic.move_out [cFunBody "var_wrong1"; sInstr "x = i"]);
+  !! Trace.failure_expected (fun () ->
+    Loop_basic.move_out [cFunBody "var_wrong2"; sInstr "x = 3"]);
+  !! Trace.failure_expected (fun () ->
+    Loop_basic.move_out [cFunBody "var_wrong3"; sInstr "x += 3"]);
 
   !! Loop_basic.move_out [cFunBody "arr"; cFor ~body:[cArrayWrite "x"] "j"];
+  !! Trace.failure_expected (fun () ->
+    Loop_basic.move_out [cFunBody "arr_wrong1"; cFor ~body:[cArrayWrite "x"] "j"]);
 )
