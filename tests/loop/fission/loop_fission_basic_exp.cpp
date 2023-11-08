1f7185ec1f124318bf6c2fe63039b6ffe630356a
Guillaume Bertholon <guillaume.bertholon@normalesup.org>
2023-11-08 17:06:18 +0100

    Update test output of loop_fission_basic

--- tests/loop/fission/loop_fission_basic_exp.cpp
+++ tests/loop/fission/loop_fission_basic_exp.cpp
@@ -14,6 +14,8 @@ int test(int* t, int* u, int n) {
     __modifies("&u[i] ~> Cell");
     int b = i;
     u[i] += b;
+    int c = i;
+    u[i] += c;
   }
   for (int i = 0; i < 5; i++) {
     __pure();
@@ -29,5 +31,70 @@ int test(int* t, int* u, int n) {
     MFREE1(5, m1);
     int* const m2 = (int* const)MALLOC1(5, sizeof(int));
     MFREE1(5, m2);
+  }
+}
+
+int testAllInstr(int* t, int* u, int n) {
+  __pure();
+  for (int i = 0; i < 5; i++) {
+    __pure();
+    int a1 = i;
+  }
+  for (int i = 0; i < 5; i++) {
+    __pure();
+    int a2 = i;
+  }
+  for (int i = 0; i < 5; i++) {
+    __pure();
+    int a3 = i;
+  }
+  for (int i = 0; i < 5; i++) {
+    __pure();
+    int a4 = i;
+  }
+  for (int i = 0; i < 5; i++) {
+    __pure();
+    int a5 = i;
+  }
+}
+
+int testAllInstr2(int* t, int* u, int n) {
+  __pure();
+  for (int i = 0; i < 5; i++) {
+    __pure();
+    int a1 = i;
+    int a2 = i;
+  }
+  for (int i = 0; i < 5; i++) {
+    __pure();
+    int a3 = i;
+  }
+  for (int i = 0; i < 5; i++) {
+    __pure();
+    int a4 = i;
+    int a5 = i;
+  }
+}
+
+int testAllInstrContracts(int* t, int* u, int n) {
+  __modifies("Group(range(1, n, 1), fun i -> &t[i] ~> Cell)");
+  __modifies("Group(range(1, n, 1), fun i -> &u[i] ~> Cell)");
+  for (int i = 1; i < n; i++) {
+    __modifies("&t[i] ~> Cell");
+    __modifies("&u[i] ~> Cell");
+    int a = i;
+    t[i] += a;
+  }
+  for (int i = 1; i < n; i++) {
+    __modifies("&t[i] ~> Cell");
+    __modifies("&u[i] ~> Cell");
+    int b = i;
+    u[i] += b;
+  }
+  for (int i = 1; i < n; i++) {
+    __modifies("&u[i] ~> Cell");
+    __modifies("&t[i] ~> Cell");
+    int c = i;
+    u[i] += c;
   }
 }
