43e3ee6d6b03c07d74e5d4cf233ce9b61882e267
FELŠÖCI Marek <marek.felsoci@inria.fr>
2023-11-20 21:12:39 +0100

    Update taskification test

--- tests/basic/apac_taskify.ml
+++ tests/basic/apac_taskify.ml
@@ -4,7 +4,8 @@ open Target
 let _ = Run.script_cpp (fun () ->
             (* Target all of the function definitions. *)
             !! Apac.constify [nbAny; cFunDefAndDecl ""];
-            !! Apac.parallel_task_group [nbAny; cFunDef "c"];
-            !! Apac.parallel_task_group [nbAny; cFunDef "main"];
-            !! Apac_core.taskify [nbAny; cFunDef ""];
+            !! Apac.parallel_task_group ~mark_group:true [nbAny; cFunDef "c"];
+            !! Apac.parallel_task_group ~mark_group:true [nbAny; cFunDef "main"];
+            !! Apac_core.taskify [nbAny; cMark Apac_core.task_group_mark];
+          (*!! Apac_core.insert_tasks [nbAny; cMark Apac_core.task_group_mark];*)
           )
