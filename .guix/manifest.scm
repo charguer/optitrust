74f10b751b357bd8cc168fc74ad15c133381ae42
FELŠÖCI Marek <marek.felsoci@inria.fr>
2023-11-22 20:31:45 +0100

    Add Ocamlgraph, Org mode and other utilities

--- .guix/manifest.scm
+++ .guix/manifest.scm
@@ -1,7 +1,19 @@
 (concatenate-manifests
  (list
   (specifications->manifest
-   (list "bash" "coreutils" "gcc-toolchain" "ocaml-merlin" "emacs"
-         "emacs-tuareg" "emacs-projectile" "emacs-spacemacs-theme" "git"))
+   (list "bash"
+         "coreutils"
+         "gcc-toolchain"
+         "nano"
+	 "graphviz"
+         "ocaml-merlin"
+         "ocaml-graph"
+         "emacs"
+         "emacs-org"
+         "emacs-org-ref"
+         "emacs-tuareg"
+         "emacs-projectile"
+         "emacs-spacemacs-theme"
+         "git"))
   (package->development-manifest
    (specification->package "ocaml-optitrust"))))
