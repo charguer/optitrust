774bd6a9f2fb3637f736a9199c7cb70715bc4ba9
FELŠÖCI Marek <marek.felsoci@inria.fr>
2024-02-08 19:04:05 +0100

    Update quicksort transformation script

--- case_studies/quicksort/quicksort.ml
+++ case_studies/quicksort/quicksort.ml
@@ -4,23 +4,23 @@ open Typ (* TODO: avoid this *)
 open Ast
 
 let _ = Run.script_cpp (fun () ->
-  !! Apac.constify [nbAny; cFunDefAndDecl ""];
-  !! Apac_basic.mark_taskification_candidates [nbAny; cFunDef ""];
-
-  (* heapify
-  (* uses add/remove mark to target sequence with depth > 0 *)
-  !! Marks.add "heapify" [nbAny; cMark "taskable"; dBody; cInDepth; cSeq()];
-  !! Marks.remove "heapify" [nbAny; cMark "taskable"; dBody; cStrictNew; cSeq()];
-  !! Apac.heapify_nested_seq [nbAny; cMark "heapify";];
-  !! Marks.remove "heapify" [nbAny; cMark "heapify"];
-
-  (* adds tasks*)
-  let fad = Apac.get_functions_args_deps [] in
-  !! Apac.insert_tasks_naive fad [nbAny; cMark "taskable"];
-
-
-  (* adds taskgroup + transforms return to goto *)
-  !! Apac.parallel_task_group [nbAny; cMark "taskable"];
-
-  !! Marks.remove "taskable" [nbAny; cMark "taskable"];*)
-)
+            (* Target all of the function definitions except for the 'main'
+               function. *)
+            !! Apac.constify [
+                nbAny;
+                cDiff [[cFunDefAndDecl ""]] [[cFunDefAndDecl "main"]]
+              ];
+            (* Target all of the function definitions. *)
+            !! Apac.parallel_task_group ~mark_group:true [
+                nbAny;
+                cFunDefAndDecl ""
+              ];
+            (* Perform cleaning. *)
+            !! Marks.remove Apac_core.task_group_mark [
+                nbAny;
+                cMark Apac_core.task_group_mark
+              ];
+          (* !! Apac_core.taskify [nbAny; cMark Apac_core.task_group_mark];
+             !! Apac_core.profile_tasks [nbAny; cMark Apac_core.task_group_mark];
+             !! Apac_core.include_apac_profiler []; *)
+          )
