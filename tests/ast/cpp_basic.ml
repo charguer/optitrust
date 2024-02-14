ed62b72c7eafcd9bd4400f0d0dd1c3330eb9003e
Berenger Bramas <Berenger.Bramas@inria.fr>
2024-02-14 12:02:56 +0100

    Add more cpp examples

---
+++ tests/ast/cpp_basic.ml
@@ -0,0 +1,13 @@
+(* A test for the parsing of C++ features *)
+
+open Optitrust
+open Syntax
+
+let _ = Flags.use_light_diff := false
+
+let _ = Run.script_cpp (fun () ->
+  !! Sequence_basic.insert (stmt "int c = 0;") [tBefore; cVarDef "a"];
+  !! Sequence_basic.insert (stmt "int h(int x) {return x;}") [tBefore; cFunDef "main"];
+  (* Trace.reparse(); *) (* TODO: transformations that insert C++ (but not C) code *)
+
+)
