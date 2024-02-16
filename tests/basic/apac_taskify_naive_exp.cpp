b86b7b5e75a0f64000c7a93390cf29dbe6373196
FELŠÖCI Marek <marek.felsoci@inria.fr>
2024-02-16 20:38:13 +0100

    Extend naive taskification test and update expected output

--- tests/basic/apac_taskify_naive_exp.cpp
+++ tests/basic/apac_taskify_naive_exp.cpp
@@ -10,25 +10,27 @@ int g(int* a) {
 int h() {
   int __res;
   /*@__apac_task_group*/ #pragma omp taskgroup {
-    int a;
-  __exit:;
-    int** c;
-    c = (int**)malloc(sizeof(int));
-    g(*c);
     int b;
     b = 0;
     b = b + 1;
     b--;
+    int** c;
+    c = (int**)malloc(sizeof(int));
+    g(*c);
+    int a;
 #pragma omp task default(shared) depend(inout : a, b)
-    { a = 1 + b++; }
-#pragma omp task default(shared) depend(inout : a)
-    { a = 2; }
+    a = 1 + b++;
 #pragma omp task default(shared) depend(inout : b)
-    { b++; }
-#pragma omp task default(shared) depend(in : a, b)
-    { f(a, b); }
+    b++;
 #pragma omp task default(shared) depend(inout : a)
-    { a = 3; }
+    a = 2;
+#pragma omp task default(shared) depend(in : a, b)
+    f(a, b);
+#pragma omp task default(shared) depend(inout : a)
+    a = 3;
+#pragma omp task default(shared) depend(in : a) depend(inout : **c)
+    **c = a;
+  __apac_exit:;
   } /*__apac_task_group@*/
   return __res;
 }
