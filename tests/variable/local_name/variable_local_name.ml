9abe51156fa33611df95ba0b7254446629386095
Thomas Koehler <thomas.koehler@thok.eu>
2024-02-21 11:07:08 +0100

    variable local name bug

--- tests/variable/local_name/variable_local_name.ml
+++ tests/variable/local_name/variable_local_name.ml
@@ -11,7 +11,13 @@ let _ = Run.script_cpp (fun _ ->
     Variable.local_name ~var:"a" ~local_var:"x" [cFunBody "ko1"; cFor "i"];
   );
   !! Trace.failure_expected (fun _e -> true) (fun () ->
+    Variable.local_name ~var:"b" ~local_var:"x" [cFunBody "ko1"; cFor "i"];
+  );
+  !! Trace.failure_expected (fun _e -> true) (fun () ->
     Variable.local_name ~var:"a" ~local_var:"x" [cFunBody "ko2"; cLabel "l"]
+  );
+  !! Trace.failure_expected (fun _e -> true) (fun () ->
+    Variable.local_name ~var:"b" ~local_var:"x" [cFunBody "ko2"; cLabel "l"]
   );
 
   (* TODO: this triggers a renaming, should it throw an error instead? *)
