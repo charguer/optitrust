e5dc372e3964b42b81261aebedbe57d67fe7a859
Thomas Koehler <thomas.koehler@thok.eu>
2024-02-23 14:52:33 +0100

    start simplifying opencv script

--- case_studies/opencv/box_filter_rowsum.cpp
+++ case_studies/opencv/box_filter_rowsum.cpp
@@ -5,83 +5,83 @@ typedef uint16_t ST;
 
 /*
   cn: number of (color) channels
-  ksize: size of box filter (convolution window)
-  width: size of the row resulting from filtering
+  kn: size of box filter (convolution window)
+  n: size of the row resulting from filtering
 */
-void rowSum(const int ksize, const T* S, ST* D, const int width, const int cn) {
-  __reads("S ~> Matrix2(width+ksize, cn)");
-  __modifies("D ~> Matrix2(width, cn)"); // TODO: writes?
+void rowSum(const int kn, const T* S, ST* D, const int n, const int cn) {
+  __reads("S ~> Matrix2(n+kn, cn)");
+  __modifies("D ~> Matrix2(n, cn)"); // TODO: writes?
 
-  __ghost(swap_groups, "items := fun i, k -> &D[MINDEX2(width, cn, i, k)] ~> Cell");
-  for (int k = 0; k < cn; k++) { // foreach channel
-    __parallel_reads("S ~> Matrix2(width+ksize, cn)");
-    __modifies("Group(range(0, width, 1), fun i -> &D[MINDEX2(width, cn, i, k)] ~> Cell)");
+  __ghost(swap_groups, "items := fun i, c -> &D[MINDEX2(n, cn, i, c)] ~> Cell");
+  for (int c = 0; c < cn; c++) { // foreach channel
+    __parallel_reads("S ~> Matrix2(n+kn, cn)");
+    __modifies("Group(range(0, n, 1), fun i -> &D[MINDEX2(n, cn, i, c)] ~> Cell)");
 
     // initialize the sliding window
     ST s = 0;
-    for (int i = 0; i < ksize; i++) {
-      __parallel_reads("S ~> Matrix2(width+ksize, cn)");
+    for (int i = 0; i < kn; i++) {
+      __parallel_reads("S ~> Matrix2(n+kn, cn)");
       __sequentially_modifies("&s ~> Cell");
-      __GHOST_BEGIN(sf, matrix2_ro_focus, "M := S, i := i, j := k");
-      s += (ST) S[MINDEX2(width+ksize, cn, i, k)];
+      __GHOST_BEGIN(sf, matrix2_ro_focus, "M := S, i := i, j := c");
+      s += (ST) S[MINDEX2(n+kn, cn, i, c)];
       __GHOST_END(sf);
     }
-    __GHOST_BEGIN(df, group_focus, "items := fun i -> &D[MINDEX2(width, cn, i, k)] ~> Cell, i := 0, bound_check_start := checked, bound_check_stop := checked,  bound_check_step := checked");
-    D[MINDEX2(width, cn, 0, k)] = s;
+    __GHOST_BEGIN(df, group_focus, "items := fun i -> &D[MINDEX2(n, cn, i, c)] ~> Cell, i := 0, bound_check_start := checked, bound_check_stop := checked,  bound_check_step := checked");
+    D[MINDEX2(n, cn, 0, c)] = s;
     __GHOST_END(df);
     // for each pixel, shift the sliding window
-    for (int i = 0; i < width-1; i++) {
-      __parallel_reads("S ~> Matrix2(width+ksize, cn)");
+    for (int i = 0; i < n-1; i++) {
+      __parallel_reads("S ~> Matrix2(n+kn, cn)");
       __sequentially_modifies("&s ~> Cell");
-      __sequentially_modifies("Group(range(0, width, 1), fun i -> &D[MINDEX2(width, cn, i, k)] ~> Cell)");
+      __sequentially_modifies("Group(range(0, n, 1), fun i -> &D[MINDEX2(n, cn, i, c)] ~> Cell)");
 
-      __GHOST_BEGIN(sf1, matrix2_ro_focus, "M := S, i := i, j := k");
-      s -= (ST) S[MINDEX2(width+ksize, cn, i, k)];
+      __GHOST_BEGIN(sf1, matrix2_ro_focus, "M := S, i := i, j := c");
+      s -= (ST) S[MINDEX2(n+kn, cn, i, c)];
       __GHOST_END(sf1);
-      __GHOST_BEGIN(sf2, matrix2_ro_focus, "M := S, i := i + ksize, j := k");
-      s += (ST) S[MINDEX2(width+ksize, cn, i + ksize, k)];
+      __GHOST_BEGIN(sf2, matrix2_ro_focus, "M := S, i := i + kn, j := c");
+      s += (ST) S[MINDEX2(n+kn, cn, i + kn, c)];
       __GHOST_END(sf2);
-      __GHOST_BEGIN(df, group_focus, "items := fun i -> &D[MINDEX2(width, cn, i, k)] ~> Cell, i := i + 1, bound_check_start := checked, bound_check_stop := checked, bound_check_step := checked");
-      D[MINDEX2(width, cn, i + 1, k)] = s;
+      __GHOST_BEGIN(df, group_focus, "items := fun i -> &D[MINDEX2(n, cn, i, c)] ~> Cell, i := i + 1, bound_check_start := checked, bound_check_stop := checked, bound_check_step := checked");
+      D[MINDEX2(n, cn, i + 1, c)] = s;
       __GHOST_END(df);
     }
     /* ALTERNATIVE
-    for (int i = 1; i < width; i++) {
-      s -= (ST) S[MINDEX2(?, cn, i - 1, k)];
-      s += (ST) S[MINDEX2(?, cn, i + ksize-1, k)];
-      D[MINDEX2(?, cn, i, k] = s;
+    for (int i = 1; i < n; i++) {
+      s -= (ST) S[MINDEX2(?, cn, i - 1, c)];
+      s += (ST) S[MINDEX2(?, cn, i + kn-1, c)];
+      D[MINDEX2(?, cn, i, c] = s;
     }
     */
   }
-  __ghost(swap_groups_rev, "items := fun i, k -> &D[MINDEX2(width, cn, i, k)] ~> Cell");
+  __ghost(swap_groups_rev, "items := fun i, c -> &D[MINDEX2(n, cn, i, c)] ~> Cell");
 }
 
-void rowSum_cn1(const int ksize, const T* S, ST* D, const int width) {
-  __reads("S ~> Matrix2(width+ksize, 1)");
-  __modifies("D ~> Matrix2(width, 1)"); // TODO: writes?
+void rowSum_cn1(const int kn, const T* S, ST* D, const int n) {
+  __reads("S ~> Matrix2(n+kn, 1)");
+  __modifies("D ~> Matrix2(n, 1)"); // TODO: writes?
 
-  rowSum(ksize, S, D, width, 1);
+  rowSum(kn, S, D, n, 1);
 }
 
-void rowSum_cn3(const int ksize, const T* S, ST* D, const int width) {
-  __reads("S ~> Matrix2(width+ksize, 3)");
-  __modifies("D ~> Matrix2(width, 3)"); // TODO: writes?
+void rowSum_cn3(const int kn, const T* S, ST* D, const int n) {
+  __reads("S ~> Matrix2(n+kn, 3)");
+  __modifies("D ~> Matrix2(n, 3)"); // TODO: writes?
 
-  rowSum(ksize, S, D, width, 3);
+  rowSum(kn, S, D, n, 3);
 }
 
-void rowSumOpt(const int ksize, const T* S, ST* D, const int width, const int cn) {
+void rowSumOpt(const int kn, const T* S, ST* D, const int n, const int cn) {
   // introduce arbitrary conditions
-  /* if (ksize == 3) {
-    // in this section, can do the substitution, or insert the line "const int ksize = 3;".
+  /* if (kn == 3) {
+    // in this section, can do the substitution, or insert the line "const int kn = 3;".
     // ...
-  } else if (ksize == 5) {
+  } else if (kn == 5) {
     // ...
   } else */ if (cn == 1) {
-    rowSum_cn1(ksize, S, D, width);
+    rowSum_cn1(kn, S, D, n);
   } else if (cn == 3) {
-    rowSum_cn3(ksize, S, D, width);
+    rowSum_cn3(kn, S, D, n);
   } else {
-    rowSum(ksize, S, D, width, cn);
+    rowSum(kn, S, D, n, cn);
   }
 }
--- case_studies/opencv/box_filter_rowsum.ml
+++ case_studies/opencv/box_filter_rowsum.ml
@@ -10,7 +10,7 @@ let _ = Run.script_cpp (fun () ->
   bigstep "cn == 1";
   let cn1 = resolve_target_exactly_one [cFunBody "rowSum_cn1"] in
   (* let cn1 = resolve_target_exactly_one_with_stringreprs_available [cIf ~cond:[sExpr "cn == 1"] (); dThen] (Trace.ast ()) in *)
-  !! Loop.unroll [cPath cn1; cFor "k"];
+  !! Loop.unroll [cPath cn1; cFor "c"];
 
   bigstep "cn == 3";
   let cn3 = resolve_target_exactly_one [cFunBody "rowSum_cn3"] in
@@ -19,8 +19,8 @@ let _ = Run.script_cpp (fun () ->
   let for_dwrite = [cPath cn3; cFor "i" ~body:[cArrayWrite "D"]] in
   !! Loop.fusion_targets ~into:(occLast :: for_dwrite) (nbMulti :: for_dwrite);
   let dwrite = [cPath cn3; cStrict; cArrayWrite "D"] in
-  let last_dwrite = Xlist.take_last 1 (resolve_target_exactly_one (occLast :: dwrite)) in
-  !! Instr.move_in_seq ~dest:[tBefore; cPath last_dwrite] (nbMulti :: dwrite);
-  let for_ksize = [cPath cn3; cFor ~stop:[cVar "ksize"] "i"] in
-  !! Loop.fusion_targets ~into:(occLast :: for_ksize) (nbMulti :: for_ksize);
+  let last_dwrite = resolve_target_exactly_one (occLast :: dwrite) in
+  !! Instr.move ~dest:[tBefore; cPath last_dwrite] (nbMulti :: dwrite);
+  let for_kn = [cPath cn3; cFor ~stop:[cVar "kn"] "i"] in
+  !! Loop.fusion_targets ~into:(occLast :: for_kn) (nbMulti :: for_kn);
 )
