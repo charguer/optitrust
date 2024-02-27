057a2e7f8a311e344583879e75829f4eb08c99b5
Guillaume Bertholon <guillaume.bertholon@normalesup.org>
2024-02-27 12:08:22 +0100

    Do not compute resources in the middle of Arith.simpl_surrounding_expr

--- src/transfo/arith.ml
+++ src/transfo/arith.ml
@@ -10,12 +10,14 @@ let%transfo simpl_surrounding_expr ?(indepth : bool = true) (f : (expr -> expr))
   Trace.tag_valid_by_composition ();
   let paths_to_simpl = ref Path_set.empty in
   let t = Trace.ast () in
-  Target.iter (fun p ->
-    paths_to_simpl := Path_set.add (Path.find_surrounding_expr p t) !paths_to_simpl;
-  ) tg;
-  Path_set.iter (fun p ->
-    Arith_basic.simpl ~indepth f (target_of_path p);
-  ) !paths_to_simpl
+  Trace.without_resource_computation_between_steps (fun () ->
+    Target.iter (fun p ->
+      paths_to_simpl := Path_set.add (Path.find_surrounding_expr p t) !paths_to_simpl;
+    ) tg;
+    Path_set.iter (fun p ->
+      Arith_basic.simpl ~indepth f (target_of_path p);
+    ) !paths_to_simpl
+  )
 
 (* TODO?
 let default_simpl tg = simpl_surrounding_expr (fun x -> compute (gather x)) (nbAny :: tg)
