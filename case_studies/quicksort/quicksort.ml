4c0a1d9188edb25fee5f5f2d332c0f73c6298066
FELŠÖCI Marek <marek.felsoci@inria.fr>
2024-02-29 20:57:53 +0100

    Fix Quicksort after code factorization

--- case_studies/quicksort/quicksort.ml
+++ case_studies/quicksort/quicksort.ml
@@ -4,41 +4,43 @@ open Typ (* TODO: avoid this *)
 open Ast
 
 let _ = Run.script_cpp (fun () ->
+            let _ = Flags.code_print_width := 1024 in
             (* Target all of the function definitions except for the 'main'
                function. *)
-            !! Apac.constify [
+            !! Apac_constify.constify [
                 nbAny;
                 cDiff [[cFunDefAndDecl ""]] [[cFunDefAndDecl "main"]]
               ];
             (* Target all of the function definitions. *)
-            !! Apac.parallel_task_group ~mark_group:true [
+            !! Apac_taskify.parallel_task_group ~mark_group:true [
                 nbAny;
                 cFunDefAndDecl "sort_core"
               ];
             (* Target the main function's definition. *)
-            !! Apac.parallel_task_group [
+            !! Apac_taskify.parallel_task_group [
                 nbAny;
                 cFunDefAndDecl "main"
               ];
-            !! Apac_core.taskify [nbAny; cMark Apac_core.task_group_mark];
-            !! Apac_core.merge [nbAny; cMark Apac_core.task_group_mark];
-            !! Apac_core.insert_tasks [nbAny; cMark Apac_core.task_group_mark];
-            !! Marks.remove Apac_core.task_group_mark [
+            !! Apac_taskify.taskify [nbAny; cMark Apac_macros.task_group_mark];
+            !! Apac_taskify.merge [nbAny; cMark Apac_macros.task_group_mark];
+            !! Apac_taskify.insert_tasks
+              [nbAny; cMark Apac_macros.task_group_mark];
+            !! Marks.remove Apac_macros.task_group_mark [
                 nbAny;
-                cMark Apac_core.task_group_mark
+                cMark Apac_macros.task_group_mark
               ];
-            !! Apac_basic.heapify [
+            !! Apac_epilogue.heapify [
                 nbAny;
-                cOr [[cMark Apac_core.heapify_mark];
-                     [cMark Apac_core.heapify_breakable_mark]]
+                cOr [[cMark Apac_macros.heapify_mark];
+                     [cMark Apac_macros.heapify_breakable_mark]]
               ];
-            !! Marks.remove Apac_core.heapify_mark [
+            !! Marks.remove Apac_macros.heapify_mark [
                 nbAny;
-                cMark Apac_core.heapify_mark
+                cMark Apac_macros.heapify_mark
               ];
-            !! Marks.remove Apac_core.heapify_breakable_mark [
+            !! Marks.remove Apac_macros.heapify_breakable_mark [
                 nbAny;
-                cMark Apac_core.heapify_breakable_mark
+                cMark Apac_macros.heapify_breakable_mark
               ];
           (* !! Apac_core.taskify [nbAny; cMark Apac_core.task_group_mark];
              !! Apac_core.profile_tasks [nbAny; cMark Apac_core.task_group_mark];
