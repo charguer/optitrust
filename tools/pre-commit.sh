a87e7a4162d2be085190f870a834a84552332e3f
Guillaume Bertholon <guillaume.bertholon@normalesup.org>
2024-03-28 13:42:36 +0100

    Pre-commit hook preserves merge commits

--- tools/pre-commit.sh
+++ tools/pre-commit.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
+shopt -s nullglob
+
 # Skip in case there is nothing staged
 if git diff --staged --quiet
 then
   exit 0
 fi
 
+pushd $(git rev-parse --show-toplevel) > /dev/null
+
+# If we are a merge commit, save the merge status since the stash command will break it
+if [ -f .git/MERGE_HEAD ]
+then
+  MERGE_FILES=(MERGE_*)
+  if [ ${#MERGE_FILES[@]} -gt 0 ]
+  then
+    echo "Saved merge is already present in the worktree" > /dev/stderr
+    echo "Need to remove files: ${MERGE_FILES[@]}" > /dev/stderr
+    exit 1
+  fi
+  mv .git/MERGE_* .
+fi
+
 STASH_NAME="Stash before pre-commit ($(date))"
 git stash push --quiet --keep-index --include-untracked -m "${STASH_NAME}"
 
-pushd $(git rev-parse --show-toplevel) > /dev/null
-
 ./tester run
 TESTER_RESULT=$?
-
-popd > /dev/null
 
 if git stash list | head -n 1 | grep -q "${STASH_NAME}"
 then
@@ -22,7 +35,15 @@ then
   # The untracked part is somehow not included in the main stash but in stash^3
   git restore --worktree --overlay -s stash@{0}^3 :/
   git stash drop --quiet
+
+  # Restore the merge status
+  if [ -f MERGE_HEAD ]
+  then
+    mv MERGE_* .git
+  fi
 fi
+
+popd > /dev/null
 
 if [ $TESTER_RESULT != 0 ]
 then
