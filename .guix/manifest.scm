155e5f06f72b752576942587ab0e5268cdd1d0ca
Marek FELŠÖCI <marek.felsoci@inria.fr>
2024-06-20 18:06:13 +0200

    Simplify Guix manifest

--- .guix/manifest.scm
+++ .guix/manifest.scm
@@ -1,29 +1,5 @@
-(use-modules (guix packages) (guix git-download) (gnu packages ocaml)
-             (guix build-system dune))
-
-(define-public ocaml-graph-mf
-  (package
-   (inherit ocaml-graph)
-   (name "ocaml-graph-mf")
-   (version "2.1.0")
-   (source
-    (origin
-     (method git-fetch)
-     (uri
-      (git-reference
-       (url "https://github.com/felsocim/ocamlgraph")
-       (commit "052d5b9f363ccc5bf9cea5227d7895d2ea9eb41f")))
-     (sha256
-      (base32
-       "1izd7zg0jcskbc41dpqn7iz097nnrj0wr5yjcmkj84zc7f9lz6kk"))))
-   (build-system dune-build-system)
-   (arguments
-    `(#:install-target "install"))
-   (inputs (list lablgtk ocaml-graphics ocaml-stdlib-shims))))
-
 (concatenate-manifests
  (list
-  (packages->manifest (list ocaml-graph-mf))
   (specifications->manifest
    (list "bash"
          "coreutils"
