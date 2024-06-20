1aba2fb4ba4818b873d4b17ae4ca91671edaf208
Marek FELŠÖCI <marek.felsoci@inria.fr>
2024-06-20 18:06:03 +0200

    Update Guix channels

--- .guix/channels.scm
+++ .guix/channels.scm
@@ -2,9 +2,9 @@
  (channel
   (name 'guix)
   (url "https://git.savannah.gnu.org/git/guix.git")
-  (commit "10f3dd0e9e06d71d1bc1615c6a60cc3aa1ad1ff4"))
+  (commit "425cf1fbe2fff25bda1b5eb948ce01170d23ab6a"))
  (channel
   (name 'guix-extra)
   (url "https://gitlab.inria.fr/mfelsoci/guix-extra.git")
   (branch "master")
-  (commit "359a0f122d9ca5845358f60ae4a25a73dc6c978f")))
+  (commit "047acf09f17d8bfc26cc2be014934ea9416814f8")))
