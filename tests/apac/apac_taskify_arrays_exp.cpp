b8c88236bf316dd3f9e27a8a79a47b7535b41b90
Marek FELŠÖCI <marek.felsoci@inria.fr>
2024-06-28 20:12:45 +0200

    Update array taskification test

--- tests/apac/apac_taskify_arrays_exp.cpp
+++ tests/apac/apac_taskify_arrays_exp.cpp
@@ -3,20 +3,20 @@ void g(int* tab) { tab[0] += 42; }
 void f(int* tab1, int* tab2, const int size) {
 #pragma omp taskgroup
   {
-#pragma omp task default(shared) depend(inout : tab1[0])
-    g(tab1);
     int a = 2, b = 0;
-#pragma omp task default(shared) depend(in : b) depend(inout : tab1[2])
-    tab1[2] = 2 + b;
-#pragma omp task default(shared) depend(in : tab1[3]) depend(inout : tab2[3])
-    tab2[3] = 4 * tab1[3];
-#pragma omp task default(shared) depend(in : tab2[0]) depend(inout : tab1[0])
-    tab1[0] = tab2[0];
-#pragma omp task default(shared) depend(in : a) depend(inout : tab2[4])
-    tab2[4] = 4 * a;
-#pragma omp task default(shared) depend(inout : tab1[0])
+#pragma omp task default(shared) depend(in : tab1) depend(inout : tab1[0])
     g(tab1);
-#pragma omp task default(shared) depend(inout : tab2[0])
+#pragma omp taskwait depend(in : b, tab1) depend(inout : tab1[2])
+    tab1[2] = 2 + b;
+#pragma omp taskwait depend(in : a, tab2) depend(inout : tab2[4])
+    tab2[4] = 4 * a;
+#pragma omp taskwait depend(in : tab1, tab2, tab2[0]) depend(inout : tab1[0])
+    tab1[0] = tab2[0];
+#pragma omp taskwait depend(in : tab1, tab1[3], tab2) depend(inout : tab2[3])
+    tab2[3] = 4 * tab1[3];
+#pragma omp task default(shared) depend(in : tab1) depend(inout : tab1[0])
+    g(tab1);
+#pragma omp task default(shared) depend(in : tab2) depend(inout : tab2[0])
     g(tab2);
   __apac_exit:;
   }
