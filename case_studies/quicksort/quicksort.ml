bfdb44e16bf35365b6a7b1f17605f8db73ffbb6d
Marek FELŠÖCI <marek.felsoci@inria.fr>
2024-08-29 12:19:09 +0200

    Keep graphs

--- case_studies/quicksort/quicksort.ml
+++ case_studies/quicksort/quicksort.ml
@@ -6,7 +6,7 @@ open Ast
 let _ = Run.script_cpp (fun () ->
             let _ = Flags.code_print_width := 1024 in
             let _ = Apac_macros.instrument_code := false in
-            let _ = Apac_macros.verbose := false in
+            let _ = Apac_macros.keep_graphs := true in
             let _ = Apac_macros.apac_main := "main" in
             (* Target all of the function definitions except for the 'main'
                function. *)
