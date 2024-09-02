4911ba3d084bc9ec5258a4f79794bfc366693105
Marek FELŠÖCI <marek.felsoci@inria.fr>
2024-09-02 18:20:24 +0200

    Update reset function

--- src/apac/apac_reset.ml
+++ src/apac/apac_reset.ml
@@ -2,5 +2,6 @@ open Ast
 
 (** [tnt_blast]: clears and resets all the data structures. *)
 let tnt_blast () : unit =
-  Apac_records.mutables := Apac_dep.Dep_map.empty;
+  Var_Hashtbl.clear Apac_records.functions;
+  Apac_records.mutables := Apac_dep.Dep_map.empty
 
