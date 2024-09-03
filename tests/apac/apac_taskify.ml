81544765628afee89cf45f14e9e4a5fbb5dbd646
Marek FELŠÖCI <marek.felsoci@inria.fr>
2024-09-03 20:26:04 +0200

    Test new prologue and epilogue

--- tests/apac/apac_taskify.ml
+++ tests/apac/apac_taskify.ml
@@ -13,60 +13,47 @@ let _ = Run.script_cpp (fun () ->
                 nbAny;
                 cFunDefAndDecl ""
               ];
-            let candidates = ref Var_set.empty in
-            Apac_taskify.select_candidates candidates [
+            !! Apac_prologue.select_candidates [
                 nbAny;
                 cFunDefAndDecl ""
               ];
-            Apac_taskify.select_callers candidates [
+            !! Apac_prologue.unify_returns [
                 nbAny;
-                cFunDefAndDecl ""
-              ];
-            !! Apac_taskify.parallel_task_group
-              ~mark_group:true ~candidates:(Some candidates) [
-                nbAny;
-                cFunDefAndDecl ""
+                cMark Apac_macros.candidate_mark
               ];
             !! Apac_taskify.taskify [
                 nbAny;
-                cMark Apac_macros.task_group_mark
+                cMark Apac_macros.candidate_body_mark
               ];
             !! Apac_taskify.merge [
                 nbAny;
-                cMark Apac_macros.task_group_mark
+                cMark Apac_macros.candidate_body_mark
               ];
             !! Apac_taskify.detect_tasks_simple [
                 nbAny;
-                cMark Apac_macros.task_group_mark
+                cMark Apac_macros.candidate_body_mark
               ];
             !! Apac_epilogue.synchronize_subscripts [
                 nbAny;
-                cMark Apac_macros.task_group_mark
+                cMark Apac_macros.candidate_body_mark
               ];
             !! Apac_epilogue.place_barriers [
                 nbAny;
-                cMark Apac_macros.task_group_mark
+                cMark Apac_macros.candidate_body_mark
               ]; 
             !! Apac_taskify.insert_tasks [
                 nbAny;
-                cMark Apac_macros.task_group_mark
-              ];
-            !! Marks.remove Apac_macros.task_group_mark [
+                cMark Apac_macros.candidate_body_mark
+              ]; 
+            !! Apac_epilogue.place_task_group [
                 nbAny;
-                cMark Apac_macros.task_group_mark
+                cMark Apac_macros.candidate_body_mark
               ];
             !! Apac_epilogue.heapify [
                 nbAny;
                 cOr [[cMark Apac_macros.heapify_mark];
                      [cMark Apac_macros.heapify_breakable_mark]]
               ];
-            !! Marks.remove Apac_macros.heapify_mark [
-                nbAny;
-                cMark Apac_macros.heapify_mark
-              ];
-            !! Marks.remove Apac_macros.heapify_breakable_mark [
-                nbAny;
-                cMark Apac_macros.heapify_breakable_mark
-              ];
+            !! Apac_epilogue.clear_marks ()
           );
         Apac_reset.tnt_blast ()
