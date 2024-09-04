5aa931ec0db1bcff1c8662d4d17a34955eb00876
Marek FELŠÖCI <marek.felsoci@inria.fr>
2024-09-04 13:58:19 +0200

    Reset flags too

--- src/apac/apac_reset.ml
+++ src/apac/apac_reset.ml
@@ -1,7 +1,12 @@
 open Ast
 
-(** [tnt_blast]: clears and resets all the data structures. *)
+(** [tnt_blast]: clears and resets all the data structures and flags. *)
 let tnt_blast () : unit =
   Var_Hashtbl.clear Apac_records.functions;
-  Apac_records.mutables := Apac_dep.Dep_map.empty
+  Apac_records.mutables := Apac_dep.Dep_map.empty;
+  Apac_macros.instrument_code := false;
+  Apac_macros.keep_graphs := false;
+  Apac_macros.verbose := false;
+  Apac_macros.apac_main := "main";
+  Flags.code_print_width := 80
 
