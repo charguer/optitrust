df8c3a584635e135b8dfa0ae484f59d651dcfa85
Marek FELŠÖCI <marek.felsoci@inria.fr>
2024-09-11 19:12:26 +0200

    Update Guix channel specification

--- .guix/channels.scm
+++ .guix/channels.scm
@@ -4,7 +4,6 @@
   (url "https://git.savannah.gnu.org/git/guix.git")
   (commit "425cf1fbe2fff25bda1b5eb948ce01170d23ab6a"))
  (channel
-  (name 'guix-extra)
-  (url "https://gitlab.inria.fr/mfelsoci/guix-extra.git")
-  (branch "master")
-  (commit "047acf09f17d8bfc26cc2be014934ea9416814f8")))
+  (name 'apac-channel)
+  (url "https://gitlab.inria.fr/apac/channel.git")
+  (commit "4be7a168db403e78cb3ef552c2e80d77dd03c0ec")))
