79500c030d14480cbd1115cc09f43cff5c3b376d
Marek FELŠÖCI <marek.felsoci@inria.fr>
2024-09-13 19:06:37 +0200

    Comment modelize and optimize

--- src/apac/apac_profiler.ml
+++ src/apac/apac_profiler.ml
@@ -180,11 +180,23 @@ let codegen (sections : trm Stack.t) (v : TaskGraph.V.t) : trms =
         surrounded with the profiling statements we have just built. *)
     opening :: (params @ [before] @ t.current @ [after])
 
+(** [modelize tg]: expects the target [tg] to point at the entire abstract
+    syntax tree featuring profiling instructions. It then generates the
+    corresponding C source code, compiles it, runs it and modelizes the
+    execution time of the different profiling sections in it. *)
 let modelize (tg : target) : unit =
+  (** [IllFormedModel, UnknownParameter, UnknownTaskCandidate, IllFormedPower]:
+      custom exceptions for handling local-specific errors. *)
   let exception IllFormedModel of string in
   let exception UnknownParameter of int in
   let exception UnknownTaskCandidate of int in
   let exception IllFormedPower of string in
+  (** [modelize.one line]: an auxiliary function to parse one [line] of an
+      execution time model and return the profiling section identifier, i.e. the
+      hash of the corresponding task candidate vertex, as well as the formula,
+      in its string form, for estimating the execution time of the latter. If
+      the parsing fails, it raises [IllFormedModel] with the problematic [line]
+      as an argument. *)
   let one (line : string) : int * string =
     let regex =
       Str.regexp "\\([0-9]+\\)(nbparams=[0-9]+)=\\(None\\|[^=\n]+\\)" in
@@ -195,15 +207,33 @@ let modelize (tg : target) : unit =
     else
       raise (IllFormedModel line)
   in
+  (** [modelize.process model]: an auxiliary function to read an execution time
+      [model] file line-by-line, extract and transpile the execution time
+      estimation formulas into their abstract syntax tree form and store them in
+      the global hash table [formulas]. *)
   let process (model : string) : unit =
+    (** [modelize.process.to_double c]: an auxiliary function to transpile the
+        string representation of the double floating-point value [c] into an
+        abstract syntax tree literal value term. *)
     let to_double (c : string) : trm =
       try
         let v = Val_lit (Lit_double (Float.of_string c)) in trm_val v
       with
       | Failure _ ->
-         failwith ("[modelize]: `" ^ c ^ "' is not a valid floating-point \
-                                          constant.")
+         failwith ("Apac_profiler.modelize: `" ^ c ^
+                     "' is not a valid floating-point constant.")
     in
+    (** [modelize.process.pow p op]: an auxiliary function to transpile the
+        string representation [op] of a power expression consisting of a call to
+        the [apac_fpow] function (see [!Apac_epilogue.dynamic_cutoff]) into an
+        adequate abstract syntax tree term. The function also replaces any
+        occurence to the generic variable name of a profiling parameter matching
+        [X\[0-9\]+] in [op] by the corresponding abstract syntax tree term
+        thanks to the binding map [p] (see [!parameters] and [!codegen]). If
+        there is no such binding in [p], the function raises [UnknownParameter]
+        and passes the problematic identifier, i.e. the integer following [X],
+        as an argument. If it fails to decode the power expression, it raises
+        [IllFormedPower] and passes [op] as an argument. *)
     let pow (p : trm Int_map.t) (op : string) : trm =
       let re = Str.regexp "apac_fpow<\\([0-9]+\\)>(X\\([0-9]+\\))" in
       if Str.string_match re op 0 then
@@ -212,13 +242,26 @@ let modelize (tg : target) : unit =
         if Int_map.mem base p then
           let exp = trm_val (Val_lit (Lit_int exp)) in
           let base = Int_map.find base p in
+          (** Building the power function variable term using
+              [trm_toplevel_free_var] allows us to use it without declaring
+              it in the abstract syntax tree of the input program. *)
           let f = trm_toplevel_free_var "apac_fpow" in
+          (** The resulting call is like [apac_fpow(exponent, base)]. *)
           trm_apps f [exp; base]
         else
           raise (UnknownParameter base)
       else
         raise (IllFormedPower op)
     in
+    (** [modelize.process.mul p op]: an auxiliary function to transpile the
+        string representation [op] of a multiplication expression into an
+        adequate abstract syntax tree term. The function also replaces any
+        occurence to the generic variable name of a profiling parameter matching
+        [X\[0-9\]+] in [op] by the corresponding abstract syntax tree term
+        thanks to the binding map [p] (see [!parameters] and [!codegen]). If
+        there is no such binding in [p], the function raises [UnknownParameter]
+        and passes the problematic identifier, i.e. the integer following [X],
+        as an argument. *)
     let mul (p : trm Int_map.t) (op : string) : trm =
       let re = Str.regexp "\\*" in
       let ops = Str.split re op in
@@ -237,6 +280,9 @@ let modelize (tg : target) : unit =
                   ) ops in
       List.fold_left (fun acc op -> trm_mul acc op) (List.hd ops) (List.tl ops)
     in
+    (** This is the core of the [process] function. It begins by skipping the
+        first line of [model] giving the number of entries in the file. We do
+        not need this information here. *)
     let model' = open_in model in
     begin
       try
@@ -245,16 +291,28 @@ let modelize (tg : target) : unit =
       | End_of_file ->
          begin
            close_in model';
-           failwith ("[modelize]: `" ^ model ^ "' is not a valid model file.")
+           failwith ("Apac_profiler.modelize: `" ^ model ^
+                       "' is not a valid model file.")
          end
     end;
+    (** We simply read [model] line-by-line until the end of the file. *)
     try
       while true do
+        (** For each [line] we read, *)
         let line = input_line model' in
+        (** we extract the profiling section identifier, i.e. the hash [v] of
+            the corresponding task candidate vertex, as well as the formula [f],
+            in its string form, for estimating the execution time of the
+            latter. *)
         let (v, f) = one line in
+        (** If [!parameters] has a binding for [v], retrieve it. *)
         if (Hashtbl.mem parameters v) then
           let p = Hashtbl.find parameters v in
+          (** If the modelization judges [v] is worth of becoming a
+              parallelizable task, i.e. when [f] is not [None], *)
           if f <> "None" then
+            (** transpile the formula from its string form into the
+                corresponding abstract syntax tree term and *)
             let re = Str.regexp "\\+" in
             let ops = Str.split re f in
             let ops = List.map String.trim ops in
@@ -266,79 +324,141 @@ let modelize (tg : target) : unit =
                                   else to_double op in
                          trm_add acc op
                        ) hd ops in
+            (** store it for [v] in [formulas]. *)
             Hashtbl.add formulas v (Some ft)
           else
+            (** Otherwise, indicate in [formulas] that [v] has no execution time
+                estimation formula associated to it. *)   
             Hashtbl.add formulas v None
         else
+          (** Otherwise, raise [UnknownTaskCandidate] and pass the problematic
+              hash [v] as an argument. *)
           raise (UnknownTaskCandidate v)
       done
     with
+    (** Close [model] when we reach the end of the file. *)
     | End_of_file -> close_in model'
+    (** Other exceptions indicate an irreparable error. Show an adequate error
+        message. *)
     | IllFormedModel m ->
-       failwith ("[modelize]: `" ^ m ^ "' is not a valid performance model.")
+       failwith ("Apac_profiler.modelize: `" ^ m ^
+                   "' is not a valid performance model.")
     | UnknownParameter p ->
-       failwith ("[modelize]: unknown parameter `" ^ (string_of_int p) ^ "'.")
+       failwith ("Apac_profiler.modelize: unknown parameter `" ^
+                   (string_of_int p) ^ "'.")
     | UnknownTaskCandidate c ->       
        failwith
-         ("[modelize]: unknown task candidate `" ^ (string_of_int c) ^ "'.")
+         ("Apac_profiler.modelize: unknown task candidate `" ^
+            (string_of_int c) ^ "'.")
     | IllFormedPower p ->
-       failwith ("[modelize]: `" ^ p ^ "' is not a valid power expression.")
+       failwith ("Apac_profiler.modelize: `" ^ p ^
+                   "' is not a valid power expression.")
     | e -> failwith
-             ("[modelize]: unexpected error occurred (" ^
+             ("Apac_profiler.modelize: unexpected error occurred (" ^
                 (Printexc.to_string e) ^ ")")
   in
+  (** This is the core of the [modelize] pass. *)
   Target.iter_at_target_paths (fun t ->
+      (** Generate file names for the [header] with profiling class definitions,
+          the temporary source [code] file representing the program with
+          profiling instructions, the corresponding [binary], [profile] and
+          execution time [model] files. *)
       let (header, code, binary, profile, model) =
         Apac_miscellaneous.hcbpm () in
+      (** Synthesize the contents of the header with profiling class definitions
+          and save it as [header]. *)
       Apac_miscellaneous.profiler_hpp profile header;
+      (** Take the current abstract syntax tree of the program and output it as
+          C++ source code to [code]. *)
       Trace.output_prog ~ast_and_enc:false (Trace.get_context ()) code t;
+      (** Compile [code] into [binary]. *)
       let err = Sys.command ("g++ -o " ^ binary ^ " " ^ code ^ ".cpp") in
       if err <> 0 then
-        failwith "[modelize]: could not compile source code with profiling \
-                  instructions."
+        failwith "Apac_profiler.modelize: could not compile source code with \
+                  profiling instructions."
       else
+        (** Run [binary] which produces [profile]. *)
         let err = Sys.command binary in
         if err <> 0 then
-          failwith "[modelize]: could not perform the profiling run."
+          failwith "Apac_profiler.modelize: could not perform the profiling \
+                    run."
         else
+          (** Run the modelizer script on [profile] which produces [model]. *)
           let err = Sys.command ("python3 -m apac_modelizer " ^ profile) in
           if err <> 0 then
-            failwith "[modelize]: could not compute execution time model."
+            failwith "Apac_profiler.modelize: could not compute execution time \
+                      model."
           else
-            let _ = process model in
-            Hashtbl.iter (fun v f ->
-                match f with
-                | Some f -> Printf.printf "%d: %s\n" v (AstC_to_c.ast_to_string f)
-                | None -> Printf.printf "%d: None\n" v
-              ) formulas
-          
+            begin
+              (** Parse [model]. *)
+              process model;
+              (** Dump the resulting execution time estimation formulas when in
+                  verbose mode. *)
+              if !Apac_macros.verbose then
+                begin
+                  Printf.printf
+                    "Execution time estimation formulas (task candidate hash, \
+                     formula):\n";
+                  Hashtbl.iter (fun v f ->
+                      match f with
+                      | Some f ->
+                         Printf.printf "(%d: %s)\n" v
+                           (AstC_to_c.ast_to_string f)
+                      | None ->
+                         Printf.printf "(%d: None)\n" v
+                    ) formulas
+                end
+            end          
     ) tg
 
+(** [optimize tg]: expects the target [tg] to point at a function body. It then
+    optimizes the task candidate graph of the function according to the results
+    of execution time modelization. *)
 let optimize (tg : target) : unit =
+  (** [optimize.aux v]: an auxiliary function to recursively optimize a task
+      candidate vertex [v]. *)
   let rec aux (v : TaskGraph.V.t) : unit =
+    (** Get the label [t] of [v]. *)
     let t = TaskGraph.V.label v in
+    (** Proceed only if [v] is an eligible task candidate, i.e. it carries the
+        [Taskifiable] attribute. *)
     if (Task.attributed t Taskifiable) then
       begin
+        (** In such case, compute the hash of [v] *)
         let h = TaskGraph.V.hash v in
         let f =
+          (** so as to retrieve the corresponding execution time estimation
+              formula in [!formulas]. *)
           try
             Hashtbl.find formulas h
           with
           | Not_found ->
              failwith ("Apac_profiler.optimize: task candidate `" ^
                          (Task.to_string t) ^ "' has no performance model.") in
+        (** If there is no formula, the modelization judges [v] is not an
+            eligible candidate. *)
         if Option.is_none f then
+          (** Remove the [Taskifiable] attribute from its set of task candidate
+              attributes. *)
           t.attrs <- TaskAttr_set.remove Taskifiable t.attrs
         else
+          (** Otherwise, check whether the formula consists of a constant
+              expression, i.e. does not feature any function calls or other
+              operations. *)
           let const = trm_fold (fun acc t ->
                           match t.desc with
                           | Trm_apps _ -> acc && false
                           | _ -> acc && true
                         ) true (Option.get f) in
+          (** Attribute [f] to [v] only if [f] does not consist of a constant
+              expression. In the opposite case, the modelization judges [v] an
+              eligible task candidate with a constant execution time. Therefore,
+              we do not need to control whether [v] becomes a parallelizable
+              task at runtime. We already know it's worth it. *)
           if (not const) then
             t.cost <- f
       end;
-    (** When [v] features nested candidate graphs, explore the substatements. *)
+    (** When [v] features nested candidate graphs, process the substatements. *)
     List.iter (fun gl ->
         List.iter (fun go ->
             TaskGraphTraverse.iter aux go
