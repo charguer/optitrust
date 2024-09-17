e5795cb5e04cafa6422c711ac16d15c561328337
Marek FELŠÖCI <marek.felsoci@inria.fr>
2024-09-17 15:53:14 +0200

    Update 'apac-channel

--- .guix/channels.scm
+++ .guix/channels.scm
@@ -6,4 +6,4 @@
  (channel
   (name 'apac-channel)
   (url "https://gitlab.inria.fr/apac/channel.git")
-  (commit "8980ba0e6a28fe503994b1cee068458028754f0f")))
+  (commit "557554d38630a8f54c0ff146369d898aa900c896")))
