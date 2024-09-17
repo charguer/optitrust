f206d196c456a349a4eb35a039494cfe06cd427b
Marek FELŠÖCI <marek.felsoci@inria.fr>
2024-09-17 18:34:56 +0200

    Compile and run Quicksort as standalone executable

--- case_studies/quicksort/quicksort.ml
+++ case_studies/quicksort/quicksort.ml
@@ -1,69 +1,10 @@
 open Optitrust
-open Target
-open Typ
-open Ast
 
-let _ = Run.script_cpp (fun () ->
-            Flags.code_print_width := 1024;
-            Apac_flags.instrument := false;
-            Apac_flags.keep_graphs := true;
-            !! Apac_prologue.build_records [
-                nbAny;
-                cFunDefAndDecl ""
-              ];
-            !! Apac_prologue.select_candidates [
-                nbAny;
-                cFunDefAndDecl ""
-              ];
-            !! Apac_prologue.unify_returns [
-                nbAny;
-                cMark Apac_macros.candidate_mark
-              ];
-            !! Apac_taskify.taskify [
-                nbAny;
-                cMark Apac_macros.candidate_body_mark
-              ];
-            !! Apac_taskify.merge [
-                nbAny;
-                cMark Apac_macros.candidate_body_mark
-              ];
-            !! Apac_taskify.detect_tasks_simple [
-                nbAny;
-                cMark Apac_macros.candidate_body_mark
-              ];
-            ?? (fun () ->
-                !! Apac_backend.profile_tasks [
-                    nbAny;
-                    cMark Apac_macros.candidate_body_mark
-                  ];
-                !! Apac_epilogue.clear_marks ();
-                !! Apac_profiler.modelize []
-              );
-            !! Apac_profiler.optimize [
-                nbAny;
-                cMark Apac_macros.candidate_body_mark
-              ];
-            !! Apac_epilogue.synchronize_subscripts [
-                nbAny;
-                cMark Apac_macros.candidate_body_mark
-              ];
-            !! Apac_epilogue.place_barriers [
-                nbAny;
-                cMark Apac_macros.candidate_body_mark
-              ];
-            !! Apac_backend.insert_tasks [
-                nbAny;
-                cMark Apac_macros.candidate_body_mark
-              ];
-            !! Apac_epilogue.place_task_group [
-                nbAny;
-                cMark Apac_macros.candidate_body_mark
-              ];
-            !! Apac_epilogue.heapify [
-                nbAny;
-                cOr [[cMark Apac_macros.heapify_mark];
-                     [cMark Apac_macros.heapify_breakable_mark]]
-              ];
-            !! Apac_epilogue.dynamic_cutoff [];
-            !! Apac_epilogue.clear_marks ()
-          )
+let () =
+  Flags.code_print_width := 1024;
+  Apac_flags.profile := true;
+  Apac_flags.keep_graphs := true
+
+let () = Run.apac
+           "./case_studies/quicksort/quicksort.cpp"
+           "./case_studies/quicksort/quicksort_parallel.cpp"
