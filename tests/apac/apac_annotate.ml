874852c1f0858212b2f3a02e19c5eaff1ec2fe29
Marek FELŠÖCI <marek.felsoci@inria.fr>
2024-09-19 18:52:06 +0200

    Fix apac_profile test and rename it to apac_annotate

---
+++ tests/apac/apac_annotate.ml
@@ -0,0 +1,36 @@
+open Optitrust
+open Target 
+
+let _ = Run.script_cpp (fun () ->
+            !! Apac_preprocessing.build_records [
+                nbAny;
+                cFunDefAndDecl ""
+              ];
+            !! Apac_preprocessing.Constification.constify
+              ~frs:(Some Apac_records.functions) [
+                nbAny;
+                cFunDefAndDecl ""
+              ];
+            !! Marks_basic.add Apac_macros.candidate_mark [
+                nbAny;
+                cFunDefAndDecl "c"
+              ];
+            !! Apac_preprocessing.unify_returns [
+                nbAny;
+                cMark Apac_macros.candidate_mark
+              ];
+            !! Apac_task_candidate_discovery.taskify [
+                nbAny;
+                cMark Apac_macros.candidate_body_mark
+              ];
+            !! Apac_task_candidate_discovery.detect_tasks_simple [
+                nbAny;
+                cMark Apac_macros.candidate_body_mark
+              ];
+            !! Apac_profiling.annotate [
+                nbAny;
+                cMark Apac_macros.candidate_body_mark
+              ];
+            !! Apac_parallelization.clear_marks ();
+          );
+        Apac_reset.tnt_blast ()
---
+++ tests/apac/apac_annotate_exp.cpp
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+
+#include "apac_profiling.hpp"
+void f(int* tab) { tab[0] += 42; }
+
+void g(const int* tab) {}
+
+void h(const int* tab) {}
+
+void p(int v) {
+  int a = 15;
+  int b = a + 2;
+  int c = a + b + v++;
+}
+
+void r(int v, int z) { int a = 15 + z, b = a + 2, c = a + b + v++; }
+
+void c(int* tab, int size) {
+  {
+    apac_s __apac_section17;
+    apac_s __apac_section16;
+    apac_s __apac_section22;
+    apac_s __apac_section21;
+    apac_s __apac_section12;
+    __apac_section12.initialize("12");
+    __apac_section12.before();
+    f(tab);
+    __apac_section12.after();
+    int i;
+    for (i = 0; i < size; i++) {
+      tab[i] += 2;
+      __apac_section21.initialize("21");
+      __apac_section21.add(tab[i]);
+      __apac_section21.before();
+      p(tab[i]);
+      __apac_section21.after();
+      __apac_section22.initialize("22");
+      __apac_section22.add(tab[i]);
+      __apac_section22.before();
+      p(tab[i]);
+      __apac_section22.after();
+    }
+    __apac_section16.initialize("16");
+    __apac_section16.before();
+    g(tab);
+    __apac_section16.after();
+    __apac_section17.initialize("17");
+    __apac_section17.before();
+    h(tab);
+    __apac_section17.after();
+  __apac_exit:;
+  }
+}
+
+int main() {
+  int* t = (int*)malloc(4 * sizeof(int));
+  c(t, 4);
+  free(t);
+  return 0;
+}
