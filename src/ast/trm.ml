56155f21894b5d792283048f08e671caf92579f5
Marek FELŠÖCI <marek.felsoci@inria.fr>
2024-09-26 15:17:59 +0200

    Simplify trm_fold

--- src/ast/trm.ml
+++ src/ast/trm.ml
@@ -983,28 +983,32 @@ let trm_iter (f : trm -> unit) (t : trm) : unit =
     ignore (trm_combinators_unsupported_case "trm_iter" t)
   | Trm_val _ | Trm_var _ | Trm_goto _  | Trm_extern _ | Trm_omp_routine _  | Trm_template _ | Trm_using_directive _ -> ()
 
-(** [trm_fold f acc t]: similar to [!List.fold_left] but for a term [t]. *)
+(** [trm_fold f acc t]: similar to [!List.fold_left], but for a term [t]. *)
 let rec trm_fold (f : 'a -> trm -> 'a) (acc : 'a) (t : trm) : 'a =
+  (** [trm_fold.trms_fold acc ts]: like [!trm_fold], but on a list of terms (see
+      type [!type:trms]). *)
+  let rec trms_fold (acc : 'a) (ts : trms) : 'a =
+    match ts with
+    | h :: tl -> trms_fold (trm_fold f acc h) tl
+    | [] -> acc
+  in
   match t.desc with
-  | Trm_array tl ->
-     Mlist.fold_left (trm_fold f) (f acc t) tl
-  | Trm_record tl ->
-     let tl = Mlist.to_list tl in
-     let (_, tl) = List.split tl in
-     let tl = Mlist.of_list tl in
-     Mlist.fold_left (trm_fold f) (f acc t) tl
+  | Trm_array { items; _ } ->
+     trms_fold (f acc t) items
+  | Trm_record { items; _ } ->
+     let (_, items) = List.split items in trms_fold (f acc t) items
   | Trm_let (vk, tv, init, _) ->
      trm_fold f (f acc t) init
-  | Trm_let_mult (vk, tvl, tl) ->
-     List.fold_left (trm_fold f) (f acc t) tl
+  | Trm_let_mult (_, _, tl) ->
+     trms_fold (f acc t) tl
   | Trm_let_fun (f', res, args, body, _) ->
      trm_fold f (f acc t) body
   | Trm_if (cond, then_, else_) ->
      trm_fold f (trm_fold f (trm_fold f (f acc t) cond) then_) else_
-  | Trm_seq tl ->
-     Mlist.fold_left (trm_fold f) (f acc t) tl
+  | Trm_seq { items; _ } ->
+     trms_fold (f acc t) items
   | Trm_apps (func, args) ->
-     List.fold_left (trm_fold f) (f acc t) (func :: args)
+     trms_fold (f acc t) (func :: args)
   | Trm_while (cond, body) ->
      trm_fold f (trm_fold f (f acc t) cond) body
   | Trm_for_c (init, cond, step, body, _) ->
@@ -1019,13 +1023,13 @@ let rec trm_fold (f : 'a -> trm -> 'a) (acc : 'a) (t : trm) : 'a =
        | Step sp -> tl @ [sp]
      in
      let tl = tl @ [body] in
-     List.fold_left (trm_fold f) (f acc t) tl
+     trms_fold (f acc t) tl
   | Trm_do_while (body, cond) ->
      trm_fold f (trm_fold f (f acc t) body) cond
   | Trm_switch (cond, cases) ->
      let (_, tl) = List.split cases in
      let tl = cond :: tl in
-     List.fold_left (trm_fold f) (f acc t) tl
+     trms_fold (f acc t) tl
   | Trm_abort a ->
      begin match a with
      | Ret (Some t') -> trm_fold f (f acc t) t'
@@ -1043,7 +1047,7 @@ let rec trm_fold (f : 'a -> trm -> 'a) (acc : 'a) (t : trm) : 'a =
                      | Record_field_method t1 -> t1 :: acc
                      | _ -> acc
                    ) rfl [] in
-        List.fold_left (trm_fold f) (f acc t) tl
+        trms_fold (f acc t) tl
      | _ -> f acc t
      end
   | Trm_fun (_, _, body, _) ->
