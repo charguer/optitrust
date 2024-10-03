c560dc65b4dd0fe26a228af3aa1a66ec2c77a819
Marek FELŠÖCI <marek.felsoci@inria.fr>
2024-10-04 01:04:28 +0200

    Simplify

--- tests/apac/apac_taskify.ml
+++ tests/apac/apac_taskify.ml
@@ -2,8 +2,6 @@ open Optitrust
 
 let () =
   Flags.code_print_width := 1024;
-  Apac_flags.constify := true
-
-let () =
+  Apac_flags.constify := true;
   Run.script_cpp Apac_main.compile;
   Apac_reset.tnt_blast ()
