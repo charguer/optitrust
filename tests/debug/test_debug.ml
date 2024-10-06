cf773e57e1a4462f94ea1d756d81dd821ae9a380
charguer <arthur@chargueraud.org>
2024-10-06 22:34:04 +0200

    cleanup opencv end

--- tests/debug/test_debug.ml
+++ tests/debug/test_debug.ml
@@ -2,18 +2,14 @@ open Optitrust
 open Prelude
 
 let _ = Run.script_cpp (fun () ->
-    !! Function.use_infix_ops ~indepth:true [];
-
+  !! Function.use_infix_ops ~indepth:true [];
+  !! Arith_basic.(simpls_rec [expand_rec; euclidian; gather_rec]) [nbMulti; cAccesses()];
   !! Loop.shift (StartAtZero) [nbMulti; cFor "i"];
-  !!! Arith_basic.(simpls_rec [expand_rec; euclidian; gather_rec]) [nbMulti; cAccesses()];
-
   let scale_range_for_cn (cn:int) =
     Loop_basic.scale_range ~factor:(trm_int cn) [nbMulti; cIf ~cond:[sExpr ("cn == " ^ string_of_int cn)] (); dThen; cFor "i"] in
   !! List.iter scale_range_for_cn [3; 4];
-
+  !! Loop_basic.scale_range ~factor:(var "cn") [nbMulti; cIf ~cond:[sExpr "cn == 4"] (); dElse; cFor "i"];
   (* repase needed before simplifications *)
   !! Arith_basic.(simpls_rec [expand_rec; gather_rec; compute]) [nbMulti; cAccesses()];
-
   !!! Arith_basic.simplify [nbMulti; cAccesses()];
-
 )
