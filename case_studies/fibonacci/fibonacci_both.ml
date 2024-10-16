03fbf9f8ee913476eedd0eab9b0e0aba319f45c2
Marek FELŠÖCI <marek.felsoci@inria.fr>
2024-10-16 17:27:21 +0200

    Generate four versions of Fibonacci

---
+++ case_studies/fibonacci/fibonacci_both.ml
@@ -0,0 +1,9 @@
+open Optitrust
+
+let () =
+  Flags.code_print_width := 1024;
+  Apac_flags.profile := true;
+  Apac_flags.cutoff_count_and_depth := true;
+  Run.apac
+    "./case_studies/fibonacci/fibonacci.cpp"
+    "./case_studies/fibonacci/fibonacci_parallel.cpp"
---
+++ case_studies/fibonacci/fibonacci_none.ml
@@ -0,0 +1,6 @@
+open Optitrust
+
+let () =
+  Run.apac
+    "./case_studies/fibonacci/fibonacci.cpp"
+    "./case_studies/fibonacci/fibonacci_parallel.cpp"
