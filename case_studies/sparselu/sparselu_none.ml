5b77562778f08efdfabdb6dd9b730005f1941282
Marek FELŠÖCI <marek.felsoci@inria.fr>
2024-10-18 16:24:36 +0200

    Remove verbose flag

--- case_studies/sparselu/sparselu_none.ml
+++ case_studies/sparselu/sparselu_none.ml
@@ -3,7 +3,6 @@ open Optitrust
 let () =
   Flags.code_print_width := 1024;
   Apac_flags.constify := true;
-  Apac_flags.verbose := true;
   Run.apac
     "./case_studies/sparselu/sparselu.cpp"
     "./case_studies/sparselu/sparselu_parallel.cpp"
