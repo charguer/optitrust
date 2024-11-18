e0a3e2915dce6655cd3d343ce5d03272e29e7268
Thomas Koehler <thomas.koehler@thok.eu>
2024-11-18 18:16:29 +0100

    elim_mops ~simpl

--- lib/framework/transfo/cleanup.ml
+++ lib/framework/transfo/cleanup.ml
@@ -5,7 +5,7 @@ let%transfo std (_u: unit) : unit =
   Trace.tag "pre-post-processing";
   Function.use_infix_ops ~indepth:true [];
   Flags.recompute_resources_between_steps := false;
-  Matrix.elim_mops [];
-  Arith.(simpl_surrounding_expr (compose [expand; euclidian; gather_rec])) [nbMulti; cAccesses()];
+  (* should: this be a transfo instead of trm -> trm ? *)
+  Matrix.elim_mops ~simpl:(Arith_core.(simplify false Arith_basic.(compose [expand; euclidian; gather_rec; compute]))) [];
   Resources.delete_annots [];
   Loop.delete_all_void []
