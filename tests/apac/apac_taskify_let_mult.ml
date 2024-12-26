ad9fc9f6bd21e980841ba7958eb221bef67b5942
Marek Felšöci <marek.felsoci@inria.fr>
2024-12-26 16:39:48 +0100

    Disable verbose output in test

--- tests/apac/apac_taskify_let_mult.ml
+++ tests/apac/apac_taskify_let_mult.ml
@@ -2,7 +2,7 @@ open Optitrust
 
 let () =
   Flags.code_print_width := 1024;
-  Apac_flags.verbose := true;
+  Apac_flags.verbose := false;
   Apac_flags.constify := true;
   Run.script_cpp Apac_main.compile;
   Apac_reset.tnt_blast ()
