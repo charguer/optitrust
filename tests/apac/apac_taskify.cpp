f12257eb9f6eeedaf21d0489ff163d968ded2141
Marek Felšöci <marek@felsoci.sk>
2025-03-17 13:59:18 +0100

    Extend taskification test

--- tests/apac/apac_taskify.cpp
+++ tests/apac/apac_taskify.cpp
@@ -10,13 +10,25 @@ void p(int * v) { int a = 15; int b = a + 2; int c = a + b + (*v)++; }
 
 void r(int v, int z) { int a = 15 + z, b = a + 2, c = a + b + v++; }
 
+int w(int a) {  return a + 1; }
+
 void c(int * tab, int size) {
   int i;
+  int l = size;
+  int r, s = 1;
   f(tab);
   for(i = 0; i < size; i++) {
     tab[i] += 2;
     p(&tab[i]);
     p(&tab[i]);
+  }
+  int * p = tab;
+  while (l > 1 || s) {
+    r = w(l);
+    *p = r;
+    p++;
+    l /= r;
+    s = 0;
   }
   g(tab);
   h(tab);
--- tests/apac/apac_taskify_exp.cpp
+++ tests/apac/apac_taskify_exp.cpp
@@ -18,10 +18,15 @@ void r(int v, int z) {
   int c = a + b + v++;
 }
 
+int w(int a) { return a + 1; }
+
 void c(int* tab, int size) {
 #pragma omp taskgroup
   {
     int i;
+    int l = size;
+    int r;
+    int s = 1;
 #pragma omp task default(shared) depend(in : tab) depend(inout : tab[0])
     f(tab);
     for (i = 0; i < size; i++) {
@@ -31,6 +36,17 @@ void c(int* tab, int size) {
         p(&tab[i]);
         p(&tab[i]);
       }
+    }
+    int* p = tab;
+    while (l > 1 || s) {
+#pragma omp task default(shared) depend(in : l) depend(inout : r)
+      r = w(l);
+#pragma omp taskwait depend(in : r) depend(inout : tab)
+      *p = r;
+      p++;
+#pragma omp taskwait depend(in : r) depend(inout : l)
+      l /= r;
+      s = 0;
     }
 #pragma omp task default(shared) depend(in : tab, tab[0])
     g(tab);
