197053190ed836a3898589a992c640a5d2512693
yanni.lefki <yanni.lefki@gmail.com>
2025-05-22 11:04:57 +0200

    working on the transformation (not working yet)

--- tests/ocamlparser_tests/cons2.ml
+++ tests/ocamlparser_tests/cons2.ml
@@ -13,10 +13,7 @@ let into_typed_var ?typ (v : var) =
   | None -> (v, typ_auto)
   | Some t -> (v, t)
 
-let intro_derived_constructor (trm_from : trm) (trm_to : trm) tg : unit =
-  (*explore the trm_from and trm_to to get the expected variables? *)
-
-
+let intro_cons2 tg : unit =
 
   let x : var = pvar "x" in
   let y : var = pvar "y" in
@@ -26,43 +23,40 @@ let intro_derived_constructor (trm_from : trm) (trm_to : trm) tg : unit =
   let typed_y : typed_var = into_typed_var y in
   let typed_t : typed_var = into_typed_var t in
 
-  (*
-  let cons = trm_var (name_to_var "Cons") in
-  let cons2 = trm_var (name_to_var "Cons2") in
-  *)
 
-  let rule_vars : typed_var list = [typed_x; typed_y; typed_t(*; cons; cons2*)] in
+  let cons = trm_toplevel_var "Cons" in
+  let cons2 = trm_toplevel_var "Cons2" in
 
+  let cons_id =
+    match trm_var_inv cons with
+    | Some v -> v.id
+    | _ -> assert false
+  in
+
+  let cons2_id =
+    match trm_var_inv cons2 with
+    | Some v -> v.id
+    | _ -> assert false
+  in
+
+
+  Printf.printf "Ccons id : %d\n Cons2 id : %d\n" cons_id cons2_id;
+
+  let rule_vars : typed_var list = [typed_x; typed_y; typed_t] in
 
   let mark = "" in
+
+  let trm_x = trm_var x in
+  let trm_y = trm_var y in
+  let trm_t = trm_var t in
+
+  let trm_from = trm_apps cons [trm_x; trm_apps cons [trm_y; trm_t]] in
+  let trm_to = trm_apps cons2 [trm_x; trm_y; trm_t] in
 
   let rule =
     {rule_vars; rule_aux_vars = []; rule_from = trm_from; rule_to = trm_to}
   in
   Target.apply_at_target_paths (Rewrite_core.apply_rule_bottom_up ~mark rule) tg
-
-
-
-let intro_cons2 tg : unit =
-
-  (* let var_cons = toplevel_var "Cons" *)
-
-   (* use new_var for pattern vars TODO
-      and put it in the pattern namespace
-   *)
-  let pattern_x : trm = trm_pattern_var "x" in
-  let pattern_y : trm = trm_pattern_var "y" in
-  let pattern_t : trm = trm_pattern_var "t" in
-
-  let cons = trm_var (name_to_var "Cons") in
-  let cons2 = trm_var (name_to_var "Cons2") in
-
-  let trm_from = trm_apps cons [pattern_x; trm_apps cons [pattern_y; pattern_t]] in
-  let trm_to = trm_apps cons2 [pattern_x; pattern_y; pattern_t] in
-  (* print_rule with flags id, then rewrite *)
-
-  (* intro_derived_constructor trm_from trm_to tg *)
- ignore (trm_from,trm_to,tg,intro_derived_constructor)
 
 let _ = Run.script_ml (fun () ->
   !! intro_cons2 [dRoot]
