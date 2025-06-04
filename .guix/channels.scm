955ce5fcfe435e5ab09d5e29e435ad083d274134
Marek Felšöci <marek@felsoci.sk>
2025-06-04 16:34:12 +0200

    Update Guix environment specification

--- .guix/channels.scm
+++ .guix/channels.scm
@@ -4,6 +4,6 @@
   (url "https://git.savannah.gnu.org/git/guix.git")
   (commit "6d0a86d22d75f8f4bb602a35218513ae0aef71f1"))
  (channel
-  (name 'apac-channel)
-  (url "https://gitlab.inria.fr/apac/channel.git")
-  (commit "749ac3ea14d1b68d5b00b8744078dc32145fdebf")))
+  (name 'guix-apac)
+  (url "https://gitlab.com/apac-compiler/guix-apac.git")
+  (commit "9e1cdfa752c886a16c3621228be8c517812f1f5d")))
