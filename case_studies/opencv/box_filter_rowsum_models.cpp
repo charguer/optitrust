e26bddcb012c807ac3421e227e2475b20121719c
Thomas Koehler <thomas.koehler@thok.eu>
2025-06-24 15:04:18 +0200

    rowsum models for input code

--- case_studies/opencv/box_filter_rowsum_models.cpp
+++ case_studies/opencv/box_filter_rowsum_models.cpp
@@ -1,9 +1,10 @@
 #include <optitrust_models.h>
 
 // TODO: move to share with matmul_models, into optitrust_models
-__DECL(reduce_sum, "int * (int -> float) -> float");
-__AXIOM(reduce_sum_empty, "forall (f: int -> float) -> 0.f =. reduce_sum(0, f)");
-__AXIOM(reduce_sum_add_right, "forall (n: int) (f: int -> float) (_: n >= 0) -> reduce_sum(n, f) +. f(n) =. reduce_sum(n + 1, f)");
+__DECL(reduce_sum, "int * (int -> int) -> int");
+__AXIOM(reduce_sum_empty, "forall (f: int -> int) -> 0 = reduce_sum(0, f)");
+__AXIOM(reduce_sum_add_right, "forall (n: int) (f: int -> int) (_: n >= 0) -> reduce_sum(n, f) + f(n) = reduce_sum(n + 1, f)");
+
 /*
   cn: number of (color) channels
   w: width of box filter (convolution window)
@@ -23,27 +24,31 @@ void rowSum(const int w, const int* s, int* d, const int n, const int cn) {
       __sreads("s ~> Matrix2(n+w-1, cn, S)");
       __xwrites("&d[MINDEX2(n, cn, i, c)] ~~> reduce_sum(w, fun k -> S(i+k,c))");
 
+      // TODO: difference with assert_prop admit ?
       __ghost(assume, "is_subrange(i..i + w, 0..n + w - 1)"); // TODO: solve
 
       int sum = 0;
-      __ghost(rewrite_float_linear, "inside := fun v -> &sum ~~> v, by := reduce_sum_empty(fun k -> S(i+k,c))");
+      __ghost(rewrite_linear, "inside := fun v -> &sum ~~> v, by := reduce_sum_empty(fun k -> S(i+k,c))");
+      __ghost(rewrite_linear, "inside := fun v -> &sum ~~> reduce_sum(v, fun k0 -> S(i + k0, c)), from := 0, to := i - i");
 
       for (int k = i; k < i+w; k++) {  // for (int k = 0; k < w; k++) {
-        __spreserves("&sum ~~> reduce_sum(k, fun k0 -> S(i+k0,c))");
+        __spreserves("&sum ~~> reduce_sum(k-i, fun k0 -> S(i+k0,c))");
 
         __ghost(in_range_extend, "k, i..i+w, 0..n+w-1"); // i+k < n+w-1
         __GHOST_BEGIN(focus, ro_matrix2_focus, "s, k, c"); // i+k
         sum += s[MINDEX2(n+w-1, cn, k, c)];
         __GHOST_END(focus);
 
-        __ghost(in_range_bounds, "k", "k_ge_0 <- lower_bound"); // i+k_ge_0
+        // __ghost(in_range_bounds, "k", "k_ge_0 <- lower_bound"); // i+k_ge_0
+        // TODO: remove admit
+        __ghost(assert_prop, "proof := admit(k - i >= 0)", "kmi_ge_0 <- proof");
 
-        // __ghost(rewrite_float_linear, "inside := fun v -> &sum ~~> v, by := add_assoc_right(k, fun k -> S((i+k)+1=i+(k+1),c), k_ge_0)");
-        __ghost(rewrite_float_linear, "inside := fun v -> &sum ~~> v, by := reduce_sum_add_right(k, fun k -> S((i+k)+1=i+(k+1),c), k_ge_0)");
-
-         __spreserves("&sum ~~> reduce_sum(k, fun k0 -> S(i+k0,c)) + S(i+k,c)");
-         __spreserves("&sum ~~> reduce_sum(k+1, fun k0 -> S(i+k0,c))");
+        __ghost(rewrite_linear, "inside := fun v -> &sum ~~> reduce_sum(k - i, fun k0 -> S(i + k0, c)) + S(v, c), from := k, to := i + (k - i)");
+        __ghost(rewrite_linear, "inside := fun v -> &sum ~~> v, by := reduce_sum_add_right(k - i, fun k -> S((i+k),c), kmi_ge_0)");
+        __ghost(rewrite_linear, "inside := fun v -> &sum ~~> reduce_sum(v, fun k0 -> S(i + k0, c)), from := k - i + 1, to := (k + 1) - i");
       }
+
+      __ghost(rewrite_linear, "inside := fun v -> &sum ~~> reduce_sum(v, fun k0 -> S(i + k0, c)), from := i + w - i, to := w");
       d[MINDEX2(n, cn, i, c)] = sum;
     }
   }
---
+++ case_studies/opencv/box_filter_rowsum_models.ml
@@ -0,0 +1,72 @@
+open Optitrust
+open Prelude
+
+let _ = Flags.check_validity := true
+let _ = Flags.recompute_resources_between_steps := true
+let _ = Flags.disable_stringreprs := true
+
+let _ = Flags.save_steps := Some Flags.Steps_effectful
+let _ = Flags.save_ast_for_steps := Some Flags.Steps_effectful
+(*
+*)
+(* Generated trace is too heavy. Keep only the steps of the transformation script
+let _ = Flags.save_steps := Some Steps_script
+ *)
+
+(** Reproducing OpenCV code from:
+  https://github.com/opencv/opencv/blob/4.10.0/modules/imgproc/src/box_filter.simd.hpp#L75
+
+   Remaining differences:
+   - [k++; + k] instead of [k++, S++, D++], we may not want to introduce such pointer arithmetic.
+   - no template support yet for S/ST types; OpenCV also casts inputs from uchar
+
+   c.f. README.md
+   *)
+
+let int = trm_int
+
+let custom_specialize_simpl tg =
+  Trace.without_resource_computation_between_steps (fun () ->
+    Arith.default_simpl tg;
+    Loop.simplify_all_ghosts_group_scale [];
+  )
+
+let only = -1
+(* comment next line for doing all versions *)
+ (* let only = 2 *)
+
+let fast = if only <> -1 then [occIndex only] else [nbMulti]
+
+let _ = Run.script_cpp (fun () ->
+  !! ();
+
+  !! Reduce.intro [cVarDef "s"];
+
+  !! Specialize.variable_multi ~mark_then:fst ~mark_else:"anyw"
+    ["w", int 3; "w", int 5] [cFunBody "rowSum"; cFor "i"];
+  !! Reduce.elim ~inline:true [nbMulti; cMark "w"; cCall "reduce_spe1"];
+  !! Loop.collapse [nbMulti; cMark "w"; cFor "i"];
+
+
+  !! Loop.swap [nbMulti; cMark "anyw"; cFor "i"];
+  !! Reduce.slide ~mark_alloc:"acc" [nbMulti; cMark "anyw"; cArrayWrite "D"];
+  !! Reduce.elim [nbMulti; cMark "acc"; cCall "reduce_spe1"];
+
+  !! Variable.elim_reuse [nbMulti; cMark "acc"];
+  !! Reduce.elim ~inline:true [nbMulti; cMark "anyw"; cFor "i"; cCall "reduce_spe1"];
+  !! Loop.shift_range (StartAtZero) [nbMulti; cMark "anyw"; cFor "i"];
+  !! Loop.scale_range ~factor:(trm_find_var "cn" []) [nbMulti; cMark "anyw"; cFor "i"];
+  !! Specialize.variable_multi ~mark_then:fst ~mark_else:"anycn" ~simpl:custom_specialize_simpl
+    ["cn", int 1; "cn", int 3; "cn", int 4] [cMark "anyw"; cFor "c"];
+      (*
+  !! Loop.unroll [nbMulti; cMark "cn"; cFor "c"];
+  !! Target.foreach (fast@[cMark "cn"]) (fun c ->
+    Loop.fusion_targets ~into:FuseIntoLast [nbMulti; c; cFor "i" ~body:[cArrayWrite "D"]];
+    Instr.gather_targets [c; cStrict; cArrayWrite "D"];
+    Loop.fusion_targets ~into:FuseIntoLast [nbMulti; c; cFor ~stop:[cVar "w"] "i"];
+    Instr.gather_targets [c; cFor "i"; cArrayWrite "D"];
+  );
+  !! Loop.shift_range (ShiftBy (trm_find_var "c" [cMark "anycn"])) [cMark "anycn"; cFor ~body:[cArrayWrite "D"] "i"];
+*)
+)
+
