0184a60c0e4f3b5aaa4f33ba02e62a53b173e9e0
Thomas Koehler <thomas.koehler@thok.eu>
2025-07-23 16:20:05 +0200

    fix first line of matrix_local_name_tile

--- tests/matrix/local_name_tile/matrix_local_name_tile.ml
+++ tests/matrix/local_name_tile/matrix_local_name_tile.ml
@@ -9,10 +9,11 @@ let _ = Run.script_cpp (fun _ ->
   !! Matrix.local_name_tile ~alloc_instr:[cVarDef "a"] ~var:"a" ~local_var:"y_local"
     (* [range 0 10; range 2 10; range 0 4] *)
     [occLast; cFor ~body:[cArrayWrite "a"] "i"];
-  (* FIXME:
+  (* FIXME: arith exprs are not syntactically equal
   !! Matrix.local_name_tile ~alloc_instr:[cVarDef "b"] ~delete:true ~var:"b" ~local_var:"y"
     (* [range 0 10; range 2 10; range 0 4] *)
     [cFor ~body:[cArrayWrite "b"] "j"];
+
   !! Matrix.local_name_tile ~alloc_instr:[cVarDef "c"] ~delete:true ~var:"c" ~local_var:"z"
     (* [(var "i", trm_int 1); range 2 10; range 0 4] *)
     [cFor ~body:[cArrayWrite "c"] "j"];
