c157deafb54a923d5cc87653d9ab2587369af851
charguer <arthur@chargueraud.org>
2025-10-10 17:20:47 +0200

    generalize f11 shortcut for opendoc

--- doc/open_doc_for.sh
+++ doc/open_doc_for.sh
@@ -4,31 +4,48 @@
 #   doc/open_doc_for.sh tests/label/add/label_add_doc.ml
 #
 # This script opens in a browser the documentation page associated
-# with a given file from the /src folder.
+# with a given file from the /src folder or from a doc file the /tests folder.
 #
-# TODO: fix support for /src/lib/transfo/
-# Eg. if filepath is "src/transfo/function.ml", then the page
-# "_doc/optitrust/Optitrust/Optitrust_transfo/Function/index.html"
-# is loaded in a browser.
+# Eg. if filepath is "tests/label/add/label_add_doc.ml", then load the page:
+#   _doc/optitrust/Optitrust/Optitrust_transfo/Label/index.html
+# Eg. if filepath is "src/transfo/function.ml", then load the page
+#   _doc/optitrust/Optitrust/Optitrust_transfo/Function/index.html
 
 OPTITRUST_PATH=$(dirname $0)/..
 TOOLS_FOLDER=${OPTITRUST_PATH}/tools
 cd ${OPTITRUST_PATH}
 
 FILEPATH=$1
-
 if [ -z ${FILEPATH} ]; then
   echo "open_doc_for.sh: missing argument"
   exit 1
 fi
 
-# Get the grand-parent folder path
-BASE=$(dirname ${FILEPATH})
-BASE=$(dirname ${BASE})
-BASE=$(basename ${BASE})
+# Compute the MODULE name. Distinguish between tests and implementation modules
+
+RELATIVE_FILEPATH=$(realpath --relative-to ${OPTITRUST_PATH} ${FILEPATH})
+
+if [[ "${RELATIVE_FILEPATH}" == "lib/transfo/"* ]]; then
+  # Extract the filename
+  BASE=$(basename ${FILEPATH} ".ml")
+
+elif [[ "${RELATIVE_FILEPATH}" == "tests/"* ]]; then
+  # Get the grand-parent folder path
+  BASE=$(dirname ${FILEPATH})
+  BASE=$(dirname ${BASE})
+  BASE=$(basename ${BASE})
+
+else
+  echo "open_doc_for.sh: '${RELATIVE_FILEPATH}' is not a path in tests/ or lib/transfo/"
+  exit 1
+fi
+
+echo "${BASE}"
+
 
 # Obtain the module name by capitalizing
 MODULE="${BASE^}"
+
 
 TARGET="_doc/optitrust/Optitrust_transfo/${MODULE}/index.html"
 
