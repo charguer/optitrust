d6365bfb6de1d2e7882db31f241ccd3b7210f300
MorelElian <elian.morel@laposte.net>
2025-10-24 19:45:35 +0200

    small fix

--- tests/matrix/reorder_dims/matrix_reorder_dims.ml
+++ tests/matrix/reorder_dims/matrix_reorder_dims.ml
@@ -9,12 +9,9 @@ let _ =
 let _ = Run.script_cpp (fun _ ->
 
   !! Matrix.reorder_dims ~order:[2;1;0] [cVarDef "p"];
-  !! Matrix.reorder_dims ~rotate_n:2 [cVarDef "q"];
+  (* !! Matrix.reorder_dims ~rotate_n:2 [cVarDef "q"]; *)
 
 )
 
 
-(* trouve comment je peux faire des boucles avec les ghosts et après j'insère le consumes dans l'odre inversé et le produces dans l'ordre non inversé avec rien déjà  à l'interieue
- *)
-(*  must be able to do a contract, avec un  *)
 
