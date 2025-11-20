9991fdcb6211e06f861871c95cdd6769501cc169
Thomas Koehler <thomas.koehler@thok.eu>
2025-11-20 10:19:28 +0100

    matmul gpu collective sync pattern

--- case_studies/gpu/matmul/matmul_models_exp2.cpp
+++ case_studies/gpu/matmul/matmul_models_exp2.cpp
@@ -20,31 +20,39 @@ void mm(float* c, float* a, float* b, int m, int n, int p,
   for (int b; b < nblk; b++) { // par
     float a_smem[MSIZE2(bk, bm)];
     float b_smem[MSIZE2(bk, bn)];
-    for (int t; t < nthr; t++) { // par
-      float rAcol[tm];
-      float rBrow[tn];
-      float rchProd[MSIZE2(tm, tn)];
 
+    float rAcol[nthr][tm];
+    // float* const rACol = MALLOC2(float, nthr, tm);
+    // Cell(Any)(v) = Cell(v)
+    float rBrow[nthr][tn];
+    float rchProd[nthr][MSIZE2(tm, tn)];
+
+    // block-level tiles
+    uint32_t num_n_tiles = n / bn;
+    uint32_t mrow = b / num_n_tiles;
+    uint32_t mcol = b % num_n_tiles;
+    const int tile_row_a = MINDEX2(m, p, mrow * bm, 0);
+    const int tile_col_b = MINDEX2(p, n, 0, mcol * bn);
+    const int tile_c = MINDEX2(m, n, mrow * bm, mcol * bn);
+
+    uint32_t threadRow[nthr]; // = t / (bn / tn);
+    uint32_t threadCol[nthr]; // = t % (bn / tn);
+
+    for (int t; t < nthr; t++) { // par
       // TODO: memset
       for (uint32_t resIdxM = 0; resIdxM < tm; resIdxM++) {
         for (uint32_t resIdxN = 0; resIdxN < tn; resIdxN++) {
-          rchProd[MINDEX2(tm, tn, resIdxM, resIdxN)] = 0.f;
+          rchProd[t][MINDEX2(tm, tn, resIdxM, resIdxN)] = 0.f;
         }
       }
 
-      // block-level tiles
-      uint32_t num_n_tiles = n / bn;
-      uint32_t mrow = b / num_n_tiles;
-      uint32_t mcol = b % num_n_tiles;
-      const int tile_row_a = MINDEX2(m, p, mrow * bm, 0);
-      const int tile_col_b = MINDEX2(p, n, 0, mcol * bn);
-      const int tile_c = MINDEX2(m, n, mrow * bm, mcol * bn);
+      threadRow[t] = t / (bn / tn);
+      threadCol[t] = t % (bn / tn);
+    }
 
-      uint32_t threadRow = t / (bn / tn);
-      uint32_t threadCol = t % (bn / tn);
-
-      for (uint32_t bkIdx = 0; bkIdx < p; bkIdx += bk) {
-        // TODO: __syncthreads requires fission/reorder
+    for (uint32_t bkIdx = 0; bkIdx < p; bkIdx += bk) {
+      // __syncthreads
+      for (int t; t < nthr; t++) { // par
         for (uint32_t i = 0; i < bm * bk; i += nthr * 4) {
           uint32_t row = (i + t * 4) / bk;
           uint32_t col = (i + t * 4) / bk;
@@ -63,28 +71,32 @@ void mm(float* c, float* a, float* b, int m, int n, int p,
             b_smem[MINDEX2(bk, bn, row, col + v)] = b_gmem[bVectorStart + v];
           }
         }
-        // TODO: __syncthreads requires fission/reorder
+      }
 
+      // TODO: __syncthreads
+      for (int t; t < nthr; t++) { // par
         for (uint32_t dotIdx = 0; dotIdx < bk; dotIdx++) {
           for (uint32_t i = 0; i < tm; i++) {
-            rAcol[i] = a_smem[MINDEX2(bk, bm, dotIdx, threadRow * tm + i)];
+            rAcol[t][i] = a_smem[MINDEX2(bk, bm, dotIdx, threadRow[t] * tm + i)];
           }
           for (uint32_t i = 0; i < tn; i++) {
-            rBrow[i] = b_smem[MINDEX2(bk, bn, dotIdx, threadCol * tn + i)];
+            rBrow[t][i] = b_smem[MINDEX2(bk, bn, dotIdx, threadCol[t] * tn + i)];
           }
           for (uint32_t resIdxM = 0; resIdxM < tm; resIdxM++) {
             for (uint32_t resIdxN = 0; resIdxN < tn; resIdxN++) {
-              rchProd[MINDEX2(tm, tn, resIdxM, resIdxN)] +=
-                rAcol[resIdxM] * rBrow[resIdxN];
+              rchProd[t][MINDEX2(tm, tn, resIdxM, resIdxN)] +=
+                rAcol[t][resIdxM] * rBrow[t][resIdxN];
             }
           }
         }
       }
+    }
 
+    for (int t; t < nthr; t++) { // par
       for (uint32_t resIdxM = 0; resIdxM < tm; resIdxM++) {
         for (uint32_t resIdxN = 0; resIdxN < tn; resIdxN++) {
-          const int out_index = tile_c + MINDEX2(m, n, threadRow * tm + resIdxM, threadCol * tn + resIdxN);
-          c_gmem[out_index] = rchProd[MINDEX2(tm, tn, resIdxM, resIdxN)];
+          const int out_index = tile_c + MINDEX2(m, n, threadRow[t] * tm + resIdxM, threadCol[t] * tn + resIdxN);
+          c_gmem[out_index] = rchProd[t][MINDEX2(tm, tn, resIdxM, resIdxN)];
           // GEMM:
           // = beta * c_gmem[out_index] + alpha * rchProd[MINDEX2(tm, tn, resIdxM, resIdxN)];
         }
