42e3e1171834b9f30c7fc4e1e8f87521ef12b57c
Bastian Koepcke <bastian.koepcke@uni-muenster.de>
2025-11-21 18:05:49 +0100

    add transformation plan

--- case_studies/gpu/matmul/matmul_models.ml
+++ case_studies/gpu/matmul/matmul_models.ml
@@ -5,7 +5,7 @@ let _ = Flags.check_validity := true
 let _ = Flags.pretty_matrix_notation := true
 let _ = Flags.recompute_resources_between_steps := true
 let _ = Flags.disable_stringreprs := true
-let _ = Flags.save_ast_for_steps := Some Flags.Steps_important
+let _ = Flags.save_ast_for_steps := Some Flags.Steps_script
 
 (* let _ = Flags.report_exectime := true *)
 
@@ -18,7 +18,12 @@ let _ = Flags.save_ast_for_steps := Some Flags.Steps_important
 
 let int = trm_int
 
-let _ = Run.script_cpp (fun () ->
+let bm = 32
+
+let current_stage = 1
+
+let _ = if current_stage = 1 then begin
+Run.script_cpp (fun () ->
   (* !! Matrix.local_name ~type_and_dims:(~var:"c" ~local_var:"c_gmem" [cFor "i"]; *)
   !! Matrix.local_name_tile ~uninit_pre:true ~var:"c" ~local_var:"c_gmem" [cFor "i"];
   !! Matrix.local_name_tile ~uninit_post:true ~var:"a" ~local_var:"a_gmem" [cFor "i"];
@@ -32,14 +37,30 @@ let _ = Run.script_cpp (fun () ->
     | [] -> ()
     in
   !! List.iter tiles [
-    "i", ["bi", 32];
-    "j", ["bj", 32];
-    "k", ["bk", 4]
+    "i", ["bi", bm; "ti", 8];
+    "j", ["bj", 32; "tj", 4];
+    "k", ["bkIdx", 4]
   ];
+  !! Loop.reorder_at ~order:["bi"; "bj"; "bkIdx"; "ti"; "tj"; "k"; "i"; "j"] [cPlusEq ~lhs:[cVar "sum"] ()];
 
+  !! Cleanup.std ()
+  (* !! Matrix.local_name_tile ~uninit_post:true ~var:"a_gmem" ~local_var:"a_smem" [cFor "bkIdx"; cFor "ti"]; *)
+  (* !! Matrix.local_name_tile ~uninit_post:true ~var:"b_gmem" ~local_var:"b_smem" [cFor "bkIdx"; cFor "ti"]; *)
+)
+  end
+
+let _ =  if current_stage = 2 then begin
+Run.script_cpp ~filename:"matmul_models_out.cpp" (fun () ->
+  !! Loop.hoist_expr ~dest:[tBefore; cFor "bkIdx"; cFor "ti"] "a_smem" ~indep:["j"; "tj"] [cArrayRead "a_gmem"];
+  !! Loop.hoist_expr ~dest:[tBefore; cFor "bkIdx"; cFor "ti"] "b_smem" ~indep:["i"; "ti"] [cArrayRead "b_gmem"];
+
+  !! Loop.hoist_expr ~dest:[tBefore; cFor "bkIdx"; cFor "i" ~body:[cPlusEq ~lhs:[cVar "sum"] ()]]
+    "a_regs" ~indep:["j"] [cArrayRead "a_smem"];
+  !! Loop.hoist_expr ~dest:[tBefore; cFor "bkIdx"; cFor "i" ~body:[cPlusEq ~lhs:[cVar "sum"] ()]]
+    "b_regs" ~indep:["i"] [cArrayRead "b_smem"];
+
+  !! Cleanup.std ();
   (*
-  !! Function.inline_def [cFunDef "mm"];
-  !! Loop.reorder_at ~order:["bi"; "bj"; "bk"; "i"; "k"; "j"] [cPlusEq ~lhs:[cVar "sum"] ()];
   !! Loop.hoist_expr ~dest:[tBefore; cFor "bi"] "bT" ~indep:["bi"; "i"] [cArrayRead "b"];
   !! Matrix.stack_copy ~var:"sum" ~copy_var:"s" ~copy_dims:1
     [cFor ~body:[cPlusEq ~lhs:[cVar "sum"] ()] "k"];
@@ -48,3 +69,4 @@ let _ = Run.script_cpp (fun () ->
   !! Loop.unroll ~simpl:Arith.do_nothing [cFor ~body:[cPlusEq ~lhs:[cVar "s"] ()] "k"];
   *)
 )
+end
