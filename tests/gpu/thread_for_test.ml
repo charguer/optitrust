f9ef4c3c9baee457c4cf897b5b52f00e877b9229
Julien de Castelnau <tripod320@gmail.com>
2026-01-21 11:43:32 -0600

    Update thread_for tests

--- tests/gpu/thread_for_test.ml
+++ tests/gpu/thread_for_test.ml
@@ -1,8 +1,16 @@
 open Optitrust
 open Prelude
+open Target
 
-let _ = Flags.check_validity := true
+let _ = Flags.check_validity := false
 let _ = Flags.pretty_matrix_notation := true
 let _ = Flags.recompute_resources_between_steps := true
 
-let _ = Run.script_cpp (fun () -> ())
+let _ = Run.script_cpp (fun _ ->
+  !! Resources.ensure_computed ();
+  !! Trace.failure_expected (function
+  | Resource_computation.ResourceError _ -> true
+  | _ -> false) (fun _ -> Instr.delete [occFirst; cTopFunDef "sync_required"; cCall "blocksync"])
+  (* TODO: negative unit tests for removing parts of the contract?
+  E.g. things should break when we get rid of KernelParams, ThreadsCtx, etc. *)
+)
--- tests/gpu/thread_for_test_exp.cpp
+++ tests/gpu/thread_for_test_exp.cpp
@@ -3,7 +3,7 @@
 
 // NOTE: using pretty matrix notation
 
-void test1(int* a, int N, int M) {
+void basic(int* a, int N, int M) {
   __requires("N: int");
   __requires("M: int");
   __requires("A: int * int -> int");
@@ -37,7 +37,7 @@ void test1(int* a, int N, int M) {
       "~~>[GMem] 1");
 }
 
-void test2(int* a, int N, int M) {
+void retile_desyncgroups(int* a, int N, int M) {
   __requires("N: int");
   __requires("M: int");
   __requires("A: int * int -> int");
@@ -117,6 +117,70 @@ void test2(int* a, int N, int M) {
       "..M -> &a[i][j] ~~>[GMem] 1 + 1");
 }
 
+void sync_required(int* a, int N, int M) {
+  __requires("N: int");
+  __requires("M: int");
+  __requires("A: int * int -> int");
+  __requires("msize_commute: __is_true(MSIZE2(N, M) == MSIZE2(M, N))");
+  __requires("bpg: int");
+  __requires("smem_sz: int");
+  __consumes("for i in 0..N -> for j in 0..M -> &a[i][j] ~~>[GMem] 0");
+  __produces("for i in 0..N -> for j in 0..M -> &a[i][j] ~~>[GMem] 1 + 1");
+  __preserves("ThreadsCtx(range_plus(MINDEX1(MSIZE2(N, M), 0), MSIZE2(N, M)))");
+  __reads("KernelParams(MSIZE2(N, M), bpg, smem_sz)");
+  __ghost(group_to_desyncgroup2, "items := fun i j -> &a[i][j] ~~>[GMem] 0");
+  __ghost(define, "x := fun (sz: int) -> range_plus(MINDEX1(sz, 0), sz)",
+          "r1 <- x");
+  __ghost(
+      define,
+      "x := fun (i: int) -> range_plus(MINDEX2(N, MSIZE1(M), i, 0), MSIZE1(M))",
+      "r2 <- x");
+  __ghost(
+      define,
+      "x := fun (i: int) -> range_plus(MINDEX2(M, MSIZE1(N), i, 0), MSIZE1(N))",
+      "r3 <- x");
+  thread for (int i = 0; i < N; i++) {
+    __strict();
+    __xconsumes("desync_for(r2(i)) j in ..M -> &a[i][j] ~~>[GMem] 0");
+    __xproduces("desync_for(r2(i)) j in ..M -> &a[i][j] ~~>[GMem] 1");
+    thread for (int j = 0; j < M; j++) {
+      __strict();
+      __xconsumes("&a[i][j] ~~>[GMem] 0");
+      __xproduces("&a[i][j] ~~>[GMem] 1");
+      __gmem_set(&a[i][j], 1);
+    }
+  }
+  blocksync();
+  __with(
+      "H := desync_for(r1(MSIZE2(N, M))) i in ..N -> desync_for(r2(i)) j in "
+      "..M -> &a[i][j] ~~>[GMem] 1");
+  __ghost(swap_groups, "items := fun i j -> &a[i][j] ~~>[GMem] 1");
+  __ghost(rewrite_linear,
+          "inside := fun sz -> ThreadsCtx(r1(sz)), by := msize_commute");
+  __ghost(group_to_desyncgroup2,
+          "D1 := N, D2 := M, items := fun i j -> &a[j][i] ~~>[GMem] 1");
+  thread for (int i = 0; i < M; i++) {
+    __strict();
+    __xconsumes("desync_for(r3(i)) j in ..N -> &a[j][i] ~~>[GMem] 1");
+    __xproduces("desync_for(r3(i)) j in ..N -> &a[j][i] ~~>[GMem] 1 + 1");
+    thread for (int j = 0; j < N; j++) {
+      __strict();
+      __xconsumes("&a[j][i] ~~>[GMem] 1");
+      __xproduces("&a[j][i] ~~>[GMem] 1 + 1");
+      const int va = __gmem_get(&a[j][i]);
+      __gmem_set(&a[j][i], va + 1);
+    }
+  }
+  __ghost(rewrite_linear,
+          "inside := fun sz -> ThreadsCtx(r1(sz)), by := eq_sym(MSIZE2(N, M), "
+          "MSIZE2(M, N), msize_commute)");
+  blocksync();
+  __with(
+      "H := desync_for(r1(MSIZE2(M, N))) i in ..M -> desync_for(r3(i)) j in "
+      "..N -> &a[j][i] ~~>[GMem] 1 + 1");
+  __ghost(swap_groups, "items := fun i j -> &a[j][i] ~~>[GMem] 1 + 1");
+}
+
 __ghost(define,
         "x := fun (sz: int) -> range_plus(MINDEX1(MSIZE1(sz), 0), MSIZE1(sz))",
         "rr1 <- x");
@@ -157,49 +221,46 @@ __ghost(assert_prop,
         "-> __is_true(reduce_sum(n, f) + f(n) == reduce_sum(n + 1, f)))",
         "reduce_sum_add_right <- proof");
 
-void read_test1(int* a, int* b, int N) {
+void read_thread_outer(int* a, int* b, int N) {
   __requires("B: int -> int");
   __preserves("ThreadsCtx(rr1(N))");
   __writes("desync_for(rr1(N)) i in ..N -> &a[i] ~~>[GMem] reduce_sum(N, B)");
   __reads("b ~> Matrix1(N, B)");
-  thread for (int i = 0; i < N; i++) {
+  thread for (int t = 0; t < N; t++) {
     __strict();
     __sreads("b ~> Matrix1(N, B)");
-    __xwrites("&a[i] ~~>[GMem] reduce_sum(N, B)");
-    __gmem_set(&a[i], 0);
+    __xwrites("&a[t] ~~>[GMem] reduce_sum(N, B)");
+    __gmem_set(&a[t], 0);
     __ghost(rewrite_linear,
-            "inside := fun v -> &a[i] ~~>[GMem] v, by := reduce_sum_empty(B)");
-    for (int j = 0; j < N; j++) {
+            "inside := fun v -> &a[t] ~~>[GMem] v, by := reduce_sum_empty(B)");
+    for (int i = 0; i < N; i++) {
       __strict();
       __spreserves(
-          "ThreadsCtx(range_plus(MINDEX2(N, MSIZE0(), i, 0), MSIZE0()))");
-      __spreserves("&a[i] ~~>[GMem] reduce_sum(j, B)");
+          "ThreadsCtx(range_plus(MINDEX2(N, MSIZE0(), t, 0), MSIZE0()))");
+      __spreserves("&a[t] ~~>[GMem] reduce_sum(i, B)");
       __sreads("b ~> Matrix1(N, B)");
       const __ghost_fn focus =
-          __ghost_begin(ro_matrix1_focus, "matrix := b, i := j");
-      const int va = __gmem_get(&a[i]);
-      const int vb = __gmem_get(&b[j]);
-      __gmem_set(&a[i], va + vb);
+          __ghost_begin(ro_matrix1_focus, "matrix := b, i := i");
+      const int va = __gmem_get(&a[t]);
+      const int vb = __gmem_get(&b[i]);
+      __gmem_set(&a[t], va + vb);
       __ghost_end(focus);
-      __ghost(in_range_bounds, "x := j", "j_geq_0 <- lower_bound");
+      __ghost(in_range_bounds, "x := i", "i_geq_0 <- lower_bound");
       __ghost(rewrite_linear,
-              "inside := fun v -> &a[i] ~~>[GMem] v, by := "
-              "reduce_sum_add_right(j, B, j_geq_0)");
+              "inside := fun v -> &a[t] ~~>[GMem] v, by := "
+              "reduce_sum_add_right(i, B, i_geq_0)");
     }
   }
 }
 
-void read_test2(int* a, int* b, int N) {
+void read_thread_inner(int* a, int* b, int N) {
   __requires("B: int -> int");
   __requires("bpg: int");
   __requires("smem_sz: int");
-  __consumes("a ~> UninitMatrix1(N)");
-  __produces("for i in 0..N -> &a[i] ~~>[GMem] reduce_sum(N, B)");
   __preserves("ThreadsCtx(rr1(N))");
+  __writes("desync_for(rr1(N)) i in ..N -> &a[i] ~~>[GMem] reduce_sum(N, B)");
   __reads("KernelParams(MSIZE1(N), bpg, smem_sz)");
   __reads("b ~> Matrix1(N, B)");
-  __ghost(group_to_desyncgroup,
-          "items := fun i -> &a[i] ~> UninitCellOf(GMem)");
   thread for (int t = 0; t < N; t++) {
     __strict();
     __xwrites("&a[t] ~~>[GMem] reduce_sum(0, B)");
@@ -207,17 +268,12 @@ void read_test2(int* a, int* b, int N) {
     __ghost(rewrite_linear,
             "inside := fun v -> &a[t] ~~>[GMem] v, by := reduce_sum_empty(B)");
   }
-  blocksync();
-  __with(
-      "H := desync_for(rr1(N)) i in ..N -> &a[i] ~~>[GMem] reduce_sum(0, B)");
   for (int i = 0; i < N; i++) {
     __strict();
     __spreserves("ThreadsCtx(range_plus(MINDEX1(MSIZE1(N), 0), MSIZE1(N)))");
-    __spreserves("for j in 0..N -> &a[j] ~~>[GMem] reduce_sum(i, B)");
-    __sreads("KernelParams(MSIZE1(N), bpg, smem_sz)");
+    __spreserves(
+        "desync_for(rr1(N)) t in ..N -> &a[t] ~~>[GMem] reduce_sum(i, B)");
     __sreads("b ~> Matrix1(N, B)");
-    __ghost(group_to_desyncgroup,
-            "items := fun j -> &a[j] ~~>[GMem] reduce_sum(i, B)");
     thread for (int t = 0; t < N; t++) {
       __strict();
       __sreads("b ~> Matrix1(N, B)");
@@ -234,9 +290,5 @@ void read_test2(int* a, int* b, int N) {
               "inside := fun v -> &a[t] ~~>[GMem] v, by := "
               "reduce_sum_add_right(i, B, i_geq_0)");
     }
-    blocksync();
-    __with(
-        "H := desync_for(rr1(N)) j in ..N -> &a[j] ~~>[GMem] reduce_sum(i + 1, "
-        "B)");
   }
 }
