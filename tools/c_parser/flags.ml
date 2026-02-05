1f2685143986881d6cb9dc29c611d45e75eaab7c
Julien de Castelnau <julien.decastelnau@epfl.ch>
2026-02-05 17:36:43 +0100

    fix: flag needs to be in c_parser as well

--- tools/c_parser/flags.ml
+++ tools/c_parser/flags.ml
@@ -3,6 +3,7 @@ open Optitrust_utils
 let verbose = ref false
 let force = ref false
 let dump_clang_ast = ref None
+let clang_use_libstdcxx = ref false
 
 (** [spec]: possible command line arguments. *)
 let spec : (string * Arg.spec * string) list = [
@@ -10,7 +11,8 @@ let spec : (string * Arg.spec * string) list = [
      ("-v", Arg.Set verbose, " shorthand for --verbose");
      ("--force", Arg.Set force, " ensures that the input file is parsed even if it is already cached");
      ("-f", Arg.Set force, " shorthand for --force");
-     ("--dump-clang-ast", Arg.String (fun f -> dump_clang_ast := Some f), " dump the AST as produced by clang into a file")
+     ("--dump-clang-ast", Arg.String (fun f -> dump_clang_ast := Some f), " dump the AST as produced by clang into a file");
+     ("--clang-use-libstdcxx", Arg.Set clang_use_libstdcxx, " use libstdc++ instead of libc++ when compiling with clangml");
    ]
 
 let program_path = Sys.argv.(0)
