337171a0d0de087d5e6172855beecb3de416d6f9
Marek Felšöci <marek@felsoci.sk>
2026-04-24 12:15:37 +0200

    Allow authentication of 'guix channel

--- .guix/channels.scm
+++ .guix/channels.scm
@@ -1,8 +1,13 @@
 (list
  (channel
   (name 'guix)
-  (url "https://git.savannah.gnu.org/git/guix.git")
-  (commit "6d0a86d22d75f8f4bb602a35218513ae0aef71f1"))
+  (url "https://git.guix.gnu.org/guix.git")
+  (branch "master")
+  (commit "6d0a86d22d75f8f4bb602a35218513ae0aef71f1")
+  (introduction
+   (make-channel-introduction "9edb3f66fd807b096b48283debdcddccfea34bad"
+    (openpgp-fingerprint
+     "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
  (channel
   (name 'guix-apac)
   (url "https://gitlab.com/apac-compiler/guix-apac.git")
