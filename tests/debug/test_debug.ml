971246fc4730288777c79dce56922f672f668dba
yanni.lefki <yanni.lefki@gmail.com>
2026-07-20 17:23:44 +0200

    done cleaning "check_validity" . [WIP] Deprecating non-model flags

--- tests/debug/test_debug.ml
+++ tests/debug/test_debug.ml
@@ -1,7 +1,8 @@
 open Optitrust
 open Prelude
 
-let _ = Flags.check_validity := true
+(* let _ = Flags.check_validity := true *)
+let _ = Flags.typechecking_mode := Flags.AnnotatedAndVerified
 let _ = Flags.disable_stringreprs := true
 
 let _ = Run.script_cpp (fun () ->
--- tests/resources_with_models/computation/fun_args.ml
+++ tests/resources_with_models/computation/fun_args.ml
@@ -1,7 +1,8 @@
 open Optitrust
 open Prelude
 
-let _ = Flags.check_validity := true
+(* let _ = Flags.check_validity := true *)
+let _ = Flags.typechecking_mode := Flags.AnnotatedAndVerified
 let _ = Flags.recompute_resources_between_steps := true
 (*let _ = Flags.resource_errors_as_warnings := true*)
 
